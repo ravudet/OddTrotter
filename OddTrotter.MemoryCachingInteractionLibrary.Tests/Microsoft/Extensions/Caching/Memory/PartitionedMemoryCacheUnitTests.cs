497a1c4e38371dc13c7288ed6a9f20fc6e107a33
ravudet <170215607+ravudet@users.noreply.github.com>
2024-06-14 09:52:31 -0700

    sasdf

---
+++ OddTrotter.MemoryCachingInteractionLibrary.Tests/Microsoft/Extensions/Caching/Memory/PartitionedMemoryCacheUnitTests.cs
@@ -0,0 +1,16 @@
+﻿namespace Microsoft.Extensions.Caching.Memory
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System;
+
+    [TestClass]
+    public sealed class PartitionedMemoryCacheUnitTests
+    {
+        [TestMethod]
+        public void NullDelegateMemoryCache()
+        {
+            //// TODO
+            Assert.ThrowsException<ArgumentNullException>(() => new PartitionedMemoryCache<string>(null, string.Empty));
+        }
+    }
+}
