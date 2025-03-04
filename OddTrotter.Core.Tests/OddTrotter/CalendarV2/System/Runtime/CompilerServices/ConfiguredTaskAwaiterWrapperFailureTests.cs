c18d6ea7b3267c3d545ae065b2c926a695fd8a34
ravudet <170215607+ravudet@users.noreply.github.com>
2025-03-04 07:24:28 -0800

    asdf

---
+++ OddTrotter.Core.Tests/OddTrotter/CalendarV2/System/Runtime/CompilerServices/ConfiguredTaskAwaiterWrapperFailureTests.cs
@@ -0,0 +1,33 @@
+﻿namespace System.Runtime.CompilerServices
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public sealed class ConfiguredTaskAwaiterWrapperFailureTests
+    {
+        [TestMethod]
+        public void OnCompletedNullContinuation()
+        {
+            var wrapper = new ConfiguredTaskAwaiterWrapper<string>(new ConfiguredTaskAwaitable<string>.ConfiguredTaskAwaiter());
+
+            Assert.ThrowsException<ArgumentNullException>(() => wrapper.OnCompleted(
+#pragma warning disable CS8625 // Cannot convert null literal to non-nullable reference type.
+                null
+#pragma warning restore CS8625 // Cannot convert null literal to non-nullable reference type.
+                ));
+        }
+
+
+        [TestMethod]
+        public void UnsafeOnCompletedNullContinuation()
+        {
+            var wrapper = new ConfiguredTaskAwaiterWrapper<string>(new ConfiguredTaskAwaitable<string>.ConfiguredTaskAwaiter());
+
+            Assert.ThrowsException<ArgumentNullException>(() => wrapper.UnsafeOnCompleted(
+#pragma warning disable CS8625 // Cannot convert null literal to non-nullable reference type.
+                null
+#pragma warning restore CS8625 // Cannot convert null literal to non-nullable reference type.
+                ));
+        }
+    }
+}
--- OddTrotter.Core/OddTrotter/CalendarV2/System/Runtime/CompilerServices/ConfiguredTaskAwaiterWrapper.cs
+++ OddTrotter.Core/OddTrotter/CalendarV2/System/Runtime/CompilerServices/ConfiguredTaskAwaiterWrapper.cs
@@ -1,15 +1,20 @@
-﻿
+﻿/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 namespace System.Runtime.CompilerServices
 {
     public sealed class ConfiguredTaskAwaiterWrapper<T> : ITaskAwaiter<T>
     {
         private readonly ConfiguredTaskAwaitable<T>.ConfiguredTaskAwaiter configuredTaskAwaiter;
 
+        /// <summary>
+        /// placeholder
+        /// </summary>
+        /// <param name="configuredTaskAwaiter"></param>
         public ConfiguredTaskAwaiterWrapper(ConfiguredTaskAwaitable<T>.ConfiguredTaskAwaiter configuredTaskAwaiter)
         {
             this.configuredTaskAwaiter = configuredTaskAwaiter;
         }
 
+        /// <inheritdoc/>
         public bool IsCompleted
         {
             get
@@ -18,18 +23,25 @@ namespace System.Runtime.CompilerServices
             }
         }
 
+        /// <inheritdoc/>
         public T GetResult()
         {
             return this.configuredTaskAwaiter.GetResult();
         }
 
+        /// <inheritdoc/>
         public void OnCompleted(Action continuation)
         {
+            ArgumentNullException.ThrowIfNull(continuation);
+
             this.configuredTaskAwaiter.OnCompleted(continuation);
         }
 
+        /// <inheritdoc/>
         public void UnsafeOnCompleted(Action continuation)
         {
+            ArgumentNullException.ThrowIfNull(continuation);
+
             this.configuredTaskAwaiter.UnsafeOnCompleted(continuation);
         }
     }
