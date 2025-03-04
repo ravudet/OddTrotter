8fb7fe680069ec109619701f2a8b85298fdab118
ravudet <170215607+ravudet@users.noreply.github.com>
2025-03-04 07:50:30 -0800

    asdf

--- OddTrotter.Core.Tests/OddTrotter/CalendarV2/System/Threading/Tasks/TaskWrapperUnitTests.cs
+++ OddTrotter.Core.Tests/OddTrotter/CalendarV2/System/Threading/Tasks/TaskWrapperUnitTests.cs
@@ -18,6 +18,59 @@
                 ));
         }
 
+        [TestMethod]
+        public async Task UnsafeOnCompleted()
+        {
+            var providedValue = "Asdf";
+            var value = await new AwaitedType<string>(providedValue).GetValue();
+            Assert.AreEqual(providedValue, value);
+        }
+
+        private sealed class AwaitedType<T>
+        {
+            private readonly T value;
+
+            public AwaitedType(T value)
+            {
+                this.value = value;
+            }
+
+            public ITask<T> GetValue()
+            {
+                // we need to delegate to something that uses the .NET task so that we can wrap it; we aren't actually
+                // implementing a task here
+                return new TaskWrapper<T>(GetValueImpl());
+            }
+
+            private async Task<T> GetValueImpl()
+            {
+                // we can't use `task.fromresult` because that returns a task that is already completed; so, we need to use
+                // something that starts out "not completed" so that the state machine will actually call the `oncompleted`
+                // methods
+                await Task.Delay(100).ConfigureAwait(false);
+                return this.value;
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         private sealed class Context : SynchronizationContext
         {
             private readonly ConcurrentQueue<(SendOrPostCallback, object?)> queue;
@@ -116,31 +169,8 @@
         private static object Lock = new object();
 
         [TestMethod]
-        public async Task Test()
-        {
-            var value = await new AwaitedType().GetValue();
-            Assert.AreEqual("asdf", value);
-
-            //// TODO use this to create a test which runs the safe oncompleted method: https://sharplab.io/#v2:CYLg1APgAgTAjAWAFBQAwAIpwKwG5lqZwB0ASgK4B2ALgJYC2ApsQMID29ADrQDaMBOAZQEA3WgGNGAZ3woAzJhjoW6AN4EFUAByYAbOgAq06gAoAlMnVJ0N9CICG/O/Z7lG6ALyYAnOkqMAd3QAQQD7WmpGYAMAT05Gc2IAcUZqADUXN3NZAF9kZE5+WgdI9ClGFyjFELCIqNj4y2RbTAUASShdAB4sVAA+dBT0zISLa1srFpaoAHY/QL0AdX57Tnj+Hrh+kwAxNjZzM1kWvKRm2yhNHU7N/vQ9g7GWyambKF9O4gARRh57GJMW1QZlYbEoADNaABzcj8RihcKmcEucpHc6vWboABE9ikwHBWOOtlOpxa6Na6FoNAEyMk6A63TY5Gohj6TXGNgZCLqGwMAyG3Mi/GyyFJtnJlzKFT4wGqnWWq3WXT56BA9JufPZLUKxXspTh9mAYJ4MT0yoG1FxAGtZBLNLoFWsBCYNRbrU8JuSWtQABa0KTES1SK2edBBm3ksU2O3q3SCgTmwapePCj02F4Yub+IKdFOOpV8ky+/2B63JZO1IWHIk2EnkmNUoW09xcysJpkslVqtosIp0cQuABybDo4Ji7C4fDoYK1tgARvsePSpBPOFOqqp0FDUrh0KdyQYk9RSNJyDxTGikFH0DHypVZbA9HmVk7eQNu7m22/ZzYdSV3AaRqUCaT5fom4YpraHIUp+iICPmzqwTy4HWimaZqF6tjFgGEFfqGuFwfwNZ7vk0GSgubBLm0K4cGuqRROSGZTNu1CYembFTJi2GlsGKbENRq7rsAxEnJGpHTAoh5DCeUhnhejEcVxfo4ahX7lsep7niK0H7mRmgACzoAA8pQgn0cALpwEo4hgnQlDkHqtBguhTHespPFWnxJlmZEFk2TQVIOdOlCXqJZx6ZghkAKqUFI9jgow3m0UJlnWbZgWOc5CnQW5JYETyxAxXFCVJZO5kmP5dlBU5IXEXWV5AA=
-        }
-
-        private sealed class AwaitedType
-        {
-            public ITask<string> GetValue()
-            {
-                return new TaskWrapper<string>(Foo());
-            }
-
-            public async Task<string> Foo()
-            {
-                await Task.Delay(100).ConfigureAwait(false);
-                return "asdf";
-            }
-        }
-
-        [TestMethod]
         public Task Test2()
-        {
+        {//// TODO use this to create a test which runs the safe oncompleted method: https://sharplab.io/#v2:CYLg1APgAgTAjAWAFBQAwAIpwKwG5lqZwB0ASgK4B2ALgJYC2ApsQMID29ADrQDaMBOAZQEA3WgGNGAZ3woAzJhjoW6AN4EFUAByYAbOgAq06gAoAlMnVJ0N9CICG/O/Z7lG6ALyYAnOkqMAd3QAQQD7WmpGYAMAT05Gc2IAcUZqADUXN3NZAF9kZE5+WgdI9ClGFyjFELCIqNj4y2RbTAUASShdAB4sVAA+dBT0zISLa1srFpaoAHY/QL0AdX57Tnj+Hrh+kwAxNjZzM1kWvKRm2yhNHU7N/vQ9g7GWyambKF9O4gARRh57GJMW1QZlYbEoADNaABzcj8RihcKmcEucpHc6vWboABE9ikwHBWOOtlOpxa6Na6FoNAEyMk6A63TY5Gohj6TXGNgZCLqGwMAyG3Mi/GyyFJtnJlzKFT4wGqnWWq3WXT56BA9JufPZLUKxXspTh9mAYJ4MT0yoG1FxAGtZBLNLoFWsBCYNRbrU8JuSWtQABa0KTES1SK2edBBm3ksU2O3q3SCgTmwapePCj02F4Yub+IKdFOOpV8ky+/2B63JZO1IWHIk2EnkmNUoW09xcysJpkslVqtosIp0cQuABybDo4Ji7C4fDoYK1tgARvsePSpBPOFOqqp0FDUrh0KdyQYk9RSNJyDxTGikFH0DHypVZbA9HmVk7eQNu7m22/ZzYdSV3AaRqUCaT5fom4YpraHIUp+iICPmzqwTy4HWimaZqF6tjFgGEFfqGuFwfwNZ7vk0GSgubBLm0K4cGuqRROSGZTNu1CYembFTJi2GlsGKbENRq7rsAxEnJGpHTAoh5DCeUhnhejEcVxfo4ahX7lsep7niK0H7mRmgACzoAA8pQgn0cALpwEo4hgnQlDkHqtBguhTHespPFWnxJlmZEFk2TQVIOdOlCXqJZx6ZghkAKqUFI9jgow3m0UJlnWbZgWOc5CnQW5JYETyxAxXFCVJZO5kmP5dlBU5IXEXWV5AA=
             StateMachine stateMachine = new StateMachine();
             stateMachine.builder = AsyncTaskMethodBuilder.Create();
             stateMachine.self = this;
@@ -183,7 +213,7 @@
                     ITaskAwaiter<string> awaiter;
                     if (num != 0)
                     {
-                        awaiter = new AwaitedType().GetValue().GetAwaiter();
+                        awaiter = new AwaitedType<string>("asfd").GetValue().GetAwaiter();
                         if (!awaiter.IsCompleted)
                         {
                             num = (state = 0);
