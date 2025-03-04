e27b7ae46a6a505844300e9fcd5e7a91f817a16b
ravudet <170215607+ravudet@users.noreply.github.com>
2025-03-04 06:54:17 -0800

    asdf

--- OddTrotter.MemoryCachingInteractionLibrary/OddTrotter/TodoList/TodoListService.cs
+++ OddTrotter.MemoryCachingInteractionLibrary/OddTrotter/TodoList/TodoListService.cs
@@ -382,7 +382,6 @@
             // unit tests
             // do everything above for unit tests
 
-            //// TODO code review unit tests for queryresultnodeextensions
             //// TODO sanity check changes from 4f9cc62edc490a6c58c11ad8bf8d713be1f59ef9 (02/27) onward
             //// TODO code review system.threading.tasks
             //// TODO code review system.runtime.compilerservices
