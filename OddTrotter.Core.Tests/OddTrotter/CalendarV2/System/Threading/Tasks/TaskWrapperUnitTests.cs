338226e11f06c864356ea6aaf275779b044dea3c
ravudet <170215607+ravudet@users.noreply.github.com>
2025-03-04 08:09:04 -0800

    asdf

--- OddTrotter.Core.Tests/OddTrotter/CalendarV2/System/Threading/Tasks/TaskWrapperUnitTests.cs
+++ OddTrotter.Core.Tests/OddTrotter/CalendarV2/System/Threading/Tasks/TaskWrapperUnitTests.cs
@@ -52,6 +52,133 @@
             }
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        /// <remarks>
+        /// This method and it's associated <see cref="SafeOnCompletedStateMachine"/> are slightly modified code from the compiler generated state machine for the following code:
+        /// ```
+        /// var providedValue = "Asdf";
+        /// var value = await new AwaitedType<string>(providedValue).GetValue();
+        /// Assert.AreEqual(providedValue, value);
+        /// ```
+        /// 
+        /// The compiler generated code has been modified in the following way:
+        /// 1. identifiers have been renamed to be legal
+        /// 2. nullability issues have been suppressed, removed, or forgiven
+        /// 3. the line `builder.AwaitUnsafeOnCompleted(ref awaiter, ref stateMachine);` has been changed to `builder.AwaitOnCompleted(ref awaiter, ref stateMachine);`
+        /// 
+        /// The intent is to have a test which covers the case where the "safe" `OnCompleted` variant is called while still provided callers the more efficient "unsafe" variant.
+        /// </remarks>
+        [TestMethod]
+        public Task SafeOnCompleted()
+        {
+            SafeOnCompletedStateMachine stateMachine = new SafeOnCompletedStateMachine();
+            stateMachine.builder = AsyncTaskMethodBuilder.Create();
+            stateMachine.self = this;
+            stateMachine.state = -1;
+            stateMachine.builder.Start(ref stateMachine);
+            return stateMachine.builder.Task;
+        }
+
+        [CompilerGenerated]
+        private sealed class SafeOnCompletedStateMachine : IAsyncStateMachine
+        {
+            public int state;
+
+            public AsyncTaskMethodBuilder builder;
+
+            public TaskWrapperUnitTests
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+                self
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+                ;
+
+            private string
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+                providedValue
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+                ;
+
+            private string
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+                value
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+                ;
+
+            private string
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+                s3
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+                ;
+
+            private object
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+                u1;
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+
+            private void MoveNext()
+            {
+                int num = state;
+                try
+                {
+                    ITaskAwaiter<string> awaiter;
+                    if (num != 0)
+                    {
+                        providedValue = "Asdf";
+                        awaiter = new AwaitedType<string>(providedValue).GetValue().GetAwaiter();
+                        if (!awaiter.IsCompleted)
+                        {
+                            num = (state = 0);
+                            u1 = awaiter;
+                            SafeOnCompletedStateMachine stateMachine = this;
+                            builder.AwaitOnCompleted(ref awaiter, ref stateMachine);
+                            return;
+                        }
+                    }
+                    else
+                    {
+                        awaiter = (ITaskAwaiter<string>)u1;
+                        u1 = null!;
+                        num = (state = -1);
+                    }
+                    s3 = awaiter.GetResult();
+                    value = s3;
+                    s3 = null!;
+                    Assert.AreEqual(providedValue, value);
+                }
+                catch (Exception exception)
+                {
+                    state = -2;
+                    providedValue = null!;
+                    value = null!;
+                    builder.SetException(exception);
+                    return;
+                }
+                state = -2;
+                providedValue = null!;
+                value = null!;
+                builder.SetResult();
+            }
+
+            void IAsyncStateMachine.MoveNext()
+            {
+                //ILSpy generated this explicit interface implementation from .override directive in MoveNext
+                this.MoveNext();
+            }
+
+            [DebuggerHidden]
+            private void SetStateMachine(IAsyncStateMachine stateMachine)
+            {
+            }
+
+            void IAsyncStateMachine.SetStateMachine(IAsyncStateMachine stateMachine)
+            {
+                //ILSpy generated this explicit interface implementation from .override directive in SetStateMachine
+                this.SetStateMachine(stateMachine);
+            }
+        }
 
 
 
@@ -64,7 +191,7 @@
 
 
 
-
+        //// TODO you pull the above state machine from sharplab: https://sharplab.io/#v2:CYLg1APgdghgtgUwM4AcYGMEAIACBGAVgFgAoAb1KytwAZc8A6AJQFcoAXAS0QYGEB7OCk4AbBACcAyhIBunTEgDclaiqo4AzPQBsuAExYAgkiQT2arBRLUbuLfl04ALEfEIAogEcWMEQAp+ACMAKwR0diwEAA8UMPYEYAAaLCDQ8KwMdh8RAEoLGytbagBfC1KSC00sU18E/VxtAHVxGBRY8QBVKE52ABVkdiQLQqK7XAAOBqxJGAAzBAB5KAEhMXjgPzzrUZHR6hkYcSwUcX45YASANV8WbABeLAAiY2BZx+VtvaoDo4ORW6wDxwAE4sFAEAB3IwQmA9BK9ACesQAPPgaAA+PwnM6cC7Aa7/BA5BgAcQQ7AJt02Hy+1GMpnE7AYhjcXmyWNO5yuNwQyT+txyNNG5Xy1BOnAO8WqCFqwHqhhhcOAiJRvXRoqouy+4sl2DcMGA/CgIgRWF6WH5CA+GtsVQVsPWKoQfnNlq2tM1Nr27AAFpwkAxLYCLTyhbSRZ8vlUAJI4bTItVYMkUnmbL3ULUerAAemzWAh2HBdXY/CwFzEAHMYFKS9VBOS/VAK1hfdWsCxTEgWz7sAwAHLuc3sGBIADWde7bYLWHQMCg+ZaKCwPUU+ewhwQUAA5BFMtkTemirnl6sEIgOJwmxkWyPxz23IfbQB2MGQhrNVrtBOY5OUhDR1ZNkFR8IyzHVq2wHBJjjb8k3JP8AJQfx3Q9TMPWPadZ23CIO2wAADYcxwYWZTjgNwkBYER2DwrBAjCGBcMnCIcCfLsYBvMcmOXNiRH1YBTXQQQkPJBJVyQfhkmnIs5VrXDHxsY9xMQX1L2bVsIiQYdGS7fgWAiR4oH4CJBNPdZHgndTu2wTSIKwOAMEbbAIVEEQMnCfcBN8VzfXwo0TOE9Y8Pk6hj2Un1+GAIZIyzEEGgYAARBARBgBE/DwGgaGJAQoFmTgKxYNx7R6PxZl8UxgOij0WO7f1A1DECbXKGwmtUSMqhqMQ5RwAxsty/K3GAOMivicQPzaCRYJALBY20YaJrVUgtXAqU+KNE0sF6vKCoSIbFWHQIxG/PgjT67bBtmvaJBnE6toG3aHQka1I1zPNkQouB7PEBF1We48kIwBBwpEC5xAsF6sGRbN3s+76wePZE0BaOAwXgBA7keQSctunaLoe8RHnRSHEfgH6bCqTb+pxubRsXdo/Aps77p6GADoQI6Gbu3G4SOTHTs56mUM9SqqBUgNeex87qeDcXKcly7xDDLBQJC+HL3vHpDXQbNSeoKpAn4fhXOjJAVgChJhhtCtyRtNCimq0WGBlxmuZGhhjdNtZRMaspSDh161YkDX+C1nX1C0c1kyYZBKPYNNI1t3AXwdp3+fl0lySjiiqOpH2Kl+/2oHV9hNe1yp7BcJYPZEjZ8AMTGuCgHwuCNQXLBtFl8vPdg+0okR3CiTAUGbqAGF6H1TghaNZh7kR/Hry8m84Funu9P0xZu2WmddyuhM9jZ58b6sl6gCrmt9/OIYD8Qg5DsvcBcLokDmRZll36u/Fr66L0P4fW4TjuWBdxnn3AeCAh7H1HuPfgk9p69z8AfRey9z6r1qinKmadH7Px3qZBI8CjQN0QSfRW5QWpUDvh1Oo3UNobzOsNFmYgxpfkTFNaMHMEh0NZt+RaFhlp6hlIaY0po2FywdPQtmiY0EiOZqzFeKtXrQ0OLDC+/1MBAxBn7CGUNAEw1DjmeGxNkawEQOjSRW8xEEyJocEmd9hEcIYbTCQ9MaGp1EZwiRzj0GuLEH/G0ycPFSP2mIaW/izEyNzho5EV8b6lzaloGa1NYLJmpnHAoNpqrgihMIreEhGHzUxH4rGm8XZiPTuwZJORT4lFIKQrA5CZSdXqNkmmn48lYBYU0rh5AeHX11FgVagiGgJMTIRUc1NZFUHBm9bRijdHgxUYDQ26iL6QwUV9WZ+irGGNRujEZ1MLHZgMboqoTTcniA/i7Vpuz5Y+OFjVAMVy8bBgedzYhyC5GX0LoHYuwcYlky0PrQ200TZv3WBbW5VtzC3ITmTJOa8GDPNdu7EFXtbk1OVhM1Wnzr7fNvrEs0cF2CZxjikjMaTYW1QRRIUpRLs6VKoOivRBci4lyOeXLA2CzY1zwHXfBC8j4tzBXsABQDe790HsPSBE8p7ALwd/QhFVfFwspeIBgHK96yoIfyoh4TllRJxb83WbLMHzDVe/T+CCtU3KFeITum5u6itAeAo0kroHSrgRa3+4zbAO2VQwY1L8q7rA1Xyz1ucantXqZQgwcZTmTWmjBBaXTIy8L6fwtapoE3og4qOL1kzVlKIUseCJWiPozIiQYlGxjHgjP2YciJ0RxXHxnG4WY6NhV2uAWKsBw8CZjwnvOTgswIYGJbZWtG1bbyPG1txCGphsDJSbBCfg4hgDo0brPKdhNswNu7cfVl74HFnMzdmq1ox20cE7Y6iVfbXWwNnn4EZCrbk+tvE828ry86FqZV8lld94ny0SeScpgrRjpLfCcw934H1KtvKU8pdKlZvIxd+7Fv68WsP8XY8RWa2HDT8AC1yCCA2tCyANbK8QojsFPXbF8GTqGFNoXtMRsa1TQYpbB3De0SplSJAhkhd9LwjVKpgeN8ZdIRETVqSZerNbNoQK2x4cYyC9BpewYocH5abosP+vGgGynyxzp+95kSsU3zkwppTKno5UXU5xh0+GDa5C05GDDDGBpYdgnZ4qBGv4EOI0PM65HohUY+OGrQgmJDCewDp7myJxNmizSw3g2L5C+D7EZQdCJA3H24bq0zOLzPoyacp1T6mkU4OAM5mwPnyucssFgCFq4GXSfy7J9ALaisXPECV6zanqW9aq9QCOGdeuGfKFUCL4govTVsUxzh8XJMRJk8HQrjwsnFNZj1rOfWkmaYNVQGLI09PlNC9UoAA=
 
 
 
@@ -167,101 +294,5 @@
         }
 
         private static object Lock = new object();
-
-        [TestMethod]
-        public Task Test2()
-        {//// TODO use this to create a test which runs the safe oncompleted method: https://sharplab.io/#v2:CYLg1APgAgTAjAWAFBQAwAIpwKwG5lqZwB0ASgK4B2ALgJYC2ApsQMID29ADrQDaMBOAZQEA3WgGNGAZ3woAzJhjoW6AN4EFUAByYAbOgAq06gAoAlMnVJ0N9CICG/O/Z7lG6ALyYAnOkqMAd3QAQQD7WmpGYAMAT05Gc2IAcUZqADUXN3NZAF9kZE5+WgdI9ClGFyjFELCIqNj4y2RbTAUASShdAB4sVAA+dBT0zISLa1srFpaoAHY/QL0AdX57Tnj+Hrh+kwAxNjZzM1kWvKRm2yhNHU7N/vQ9g7GWyambKF9O4gARRh57GJMW1QZlYbEoADNaABzcj8RihcKmcEucpHc6vWboABE9ikwHBWOOtlOpxa6Na6FoNAEyMk6A63TY5Gohj6TXGNgZCLqGwMAyG3Mi/GyyFJtnJlzKFT4wGqnWWq3WXT56BA9JufPZLUKxXspTh9mAYJ4MT0yoG1FxAGtZBLNLoFWsBCYNRbrU8JuSWtQABa0KTES1SK2edBBm3ksU2O3q3SCgTmwapePCj02F4Yub+IKdFOOpV8ky+/2B63JZO1IWHIk2EnkmNUoW09xcysJpkslVqtosIp0cQuABybDo4Ji7C4fDoYK1tgARvsePSpBPOFOqqp0FDUrh0KdyQYk9RSNJyDxTGikFH0DHypVZbA9HmVk7eQNu7m22/ZzYdSV3AaRqUCaT5fom4YpraHIUp+iICPmzqwTy4HWimaZqF6tjFgGEFfqGuFwfwNZ7vk0GSgubBLm0K4cGuqRROSGZTNu1CYembFTJi2GlsGKbENRq7rsAxEnJGpHTAoh5DCeUhnhejEcVxfo4ahX7lsep7niK0H7mRmgACzoAA8pQgn0cALpwEo4hgnQlDkHqtBguhTHespPFWnxJlmZEFk2TQVIOdOlCXqJZx6ZghkAKqUFI9jgow3m0UJlnWbZgWOc5CnQW5JYETyxAxXFCVJZO5kmP5dlBU5IXEXWV5AA=
-            StateMachine stateMachine = new StateMachine();
-            stateMachine.builder = AsyncTaskMethodBuilder.Create();
-            stateMachine.self = this;
-            stateMachine.state = -1;
-            stateMachine.builder.Start(ref stateMachine);
-            return stateMachine.builder.Task;
-        }
-
-        /// <summary>
-        ///
-        /// </summary>
-        /// <remarks>
-        /// This class is a copy of the compiler-generated code for the async test method with the following body:
-        /// ```
-        /// var value = await new AwaitedType().GetValue();
-        /// Assert.AreEqual("asdf", value);
-        /// ```
-        /// 
-        /// It has only been modified to use <see cref="AsyncTaskMethodBuilder.AwaitOnCompleted{TAwaiter, TStateMachine}(ref TAwaiter, ref TStateMachine)"/> instead of <see cref="AsyncTaskMethodBuilder.AwaitUnsafeOnCompleted{TAwaiter, TStateMachine}(ref TAwaiter, ref TStateMachine)"/> in order to get code covereage on the <see cref="TaskAwaiterWrapper{T}.OnCompleted(Action)"/> method.
-        /// </remarks>
-        private sealed class StateMachine : IAsyncStateMachine
-        {
-            public int state;
-
-            public AsyncTaskMethodBuilder builder;
-
-            public TaskWrapperUnitTests? self;
-
-            private string? value;
-
-            private string? s;
-
-            private object? u;
-
-            private void MoveNext()
-            {
-                int num = state;
-                try
-                {
-                    ITaskAwaiter<string> awaiter;
-                    if (num != 0)
-                    {
-                        awaiter = new AwaitedType<string>("asfd").GetValue().GetAwaiter();
-                        if (!awaiter.IsCompleted)
-                        {
-                            num = (state = 0);
-                            u = awaiter;
-                            StateMachine stateMachine = this;
-                            builder.AwaitOnCompleted(ref awaiter, ref stateMachine);
-                            return;
-                        }
-                    }
-                    else
-                    {
-                        awaiter = ((ITaskAwaiter<string>)u!); //// TODO you addeed the bang, is that ok?
-                        u = null;
-                        num = (state = -1);
-                    }
-                    s = awaiter.GetResult();
-                    value = s;
-                    s = null;
-                    Assert.AreEqual("asdf", value);
-                }
-                catch (Exception exception)
-                {
-                    state = -2;
-                    value = null;
-                    builder.SetException(exception);
-                    return;
-                }
-                state = -2;
-                value = null;
-                builder.SetResult();
-            }
-
-            void IAsyncStateMachine.MoveNext()
-            {
-                //ILSpy generated this explicit interface implementation from .override directive in MoveNext
-                this.MoveNext();
-            }
-
-            [DebuggerHidden]
-            private void SetStateMachine(IAsyncStateMachine stateMachine)
-            {
-            }
-
-            void IAsyncStateMachine.SetStateMachine(IAsyncStateMachine stateMachine)
-            {
-                //ILSpy generated this explicit interface implementation from .override directive in SetStateMachine
-                this.SetStateMachine(stateMachine);
-            }
-        }
     }
 }
