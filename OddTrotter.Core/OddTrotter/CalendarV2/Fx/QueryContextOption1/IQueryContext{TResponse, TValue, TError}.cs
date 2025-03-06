5a7c195785faf9e9be371b6415cc3383108e82da
ravudet <170215607+ravudet@users.noreply.github.com>
2025-03-06 08:24:24 -0800

    asfd

--- OddTrotter.Core/OddTrotter/CalendarV2/Fx/QueryContextOption1/IQueryContext{TResponse, TValue, TError}.cs
+++ OddTrotter.Core/OddTrotter/CalendarV2/Fx/QueryContextOption1/IQueryContext{TResponse, TValue, TError}.cs
@@ -1,22 +1,11 @@
 ﻿namespace Fx.QueryContextOption1
 {
-    using Stash;
-    using System.Collections.Generic;
-    using System.Runtime.CompilerServices;
     using System.Threading.Tasks;
 
     using Fx.QueryContext;
 
-    public interface IQueryContext<out TResponse, out TValue, out TError> //// TODO you can't get covariance without an interface for queryresultnode
+    public interface IQueryContext<out TResponse, out TValue, out TError>
     {
         ITask<IQueryResult<TResponse, TError>> Evaluate();
-    }
-
-    public static class Play
-    {
-        public static async Task Do(IQueryContext<string, string, System.Exception> context)
-        {
-            await context.Evaluate();
-        }
     }
 }
--- OddTrotter.Core/OddTrotter/CalendarV2/Fx/QueryContextOption1/Mixins/IWhereQueryContextMixin.cs
+++ OddTrotter.Core/OddTrotter/CalendarV2/Fx/QueryContextOption1/Mixins/IWhereQueryContextMixin.cs
@@ -3,6 +3,13 @@
     using System;
     using System.Linq.Expressions;
 
+    /// <summary>
+    /// TODO does this need to implement iquerycontext? i'm not sure if that provides any value to the implementer
+    /// </summary>
+    /// <typeparam name="TResponse"></typeparam>
+    /// <typeparam name="TValue"></typeparam>
+    /// <typeparam name="TError"></typeparam>
+    /// <typeparam name="TQueryContext"></typeparam>
     public interface IWhereQueryContextMixin<TResponse, TValue, TError, TQueryContext> : IQueryContext<TResponse, TValue, TError> where TQueryContext : IQueryContext<TResponse, TValue, TError>
     {
         TQueryContext Where(Expression<Func<TValue, bool>> predicate);
