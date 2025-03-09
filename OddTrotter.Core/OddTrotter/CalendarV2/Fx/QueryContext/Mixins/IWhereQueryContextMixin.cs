63d801087d2c349b298d8d3336e1dd462080e198
ravudet <170215607+ravudet@users.noreply.github.com>
2025-03-09 14:46:03 -0700

    asdf

--- OddTrotter.Core/OddTrotter/CalendarV2/Fx/QueryContext/Mixins/IWhereQueryContextMixin.cs
+++ OddTrotter.Core/OddTrotter/CalendarV2/Fx/QueryContext/Mixins/IWhereQueryContextMixin.cs
@@ -10,7 +10,7 @@
     /// <typeparam name="TValue"></typeparam>
     /// <typeparam name="TError"></typeparam>
     /// <typeparam name="TQueryContext"></typeparam>
-    public interface IWhereQueryContextMixin<TResponse, TValue, TError, TQueryContext> : IQueryContext<TResponse, TValue, TError> where TQueryContext : IQueryContext<TResponse, TValue, TError> //// TODO you definitely need better names to differentiate tresponse and tvalue
+    public interface IWhereQueryContextMixin<TResponse, TValue, TError, TQueryContext> : IQueryContext<TResponse, TValue, TError> where TQueryContext : IQueryContext<TResponse, TValue, TError>
     {
         TQueryContext Where(Expression<Func<TValue, bool>> predicate);
     }
