fbc840d4d219c2a38549851b0d9ca1c17344c150
ravudet <170215607+ravudet@users.noreply.github.com>
2025-03-13 10:31:00 -0700

    asdf

--- OddTrotter.MemoryCachingInteractionLibrary/OddTrotter/TodoList/TodoListService.cs
+++ OddTrotter.MemoryCachingInteractionLibrary/OddTrotter/TodoList/TodoListService.cs
@@ -390,6 +390,7 @@
             //// TODO then move argumentnullinline
             //// TODO then do querycontext (and move everything to use it); the query context stuff will need some diligence and effort around adding the third type parameter that allows mixins to work correctly; do query contexts next probably and just know that you're going to need to put the effort in
             //// TODO add obsolete attribute to the implicit operator on either so you get a build warning if you actually use it; is there a way to make *this specific one* a warning instead of an error?
+            //// TODO write tests that assert the deferred execution of query results
             //// TODO do a pass cleaning up the existing code using the code quality above and either addressing todos or marking them TODO FUTURE and TODO TOPIC
 
             //// TODO have a thing to set vacation day messages
