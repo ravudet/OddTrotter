0298cf0bfecad6059057719c9d3c36ae1d9f25df
ravudet <170215607+ravudet@users.noreply.github.com>
2025-03-14 08:15:19 -0700

    asdf

---
+++ OddTrotter.Core.Internals.Tests/CalendarV2/System/ArgumentNullInlineUnitTests.cs
@@ -0,0 +1,42 @@
+﻿namespace System
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public sealed class ArgumentNullInlineUnitTests
+    {
+        [TestMethod]
+        public void ThrowIfNullNullClass()
+        {
+            MockClass @class =
+#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
+                null
+#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
+                ;
+
+            Assert.ThrowsException<ArgumentNullException>(() => ArgumentNullInline.ThrowIfNull(@class));
+        }
+
+        private sealed class MockClass
+        {
+        }
+
+        [TestMethod]
+        public void ThrowIfNullNullNullableClass()
+        {
+            MockClass? @class = null;
+
+            Assert.ThrowsException<ArgumentNullException>(() => ArgumentNullInline.ThrowIfNull(@class));
+        }
+
+        [TestMethod]
+        public void ThrowIfNullNonNullClass()
+        {
+            var @class = new MockClass();
+
+            var returned = ArgumentNullInline.ThrowIfNull(@class);
+
+            Assert.AreEqual(@class, returned);
+        }
+    }
+}
