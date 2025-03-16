03ebddc15cdc26047a22a76915ba29f7d9855b79
ravudet <170215607+ravudet@users.noreply.github.com>
2025-03-15 17:09:14 -0700

    asdf

--- OddTrotter.Core.Internals.Tests/CalendarV2/System/ArgumentNullInlineUnitTests.cs
+++ OddTrotter.Core.Internals.Tests/CalendarV2/System/ArgumentNullInlineUnitTests.cs
@@ -22,6 +22,16 @@
         }
 
         [TestMethod]
+        public void ThrowIfNullNonNullClass()
+        {
+            var @class = new MockClass();
+
+            var returned = ArgumentNullInline.ThrowIfNull(@class);
+
+            Assert.AreEqual(@class, returned);
+        }
+
+        [TestMethod]
         public void ThrowIfNullNullNullableClass()
         {
             MockClass? @class = null;
@@ -30,13 +40,41 @@
         }
 
         [TestMethod]
-        public void ThrowIfNullNonNullClass()
+        public void ThrowIfNullNonNullNullableClass()
         {
-            var @class = new MockClass();
+            MockClass? @class = new MockClass();
 
             var returned = ArgumentNullInline.ThrowIfNull(@class);
 
             Assert.AreEqual(@class, returned);
+        }
+
+        [TestMethod]
+        public void ThrowIfNullNullStruct()
+        {
+            MockStruct? @struct = null;
+
+            Assert.ThrowsException<ArgumentNullException>(() => ArgumentNullInline.ThrowIfNull(@struct));
+        }
+
+        private readonly struct MockStruct
+        {
+            public MockStruct(int value)
+            {
+                Value = value;
+            }
+
+            public int Value { get; }
+        }
+
+        [TestMethod]
+        public void ThrowIfNullNonNullStruct()
+        {
+            MockStruct? @struct = new MockStruct(42);
+
+            var returned = ArgumentNullInline.ThrowIfNull(@struct);
+
+            Assert.AreEqual(@struct, returned);
         }
     }
 }
