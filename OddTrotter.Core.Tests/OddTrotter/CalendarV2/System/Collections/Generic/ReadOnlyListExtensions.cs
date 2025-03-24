fb097f2c01ed57ba3b1532927251cb8fa6a8ac20
ravudet <170215607+ravudet@users.noreply.github.com>
2025-03-24 16:44:16 -0700

    asedf

--- OddTrotter.Core.Tests/OddTrotter/CalendarV2/System/Collections/Generic/ReadOnlyListExtensions.cs
+++ OddTrotter.Core.Tests/OddTrotter/CalendarV2/System/Collections/Generic/ReadOnlyListExtensions.cs
@@ -7,6 +7,13 @@ namespace System.Collections.Generic
 
     internal static class ReadOnlyListExtensions
     {
+        /// <summary>
+        /// placeholder
+        /// </summary>
+        /// <typeparam name="TValue"></typeparam>
+        /// <param name="list"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="list"/> is <see langword="null"/></exception>
         public static ToQueryResultBuilder<TValue> ToQueryResult<TValue>(this IReadOnlyList<TValue> list)
         {
             ArgumentNullException.ThrowIfNull(list);
@@ -20,12 +27,23 @@ namespace System.Collections.Generic
 
             private readonly bool isInitialized;
 
+            /// <summary>
+            /// placeholder
+            /// </summary>
+            /// <exception cref="InvalidOperationException">
+            /// Always thrown; a default instance of <see cref="ToQueryResultBuilder{TValue}"/> is invalid
+            /// </exception>
             public ToQueryResultBuilder()
             {
                 throw new InvalidOperationException(
                     $"Initializing a default instance of '{typeof(ToQueryResultBuilder<TValue>).FullName}' results in an invalid state.");
             }
 
+            /// <summary>
+            /// placeholder
+            /// </summary>
+            /// <param name="list"></param>
+            /// <exception cref="ArgumentNullException">Thrown if <paramref name="list"/> is <see langword="null"/></exception>
             public ToQueryResultBuilder(IReadOnlyList<TValue> list)
             {
                 ArgumentNullException.ThrowIfNull(list);
@@ -35,6 +53,15 @@ namespace System.Collections.Generic
                 isInitialized = true;
             }
 
+            /// <summary>
+            /// placeholder
+            /// </summary>
+            /// <typeparam name="TError"></typeparam>
+            /// <param name="error"></param>
+            /// <returns></returns>
+            /// <exception cref="InvalidOperationException">
+            /// Thrown if this instance of <see cref="ToQueryResultBuilder{TValue}"/> is a default instance
+            /// </exception>
             public IQueryResult<TValue, TError> WithError<TError>(TError error)
             {
                 if (!isInitialized)
@@ -43,9 +70,17 @@ namespace System.Collections.Generic
                         $"This instance of '{typeof(ToQueryResultBuilder<TValue>).FullName}' was initialized as a default instance and is in an invalid state.");
                 }
 
-                return ToQueryResult(list, new RealNullable<TError>(error));
+                return ToQueryResult(this.list, new RealNullable<TError>(error));
             }
 
+            /// <summary>
+            /// placeholder
+            /// </summary>
+            /// <typeparam name="TError"></typeparam>
+            /// <returns></returns>
+            /// <exception cref="InvalidOperationException">
+            /// Thrown if this instance of <see cref="ToQueryResultBuilder{TValue}"/> is a default instance
+            /// </exception>
             public IQueryResult<TValue, TError> WithoutError<TError>()
             {
                 if (!isInitialized)
@@ -58,6 +93,15 @@ namespace System.Collections.Generic
             }
         }
 
+        /// <summary>
+        /// placeholder
+        /// </summary>
+        /// <typeparam name="TValue"></typeparam>
+        /// <typeparam name="TError"></typeparam>
+        /// <param name="list"></param>
+        /// <param name="error"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="list"/> is <see langword="null"/></exception>
         private static IQueryResult<TValue, TError> ToQueryResult<TValue, TError>(
             IReadOnlyList<TValue> list,
             RealNullable<TError> error)
@@ -69,6 +113,11 @@ namespace System.Collections.Generic
 
         private sealed class ToQueryResultQueryResult<TValue, TError> : IQueryResult<TValue, TError>
         {
+            /// <summary>
+            /// placeholder
+            /// </summary>
+            /// <param name="nodes"></param>
+            /// <exception cref="ArgumentNullException">Thrown if <paramref name="nodes"/> is <see langword="null"/></exception>
             public ToQueryResultQueryResult(IQueryResultNode<TValue, TError> nodes)
             {
                 ArgumentNullException.ThrowIfNull(nodes);
@@ -79,6 +128,17 @@ namespace System.Collections.Generic
             public IQueryResultNode<TValue, TError> Nodes { get; }
         }
 
+        /// <summary>
+        /// placeholder
+        /// </summary>
+        /// <typeparam name="TValue"></typeparam>
+        /// <typeparam name="TError"></typeparam>
+        /// <param name="list"></param>
+        /// <param name="index"></param>
+        /// <param name="possibleError"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="list"/> is <see langword="null"/></exception>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown if <paramref name="index"/> is negative or greater than the <see cref="IReadOnlyCollection{T}.Count"/> of <paramref name="list"/></exception>
         private static IQueryResultNode<TValue, TError> ToQueryResultNode<TValue, TError>(IReadOnlyList<TValue> list, int index, RealNullable<TError> possibleError)
         {
             ArgumentNullException.ThrowIfNull(list);
@@ -137,6 +197,14 @@ namespace System.Collections.Generic
 
             private readonly RealNullable<TError> possibleError;
 
+            /// <summary>
+            /// placeholder
+            /// </summary>
+            /// <param name="list"></param>
+            /// <param name="index"></param>
+            /// <param name="possibleError"></param>
+            /// <exception cref="ArgumentNullException">Thrown if <paramref name="list"/> is <see langword="null"/></exception>
+            /// <exception cref="ArgumentOutOfRangeException">Thrown if <paramref name="index"/> is negative or greater than or equal to the <see cref="IReadOnlyCollection{T}.Count"/> of <paramref name="list"/></exception>
             public ToQueryResultNodeElement(IReadOnlyList<TValue> list, int index, RealNullable<TError> possibleError)
             {
                 ArgumentNullException.ThrowIfNull(list);
