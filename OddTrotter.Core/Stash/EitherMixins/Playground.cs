63786ef8536c9d41c078f0105c54fc64e0d80a00
ravudet <170215607+ravudet@users.noreply.github.com>
2025-03-31 07:57:43 -0700

    asdf

--- OddTrotter.Core/Stash/EitherMixins/Playground.cs
+++ OddTrotter.Core/Stash/EitherMixins/Playground.cs
@@ -13,12 +13,59 @@
             TContext context);
     }
 
-    public interface IAsyncEither<out TLeft, out TRight> //// TODO should the name be the other way around?
+    public interface IAsyncEither<out TLeft, out TRight>
     {
         Task<TResult> ApplyAsync<TResult, TContext>(
             Func<TLeft, TContext, Task<TResult>> leftMap,
             Func<TRight, TContext, Task<TResult>> rightMap,
             TContext context);
+
+        IAsyncEitherFactory Factory { get; }
+    }
+
+    public interface IAsyncEitherFactory
+    {
+        Fx.Either.IEither<TLeft, TRight> CreateLeft<TLeft, TRight>(TLeft left);
+
+        Fx.Either.IEither<TLeft, TRight> CreateRight<TLeft, TRight>(TRight right);
+    }
+
+    public sealed class AsyncEither<TLeft, TRight> : IAsyncEither<TLeft, TRight>, Fx.Either.IEither<TLeft, TRight>
+    {
+        public AsyncEither(TLeft left)
+        {
+        }
+
+        public AsyncEither(TRight right)
+        {
+        }
+
+        public IAsyncEitherFactory Factory { get; } = new AsyncEitherFactory(); //// TODO you should almost certainly do this in the "basic" either interface as well
+
+        private sealed class AsyncEitherFactory : IAsyncEitherFactory
+        {
+            public Fx.Either.IEither<TLeft1, TRight1> CreateLeft<TLeft1, TRight1>(TLeft1 left)
+            {
+                //// TODO if the implementer doesn't care about subsequent calls, they can just do:
+                return Either.Left(left).Right<TRight1>();
+                ////return new AsyncEither<TLeft1, TRight1>(left);
+            }
+
+            public Fx.Either.IEither<TLeft1, TRight1> CreateRight<TLeft1, TRight1>(TRight1 right)
+            {
+                return new AsyncEither<TLeft1, TRight1>(right);
+            }
+        }
+
+        public Task<TResult> ApplyAsync<TResult, TContext>(Func<TLeft, TContext, Task<TResult>> leftMap, Func<TRight, TContext, Task<TResult>> rightMap, TContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TResult Apply<TResult, TContext>(Func<TLeft, TContext, TResult> leftMap, Func<TRight, TContext, TResult> rightMap, TContext context)
+        {
+            throw new NotImplementedException();
+        }
     }
 
     public static class EitherExtensions
@@ -30,8 +77,6 @@
         {
             if (either is IAsyncEither<TLeftOld, TRightOld> asyncEither)
             {
-                //// TODO two issues (maybe more?)
-                //// 2. the type of `either` is not preserved in the return type
                 return await asyncEither
                     .SelectAsync(
                         leftSelector,
@@ -54,22 +99,16 @@
         public static async Task<Fx.Either.IEither<TLeftNew, TRightNew>> SelectAsync<TLeftOld, TRightOld, TLeftNew, TRightNew>(
             this IAsyncEither<TLeftOld, TRightOld> either,
             Func<TLeftOld, Task<TLeftNew>> leftSelector,
-            Func<TRightOld, Task<TRightNew>> rightSelector)
+            Func<TRightOld, Task<TRightNew>> rightSelector) //// TODO this should probably return an iasynceither
         {
             //// TODO are you ok with this? you now need to re-implement all of the either extensions, but for async, *and* you need to implement the above "adapter" to async...and you'll need to do this for all "core" either variants
             //// TODO i think trying this for "allows ref struct" will show if this is even feasible as a general pattern
             return await either
                     .ApplyAsync(
                         async (left, context) =>
-                            Either
-                                .Left(
-                                    await leftSelector(left).ConfigureAwait(false))
-                                .Right<TRightNew>(),
+                            either.Factory.CreateLeft<TLeftNew, TRightNew>(await leftSelector(left).ConfigureAwait(false)),
                         async (right, context) =>
-                            Either
-                                .Left<TLeftNew>()
-                                .Right(
-                                    await rightSelector(right).ConfigureAwait(false)),
+                            either.Factory.CreateRight<TLeftNew, TRightNew>(await rightSelector(right).ConfigureAwait(false)),
                         new Nothing())
                     .ConfigureAwait(false);
         }
