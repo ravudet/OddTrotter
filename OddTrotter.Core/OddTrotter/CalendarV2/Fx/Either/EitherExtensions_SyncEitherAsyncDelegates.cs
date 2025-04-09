d619d304e39e50a717a3025643a1a1753b0c6292
ravudet <170215607+ravudet@users.noreply.github.com>
2025-04-09 07:17:35 -0700

    asdf

--- OddTrotter.Core/OddTrotter/CalendarV2/Fx/Either/EitherExtensions_SyncEitherAsyncDelegates.cs
+++ OddTrotter.Core/OddTrotter/CalendarV2/Fx/Either/EitherExtensions_SyncEitherAsyncDelegates.cs
@@ -1,11 +1,12 @@
 ﻿namespace Fx.Either
 {
     using System;
+    using System.Runtime.CompilerServices;
     using System.Threading.Tasks;
 
     public static partial class EitherExtensions
     {
-        public static async Task<Either<TLeftResult, TRightResult>> SelectAsync //// TODO should return `ieither`
+        public static async ValueTask<Either<TLeftResult, TRightResult>> SelectAsync //// TODO should return `ieither`
             <
                 TLeftValue,
                 TRightValue,
@@ -38,6 +39,64 @@
                     },
                     new Nothing())
                 .ConfigureAwait(false);
+        }
+
+        public static async TaskLike<string> FooAsync()
+        {
+            await Task.Yield();
+            await default(TaskLike<string>);
+            return "Asdf";
+        }
+
+        public sealed class TaskLikeMethodBuilder<T>
+        {
+            public TaskLikeMethodBuilder()
+                => Console.WriteLine(".ctor");
+
+            public static TaskLikeMethodBuilder<T> Create()
+                => new TaskLikeMethodBuilder<T>();
+
+            public void Start<TStateMachine>(ref TStateMachine stateMachine)
+                where TStateMachine : IAsyncStateMachine
+            {
+                Console.WriteLine("Start");
+                stateMachine.MoveNext();
+            }
+
+            public void SetStateMachine(IAsyncStateMachine stateMachine) => throw new Exception("TODO");
+
+            public void SetException(Exception exception) => throw new Exception("TODO");
+
+            public void SetResult(T result) => throw new Exception("TODO");
+
+            public void AwaitOnCompleted<TAwaiter, TStateMachine>(
+        ref TAwaiter awaiter, ref TStateMachine stateMachine)
+        where TAwaiter : INotifyCompletion
+        where TStateMachine : IAsyncStateMachine
+                => throw new Exception("TODO");
+
+            public void AwaitUnsafeOnCompleted<TAwaiter, TStateMachine>(
+                ref TAwaiter awaiter, ref TStateMachine stateMachine)
+                where TAwaiter : ICriticalNotifyCompletion
+                where TStateMachine : IAsyncStateMachine
+                 => throw new Exception("TODO");
+
+            public TaskLike<T> Task => default(TaskLike<T>);
+        }
+
+        [System.Runtime.CompilerServices.AsyncMethodBuilder(typeof(TaskLikeMethodBuilder<>))]
+        public struct TaskLike<T>
+        {
+            public TaskLikeAwaiter GetAwaiter() => default(TaskLikeAwaiter);
+        }
+
+        public struct TaskLikeAwaiter : INotifyCompletion
+        {
+            public void GetResult() { }
+
+            public bool IsCompleted => true;
+
+            public void OnCompleted(Action continuation) { }
         }
     }
 }
