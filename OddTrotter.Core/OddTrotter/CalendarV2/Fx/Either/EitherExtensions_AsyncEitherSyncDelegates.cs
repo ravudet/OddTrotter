0101c3c24dcdd6c3d2e073eaa45d4e3d9847d21e
ravudet <170215607+ravudet@users.noreply.github.com>
2025-04-10 16:20:38 -0700

    asdf

--- OddTrotter.Core/OddTrotter/CalendarV2/Fx/Either/EitherExtensions_AsyncEitherSyncDelegates.cs
+++ OddTrotter.Core/OddTrotter/CalendarV2/Fx/Either/EitherExtensions_AsyncEitherSyncDelegates.cs
@@ -10,6 +10,28 @@ namespace Fx.Either
                 TLeftValue,
                 TRightValue,
                 TLeftResult,
+                TRightResult,
+                TContext
+            >
+            (
+                this Task<IEither<TLeftValue, TRightValue>> either,
+                Func<TLeftValue, TContext, TLeftResult> leftSelector,
+                Func<TRightValue, TContext, TRightResult> rightSelector,
+                TContext context
+            )
+        {
+            ArgumentNullException.ThrowIfNull(either);
+            ArgumentNullException.ThrowIfNull(leftSelector);
+            ArgumentNullException.ThrowIfNull(rightSelector);
+
+            return (await either.ConfigureAwait(false)).Select(leftSelector, rightSelector, context);
+        }
+
+        public static async Task<IEither<TLeftResult, TRightResult>> Select
+            <
+                TLeftValue,
+                TRightValue,
+                TLeftResult,
                 TRightResult
             >
             (
@@ -18,7 +40,28 @@ namespace Fx.Either
                 Func<TRightValue, TRightResult> rightSelector
             )
         {
+            ArgumentNullException.ThrowIfNull(either);
+            ArgumentNullException.ThrowIfNull(leftSelector);
+            ArgumentNullException.ThrowIfNull(rightSelector);
+
             return (await either.ConfigureAwait(false)).Select(leftSelector, rightSelector);
+        }
+
+        public static async Task<IEither<TLeftResult, TRightValue>> SelectLeft
+            <
+                TLeftValue,
+                TRightValue,
+                TLeftResult
+            >
+            (
+                this Task<IEither<TLeftValue, TRightValue>> either,
+                Func<TLeftValue, TLeftResult> leftSelector
+            )
+        {
+            ArgumentNullException.ThrowIfNull(either);
+            ArgumentNullException.ThrowIfNull(leftSelector);
+
+            return (await either.ConfigureAwait(false)).SelectLeft(leftSelector);
         }
     }
 }
