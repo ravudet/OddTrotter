df3de1082fb7833d25a88f19e59c230883c80e3a
ravudet <170215607+ravudet@users.noreply.github.com>
2025-04-14 08:23:13 -0700

    asdf

--- OddTrotter.Core/OddTrotter/CalendarV2/Fx/Either/EitherExtensions_SyncEitherAsyncDelegates.cs
+++ OddTrotter.Core/OddTrotter/CalendarV2/Fx/Either/EitherExtensions_SyncEitherAsyncDelegates.cs
@@ -19,7 +19,10 @@ namespace Fx.Either
         /// <param name="rightSelector"></param>
         /// <param name="context"></param>
         /// <returns></returns>
-        /// <exception cref="ArgumentNullException">Thrown if <paramref name="either"/> or <paramref name="leftSelector"/> or <paramref name="rightSelector"/> is <see langword="null"/></exception>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown if <paramref name="either"/> or <paramref name="leftSelector"/> or <paramref name="rightSelector"/> is 
+        /// <see langword="null"/>
+        /// </exception>
         /// <exception cref="LeftMapException">
         /// Thrown if <paramref name="leftSelector"/> throws an exception. The <see cref="Exception.InnerException"/> will be set
         /// to whatever exception <paramref name="leftSelector"/> threw.
@@ -72,7 +75,10 @@ namespace Fx.Either
         /// <param name="leftSelector"></param>
         /// <param name="rightSelector"></param>
         /// <returns></returns>
-        /// <exception cref="ArgumentNullException">Thrown if <paramref name="either"/> or <paramref name="leftSelector"/> or <paramref name="rightSelector"/> is <see langword="null"/></exception>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown if <paramref name="either"/> or <paramref name="leftSelector"/> or <paramref name="rightSelector"/> is
+        /// <see langword="null"/>
+        /// </exception>
         /// <exception cref="LeftMapException">
         /// Thrown if <paramref name="leftSelector"/> throws an exception. The <see cref="Exception.InnerException"/> will be set
         /// to whatever exception <paramref name="leftSelector"/> threw.
@@ -113,7 +119,9 @@ namespace Fx.Either
         /// <param name="either"></param>
         /// <param name="leftSelector"></param>
         /// <returns></returns>
-        /// <exception cref="ArgumentNullException">Thrown if <paramref name="either"/> or <paramref name="leftSelector"/> is <see langword="null"/></exception>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown if <paramref name="either"/> or <paramref name="leftSelector"/> is <see langword="null"/>
+        /// </exception>
         /// <exception cref="LeftMapException">
         /// Thrown if <paramref name="leftSelector"/> throws an exception. The <see cref="Exception.InnerException"/> will be set
         /// to whatever exception <paramref name="leftSelector"/> threw.
