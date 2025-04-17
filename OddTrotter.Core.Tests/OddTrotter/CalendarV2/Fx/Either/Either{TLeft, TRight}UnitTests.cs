e793b733583882d898b1de6a49a7e20a97341c18
ravudet <170215607+ravudet@users.noreply.github.com>
2025-04-17 08:26:09 -0700

    asdf

--- OddTrotter.Core.Tests/OddTrotter/CalendarV2/Fx/Either/Either{TLeft, TRight}UnitTests.cs
+++ OddTrotter.Core.Tests/OddTrotter/CalendarV2/Fx/Either/Either{TLeft, TRight}UnitTests.cs
@@ -2,7 +2,7 @@
 namespace Fx.Either
 {
     using System;
-
+    using System.Threading.Tasks;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
 
     [TestClass]
@@ -219,6 +219,190 @@ namespace Fx.Either
         }
 
         [TestMethod]
+        public async Task VisitAsyncNullNode()
+        {
+            await Assert
+                .ThrowsExceptionAsync<ArgumentNullException>(
+                    async () => await MockAsyncVisitor
+                        .Instance
+                        .VisitAsync(
+#pragma warning disable CS8625 // Cannot convert null literal to non-nullable reference type.
+                            null
+#pragma warning restore CS8625 // Cannot convert null literal to non-nullable reference type.
+                            , default)
+                        .ConfigureAwait(false))
+                .ConfigureAwait(false);
+        }
+
+        [TestMethod]
+        public void LeftAccept()
+        {
+            Either<string, int> either = new Either<string, int>.Left("asdf");
+
+            var result = MockVisitor.Instance.Visit(either, default);
+
+            Assert.AreEqual('a', result);
+        }
+
+        [TestMethod]
+        public void RightAccept()
+        {
+            Either<string, int> either = new Either<string, int>.Right(42);
+
+            var result = MockVisitor.Instance.Visit(either, default);
+
+            Assert.AreEqual('4', result);
+        }
+
+        private sealed class MockAsyncVisitor : Either<string, int>.AsyncVisitor<char, Nothing>
+        {
+            /// <summary>
+            /// 
+            /// </summary>
+            private MockAsyncVisitor()
+            {
+            }
+
+            /// <summary>
+            /// 
+            /// </summary>
+            public static MockAsyncVisitor Instance { get; } = new MockAsyncVisitor();
+
+            protected override async Task<char> AcceptAsync(Either<string, int>.Left node, Nothing context)
+            {
+                ArgumentNullException.ThrowIfNull(node);
+
+                return await Task.FromResult(node.Value[0]).ConfigureAwait(false);
+            }
+
+            protected override async Task<char> AcceptAsync(Either<string, int>.Right node, Nothing context)
+            {
+                ArgumentNullException.ThrowIfNull(node);
+
+                return await Task.FromResult(node.Value.ToString()[0]).ConfigureAwait(false);
+            }
+        }
+
+        /*[TestMethod]
+        public void VisitNullContext()
+        {
+            MockVisitNullContextVisitor.Instance.Visit(new Either<string, int>.Left("sadf"),
+#pragma warning disable CS8625 // Cannot convert null literal to non-nullable reference type.
+                null
+#pragma warning restore CS8625 // Cannot convert null literal to non-nullable reference type.
+                );
+        }
+
+        private sealed class MockVisitNullContextVisitor : Either<string, int>.Visitor<char, object>
+        {
+            /// <summary>
+            /// 
+            /// </summary>
+            private MockVisitNullContextVisitor()
+            {
+            }
+
+            /// <summary>
+            /// 
+            /// </summary>
+            public static MockVisitNullContextVisitor Instance { get; } = new MockVisitNullContextVisitor();
+
+            /// <inheritdoc/>
+            protected sealed override char Accept(Either<string, int>.Left node, object context)
+            {
+                ArgumentNullException.ThrowIfNull(node);
+
+                return node.Value[0];
+            }
+
+            /// <inheritdoc/>
+            protected sealed override char Accept(Either<string, int>.Right node, object context)
+            {
+                ArgumentNullException.ThrowIfNull(node);
+
+                return node.Value.ToString()[0];
+            }
+        }
+
+        [TestMethod]
+        public void ApplyLeft()
+        {
+            Either<string, int> either = new Either<string, int>.Left("asdf");
+
+            var result = either.Apply((left, context) => left[0], (right, context) => right.ToString()[0], new Nothing());
+
+            Assert.AreEqual('a', result);
+        }
+
+        [TestMethod]
+        public void ApplyRight()
+        {
+            Either<string, int> either = new Either<string, int>.Right(42);
+
+            var result = either.Apply((left, context) => left[0], (right, context) => right.ToString()[0], new Nothing());
+
+            Assert.AreEqual('4', result);
+        }
+
+        [TestMethod]
+        public void ApplyLeftException()
+        {
+            Either<string, int> either = new Either<string, int>.Left("asdf");
+
+            var invalidOperationException = new InvalidOperationException();
+            var invalidCastException = new InvalidCastException();
+
+            var leftMapException = Assert.ThrowsException<LeftMapException>(
+                () => either.Apply<char, Nothing>(
+                    (left, context) => throw invalidOperationException,
+                    (right, context) => throw invalidCastException,
+                    default));
+
+            Assert.AreEqual(invalidOperationException, leftMapException.InnerException);
+        }
+
+        [TestMethod]
+        public void ApplyRightException()
+        {
+            Either<string, int> either = new Either<string, int>.Right(42);
+
+            var invalidOperationException = new InvalidOperationException();
+            var invalidCastException = new InvalidCastException();
+
+            var rightMapException = Assert.ThrowsException<RightMapException>(
+                () => either.Apply<char, Nothing>(
+                    (left, context) => throw invalidOperationException,
+                    (right, context) => throw invalidCastException,
+                    default));
+
+            Assert.AreEqual(invalidCastException, rightMapException.InnerException);
+        }
+
+        [TestMethod]
+        public void ApplyNullLeftMap()
+        {
+            Either<string, int> either = new Either<string, int>.Left("asdf");
+
+            Assert.ThrowsException<ArgumentNullException>(() => either.Apply<Nothing, Nothing>(
+#pragma warning disable CS8625 // Cannot convert null literal to non-nullable reference type.
+                null,
+#pragma warning restore CS8625 // Cannot convert null literal to non-nullable reference type.
+                (right, context) => default, default));
+        }
+
+        [TestMethod]
+        public void ApplyNullRightMap()
+        {
+            Either<string, int> either = new Either<string, int>.Left("asdf");
+
+            Assert.ThrowsException<ArgumentNullException>(() => either.Apply<Nothing, Nothing>((left, context) => default,
+#pragma warning disable CS8625 // Cannot convert null literal to non-nullable reference type.
+                null,
+#pragma warning restore CS8625 // Cannot convert null literal to non-nullable reference type.
+                default));
+        }
+
+        [TestMethod]
         public void ImplicitLeft()
         {
             Either<string, int> either =
@@ -244,6 +428,6 @@ namespace Fx.Either
             var result = MockVisitor.Instance.Visit(either, default);
 
             Assert.AreEqual('4', result);
-        }
+        }*/
     }
 }
