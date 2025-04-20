3f7442379e7f86c87075b39d3c6b7a27ca703010
ravudet <170215607+ravudet@users.noreply.github.com>
2025-04-19 18:43:48 -0700

    asdf

--- OddTrotter.MemoryCachingInteractionLibrary/OddTrotter/TodoList/TodoListService.cs
+++ OddTrotter.MemoryCachingInteractionLibrary/OddTrotter/TodoList/TodoListService.cs
@@ -427,6 +427,7 @@
             //// TODO is anything using the implicit conversions in either? if nothing is using them at this point, you should just remove them
             //// TODO write tests for the toqueryresult extension on ireadonlylist; then, update all tests to use this extension rather than building the queryresults themselves
 
+            //// TODO FUTURE is there a way to add tests for the `either.applyasync` (and others that leverage it like `queryresultnode` and `firstordefault`) that ensure that `applyasync` is called rather than `apply`?
             //// TODO FUTURE if you move `ieither` to fx.core, you need to be open to having a v2 in order to get it "right" how you handle all of the different `apply` variants (including ones that you don't know about yet)
             //// TODO future use a newer version of .net and update argumentoutofrangeexception checks
             //// TODO FUTURE add more query context mixins
