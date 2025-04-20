a8effb7ee6e81353f5d94d3296209efab7598458
ravudet <170215607+ravudet@users.noreply.github.com>
2025-04-19 18:52:28 -0700

    asdf

--- OddTrotter.MemoryCachingInteractionLibrary/OddTrotter/TodoList/TodoListService.cs
+++ OddTrotter.MemoryCachingInteractionLibrary/OddTrotter/TodoList/TodoListService.cs
@@ -404,9 +404,6 @@
             //// TODO https://github.com/dotnet/roslyn/blob/main/docs/features/task-types.md
             //// TODO https://learn.microsoft.com/en-us/dotnet/csharp/asynchronous-programming/async-return-types
             
-            //// TODO you are code reviewing adding applyasync to ieither; you just finished code reviewing queryresultnode
-            //// TODO rename eitherextensionunittests
-            //// TODO you need to code review from the commit about starting async either implementation
             //// TODO you have written a "task method builder" for `itask`; it's in stash
             //// TODO do a pass cleaning up the existing code using the code quality above and either addressing todos or marking them TODO FUTURE and TODO TOPIC
 
