ccfb88b39d1c60e46c0a48c08f97497d257a402a
ravudet <170215607+ravudet@users.noreply.github.com>
2025-04-25 08:25:39 -0700

    asdf

--- OddTrotter.Core/OddTrotter/CalendarV2/Fx/QueryContext/QueryResultExtensions_AsyncQueryResultSyncDelegates.cs
+++ OddTrotter.Core/OddTrotter/CalendarV2/Fx/QueryContext/QueryResultExtensions_AsyncQueryResultSyncDelegates.cs
@@ -8,6 +8,16 @@ namespace Fx.QueryContext
 
     public static partial class QueryResultExtensions
     {
+        /// <summary>
+        /// placeholder
+        /// </summary>
+        /// <typeparam name="TValueSource"></typeparam>
+        /// <typeparam name="TError"></typeparam>
+        /// <typeparam name="TValueResult"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="selector"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="source"/> or <paramref name="selector"/> is <see langword="null"/></exception>
         public static async Task<IQueryResult<TValueResult, TError>> Select<TValueSource, TError, TValueResult>(
             this Task<IQueryResult<TValueSource, TError>> source,
             Func<TValueSource, TValueResult> selector)
@@ -18,6 +28,16 @@ namespace Fx.QueryContext
             return (await source.ConfigureAwait(false)).Select(selector);
         }
 
+        /// <summary>
+        /// placeholder
+        /// </summary>
+        /// <typeparam name="TValue"></typeparam>
+        /// <typeparam name="TError"></typeparam>
+        /// <typeparam name="TResult"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="try"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="source"/> or <paramref name="try"/> is <see langword="null"/></exception>
         public static async Task<IQueryResult<TResult, TError>> TrySelect<TValue, TError, TResult>(
             this Task<IQueryResult<TValue, TError>> source,
             Try<TValue, TResult> @try)
