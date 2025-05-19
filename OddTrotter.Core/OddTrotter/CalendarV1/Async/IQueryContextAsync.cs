833534a004fe89758e6dfdb18341c2002537c96c
ravudet <170215607+ravudet@users.noreply.github.com>
2025-05-19 07:58:45 -0700

    asdf

--- OddTrotter.Core/OddTrotter/CalendarV1/Async/IQueryContextAsync.cs
+++ OddTrotter.Core/OddTrotter/CalendarV1/Async/IQueryContextAsync.cs
@@ -3,12 +3,12 @@ namespace Fx.QueryContext
 {
     using System.Threading.Tasks;
 
-    public interface IQueryContext<out TClientValue, out TDataStoreValue, out TError>
+    public interface IQueryContextAsync<out TClientValue, out TDataStoreValue, out TError>
     {
         /// <summary>
         /// placeholder
         /// </summary>
         /// <returns></returns>
-        ITask<IQueryResult<TClientValue, TError>> Evaluate();
+        ITask<IQueryResultAsync<TClientValue, TError>> Evaluate();
     }
 }
