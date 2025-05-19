48f1d9b470c0ab16c20ad89261b9bac61a6282ff
ravudet <170215607+ravudet@users.noreply.github.com>
2025-05-19 08:02:40 -0700

    asdf

--- OddTrotter.MemoryCachingInteractionLibrary/OddTrotter/TodoList/TodoListService.cs
+++ OddTrotter.MemoryCachingInteractionLibrary/OddTrotter/TodoList/TodoListService.cs
@@ -423,7 +423,6 @@
             //// TODO https://github.com/dotnet/roslyn/blob/main/docs/features/task-types.md
             //// TODO https://learn.microsoft.com/en-us/dotnet/csharp/asynchronous-programming/async-return-types
 
-            //// TODO move everything that's async from 61cd991917ba2ed30745d2be82ab5c18ed9f2d3a to v1
             //// TODO code review everything that's *not* async
             //// TODO start using feature branches
             //// TODO move the async stuff to v2 "correctly"
