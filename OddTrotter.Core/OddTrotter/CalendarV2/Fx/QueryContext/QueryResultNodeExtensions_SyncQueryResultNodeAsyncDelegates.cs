27f4693591056857e03551f0963d367b18135633
ravudet <170215607+ravudet@users.noreply.github.com>
2025-05-20 07:35:44 -0700

    asdf

--- OddTrotter.Core/OddTrotter/CalendarV2/Fx/QueryContext/QueryResultNodeExtensions_SyncQueryResultNodeAsyncDelegates.cs
+++ OddTrotter.Core/OddTrotter/CalendarV2/Fx/QueryContext/QueryResultNodeExtensions_SyncQueryResultNodeAsyncDelegates.cs
@@ -1,6 +1,17 @@
 ﻿namespace Fx.QueryContext
 {
+    using System;
+
+    using Fx.Either;
+
     public static partial class QueryResultNodeExtensions
     {
+        public static QueryResultNodeAsync<TValue, TError> ToQueryResultNode<TValue, TError>(
+            this IEither<IElementAsync<TValue, TError>, IEither<IError<TError>, IEmpty>> node)
+        {
+            ArgumentNullException.ThrowIfNull(node);
+
+            return new QueryResultNodeAsync<TValue, TError>(node);
+        }
     }
 }
