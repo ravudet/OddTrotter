443aec0a65b232e46e0a9e696e13513250b9d09b
ravudet <170215607+ravudet@users.noreply.github.com>
2025-05-20 07:36:56 -0700

    asdf

--- OddTrotter.Core/OddTrotter/CalendarV1/Async/QueryResultExtensions_SyncQueryResultAsyncDelegates.cs
+++ OddTrotter.Core/OddTrotter/CalendarV1/Async/QueryResultExtensions_SyncQueryResultAsyncDelegates.cs
@@ -13,26 +13,6 @@ namespace Fx.QueryContext
             return new QueryResultNodeAsync<TValue, TError>(source);
         }
 
-        public sealed class QueryResultNodeAsync<TValue, TError> : IQueryResultNodeAsync<TValue, TError>
-        {
-            private readonly IEither<IElementAsync<TValue, TError>, IEither<IError<TError>, IEmpty>> source;
-
-            public QueryResultNodeAsync(IEither<IElementAsync<TValue, TError>, IEither<IError<TError>, IEmpty>> source)
-            {
-                this.source = source;
-            }
-
-            public TResult Apply<TResult, TContext>(Func<IElementAsync<TValue, TError>, TContext, TResult> leftMap, Func<IEither<IError<TError>, IEmpty>, TContext, TResult> rightMap, TContext context)
-            {
-                return this.source.Apply(leftMap, rightMap, context);
-            }
-
-            public Task<TResult> Apply<TResult, TContext>(Func<IElementAsync<TValue, TError>, TContext, Task<TResult>> leftMap, Func<IEither<IError<TError>, IEmpty>, TContext, Task<TResult>> rightMap, TContext context)
-            {
-                return this.source.Apply(leftMap, rightMap, context);
-            }
-        }
-
         public static IQueryResultAsync<TValueResult, TError> Select<TValueSource, TError, TValueResult>(
             this IQueryResult<TValueSource, TError> source,
             Func<TValueSource, Task<TValueResult>> selector)
