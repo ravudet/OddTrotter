feb8c4dabdd1cc6f3f028bc0c9eba42dd647db67
ravudet <170215607+ravudet@users.noreply.github.com>
2025-05-20 07:16:43 -0700

    asdf

--- OddTrotter.Core/OddTrotter/CalendarV2/Fx/QueryContext/IQueryResultAsync.cs
+++ OddTrotter.Core/OddTrotter/CalendarV2/Fx/QueryContext/IQueryResultAsync.cs
@@ -1,9 +1,14 @@
-﻿namespace Fx.QueryContext
+﻿/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+namespace Fx.QueryContext
 {
     using System.Threading.Tasks;
 
     public interface IQueryResultAsync<out TValue, out TError>
     {
+        /// <summary>
+        /// placeholder
+        /// </summary>
+        /// <returns></returns>
         ITask<IQueryResultNodeAsync<TValue, TError>> GetNodes();
     }
 }
--- OddTrotter.MemoryCachingInteractionLibrary/OddTrotter/TodoList/TodoListService.cs
+++ OddTrotter.MemoryCachingInteractionLibrary/OddTrotter/TodoList/TodoListService.cs
@@ -435,6 +435,7 @@
             //// TODO you have written a "task method builder" for `itask`; it's in stash
             //// TODO do a pass cleaning up the existing code using the code quality above and either addressing todos or marking them TODO FUTURE and TODO TOPIC
 
+            //// TODO move queryresult to its own namespace (probably queryresultnode should go in the same namespace); make a note that you are not moving the async stuff to its own namespace because you don't want someone accidentally using the sync extensions with async delegates
             //// TODO `try`s should really be covariants:
             //// ```
             //// public delegate bool TestTry<in TInput, out TOutput>(TInput input);
