6103a46cd9e0435a6728c1280ba5ef60a08b68aa
ravudet <170215607+ravudet@users.noreply.github.com>
2025-06-05 06:55:20 -0700

    asdf

--- ConsoleApp1/System/Runtime/CompilerServices/IConfiguredAwaitable.cs
+++
@@ -1,9 +0,0 @@
-﻿/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-namespace System.Runtime.CompilerServices
-{
-    public interface IConfiguredAwaitable<out T>
-    {
-        /// <inheritdoc cref="ConfiguredTaskAwaitable{TResult}.GetAwaiter"/>
-        ITaskAwaiter<T> GetAwaiter();
-    }
-}
