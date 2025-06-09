3dee1291c21b923c260f08586b60cfee5f6a7c4f
ravudet <170215607+ravudet@users.noreply.github.com>
2025-06-09 07:48:41 -0700

    asdf

--- OddTrotter.Core/OddTrotter/CalendarV2/Fx/Either/Either.cs
+++ OddTrotter.Core/OddTrotter/CalendarV2/Fx/Either/Either.cs
@@ -165,24 +165,6 @@ namespace Fx.Either
             }
         }
 
-        public static async Task<IEither<TLeft, TRight>> Create<TValue, TLeft, TRight>(
-            TValue value,
-            Func<TValue, bool> discriminator,
-            Func<TValue, Task<TLeft>> leftFactory,
-            Func<TValue, Task<TRight>> rightFactory)
-        {
-            if (discriminator(value))
-            {
-                var leftResult = await leftFactory(value).ConfigureAwait(false);
-                return Either.Left(leftResult).Right<TRight>();
-            }
-            else
-            {
-                var rightResult = await rightFactory(value).ConfigureAwait(false);
-                return Either.Left<TLeft>().Right(rightResult);
-            }
-        }
-
         /// <summary>
         /// placeholder
         /// </summary>
@@ -217,15 +199,6 @@ namespace Fx.Either
             {
                 return Either.Left<TLeft>().Right(rightFactory(value));
             }
-        }
-
-        public static async Task<IEither<TLeft, TRight>> TryCreate<TValue, TResult, TLeft, TRight>(
-            TValue value,
-            Try<TValue, TResult> discriminator,
-            Func<TValue, TResult, Task<TLeft>> leftFactory,
-            Func<TValue, Task<TRight>> rightFactory)
-        {
-
         }
     }
 }
