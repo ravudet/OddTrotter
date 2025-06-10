22df92258f9b929ddcf8154d160dd6f43fa1c1d6
ravudet <170215607+ravudet@users.noreply.github.com>
2025-06-10 07:38:50 -0700

    asdf

--- OddTrotter.Core/OddTrotter/CalendarV1/CalendarEventsContext.cs
+++ OddTrotter.Core/OddTrotter/CalendarV1/CalendarEventsContext.cs
@@ -287,7 +287,7 @@ namespace OddTrotter.Calendar
                 //// TODO you added these async variants for `either` and `queryresult` but you didn't actually put thought into if these work as expected; for example, does it actually make sense to await the `queryresult.selectasync` call here? or does that imply something about the lazy evaluation that you don't want to actually do; would it make sense to have a `selectasync` method overload that is on `task<queryresult>` as well so that you can chain them? //// TODO you wrote a "convenience" overload for now, but you should reall revisit this and think it all the way through; i know you have doing that, but probably unit tests will help
                 //// TODO and is it also ok to pass the tasks returned without awaiting them until the "very end" so to speak? https://github.com/microsoft/vs-threading/blob/main/doc/analyzers/VSTHRD003.md
                 .Select(
-                    seriesMasterOrTranslationError => seriesMasterOrTranslationError
+                    async seriesMasterOrTranslationError => await seriesMasterOrTranslationError
                         .SelectLeft(
                             async seriesMaster =>
                             {
@@ -295,7 +295,8 @@ namespace OddTrotter.Calendar
                                 return (SeriesMaster: seriesMaster, FirstInstance: instances.FirstOrDefault(new Nothing())); //// TODO should there be a parameterless overload for firstordefault
                             }/*,
                             translationError => Task.FromResult(translationError))) //// TODO go through all of your lambda code and make sure they have meaningful names*/
-                            ))
+                            )
+                        .ConfigureAwait(false))
                 .Select(
                     seriesPlusInstanceOrTranslationError =>
                         seriesPlusInstanceOrTranslationError
