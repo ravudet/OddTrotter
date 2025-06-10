531991b7349d942be2e9a6435be2a69dc4b9d37b
ravudet <170215607+ravudet@users.noreply.github.com>
2025-06-10 06:55:06 -0700

    asdf

--- OddTrotter.Core.Tests/OddTrotter/CalendarV2/Fx/Either/EitherUnitTests.cs
+++ OddTrotter.Core.Tests/OddTrotter/CalendarV2/Fx/Either/EitherUnitTests.cs
@@ -88,13 +88,65 @@ namespace Fx.Either
         [TestMethod]
         public void ToEitherPredicateBranchNotTaken()
         {
-            var value = "asdf";
+            var value = "asdfa";
 
-            var either = value.ToEither(_ => _.Length % 2 == 1);
+            var either = value.ToEither(_ => _.Length % 2 == 0);
 
             Assert.IsFalse(either.TryGetLeft(out var left));
             Assert.IsTrue(either.TryGetRight(out var right));
             Assert.AreNotEqual(value, right);
+        }
+
+        [TestMethod]
+        public void ToEitherTryNullPredicate()
+        {
+            var value = "asdf";
+
+            Assert.ThrowsException<ArgumentNullException>(() => value.ToEither(
+#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
+#pragma warning disable CS8625 // Cannot convert null literal to non-nullable reference type.
+                (Try<string, int>)null
+#pragma warning restore CS8625 // Cannot convert null literal to non-nullable reference type.
+#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
+                ));
+        }
+
+        private static bool TryGetLength(string value, out int length)
+        {
+            if (value.Length % 2 == 0)
+            {
+                length = value.Length;
+                return true;
+            }
+            else
+            {
+                length = default;
+                return false;
+            }
+        }
+
+        [TestMethod]
+        public void ToEitherTryBranchTaken()
+        {
+            var value = "asdf";
+
+            var either = value.ToEither<string, int>(TryGetLength);
+
+            Assert.IsTrue(either.TryGetLeft(out var left));
+            Assert.AreEqual(value.Length, left);
+            Assert.IsFalse(either.TryGetRight(out var right));
+        }
+
+        [TestMethod]
+        public void ToEitherTryBranchNotTaken()
+        {
+            var value = "asdfa";
+
+            var either = value.ToEither<string, int>(TryGetLength);
+
+            Assert.IsFalse(either.TryGetLeft(out var left));
+            Assert.IsTrue(either.TryGetRight(out var right));
+            Assert.AreNotEqual(value.Length, right);
         }
     }
 }
