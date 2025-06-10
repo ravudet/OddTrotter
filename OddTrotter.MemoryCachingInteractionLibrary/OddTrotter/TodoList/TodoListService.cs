003ebf74cabbd869b03bcbe2ba1f6dfde8335055
ravudet <170215607+ravudet@users.noreply.github.com>
2025-06-10 07:20:46 -0700

    asdf

--- OddTrotter.MemoryCachingInteractionLibrary/OddTrotter/TodoList/TodoListService.cs
+++ OddTrotter.MemoryCachingInteractionLibrary/OddTrotter/TodoList/TodoListService.cs
@@ -435,6 +435,7 @@
             //// TODO you have written a "task method builder" for `itask`; it's in stash
             //// TODO do a pass cleaning up the existing code using the code quality above and either addressing todos or marking them TODO FUTURE and TODO TOPIC
 
+            //// TODO you need to start being consisten about the use of `adapter` and `wrapper`; perhaps "wrapper" literally means "direct passthrough to make the input compile as the implemented thing" and "adapter" means "take this input and rework the data so that it fits this other contract"
             //// TODO move queryresult to its own namespace (probably queryresultnode should go in the same namespace); make a note that you are not moving the async stuff to its own namespace because you don't want someone accidentally using the sync extensions with async delegates
             //// TODO `try`s should really be covariants:
             //// ```
