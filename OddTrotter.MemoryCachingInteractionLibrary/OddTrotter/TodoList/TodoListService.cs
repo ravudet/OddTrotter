8ee4f7ca99a192a78e65d7cf8806318ea8226384
ravudet <170215607+ravudet@users.noreply.github.com>
2025-06-11 08:10:24 -0700

    asdf

--- OddTrotter.MemoryCachingInteractionLibrary/OddTrotter/TodoList/TodoListService.cs
+++ OddTrotter.MemoryCachingInteractionLibrary/OddTrotter/TodoList/TodoListService.cs
@@ -423,6 +423,7 @@
             //// TODO https://github.com/dotnet/roslyn/blob/main/docs/features/task-types.md
             //// TODO https://learn.microsoft.com/en-us/dotnet/csharp/asynchronous-programming/async-return-types
 
+            //// TODO you are trying to remove the use of `totaskwrapper` in queryresultextensions async+sync, then you need to move the node extensions to v2, unit test them, move the result extensions to v2, unit test them, and move on
             //// TODO `ieither.apply` should return `itask`?
             //// TODO move the async stuff to v2 "correctly"
             //// TODO don't forget about QueryResultExtensionsUnitTests_AsyncQueryResultSyncDelegates, this is async stuff that needs to be "moved"
