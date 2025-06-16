3ff234ae1c14cab6db319a58d4553cd6a00653b8
ravudet <170215607+ravudet@users.noreply.github.com>
2025-06-16 08:16:27 -0700

    asdf

--- OddTrotter.Core/OddTrotter/CalendarV1/Async/QueryResultExtensions_AsyncQueryResultSyncDelegates.cs
+++ OddTrotter.Core/OddTrotter/CalendarV1/Async/QueryResultExtensions_AsyncQueryResultSyncDelegates.cs
@@ -68,51 +68,6 @@ namespace Fx.QueryContext
             }
         }
 
-        public static async ITask<IQueryResultNodeAsync<TValueResult, TError>> Select<TValueSource, TError, TValueResult>(
-            this ITask<IQueryResultNodeAsync<TValueSource, TError>> source,
-            Func<TValueSource, TValueResult> selector)
-        {
-            return await (await source.ConfigureAwait(false)).Select(selector).ConfigureAwait(false);
-        }
-
-        public static async ITask<IQueryResultNodeAsync<TValueResult, TError>> Select<TValueSource, TError, TValueResult>(
-            this IQueryResultNodeAsync<TValueSource, TError> source,
-            Func<TValueSource, TValueResult> selector)
-        {
-            return await source
-                .SelectLeft(
-                    async element =>  
-                        new SelectElementAsync<TValueSource, TError, TValueResult>(
-                            selector(element.Value),
-                            await element.Next().ConfigureAwait(false),
-                            selector))
-                .ToQueryResultNodeAsync()
-                .ConfigureAwait(false);
-        }
-
-        private sealed class SelectElementAsync<TValueSource, TError, TValueResult> : IElementAsync<TValueResult, TError>
-        {
-            private readonly IQueryResultNodeAsync<TValueSource, TError> next;
-            private readonly Func<TValueSource, TValueResult> selector;
-
-            public SelectElementAsync(
-                TValueResult value,
-                IQueryResultNodeAsync<TValueSource, TError> next,
-                Func<TValueSource, TValueResult> selector)
-            {
-                this.Value = value;
-                this.next = next;
-                this.selector = selector;
-            }
-
-            public TValueResult Value { get; }
-
-            public async ITask<IQueryResultNodeAsync<TValueResult, TError>> Next()
-            {
-                return await this.next.Select(this.selector).ConfigureAwait(false);
-            }
-        }
-
         /// <summary>
         /// placeholder
         /// </summary>
---
+++ OddTrotter.Core/OddTrotter/CalendarV2/Fx/QueryContext/QueryResultNodeAsyncExtensions.cs
@@ -0,0 +1,55 @@
+﻿namespace Fx.QueryContext
+{
+    using System;
+    using System.Threading.Tasks;
+
+    using Fx.Either;
+
+    public static class QueryResultNodeAsyncExtensions
+    {
+        public static async ITask<IQueryResultNodeAsync<TValueResult, TError>> Select<TValueSource, TError, TValueResult>(
+            this ITask<IQueryResultNodeAsync<TValueSource, TError>> source,
+            Func<TValueSource, TValueResult> selector)
+        {
+            return await (await source.ConfigureAwait(false)).Select(selector).ConfigureAwait(false);
+        }
+
+        public static async ITask<IQueryResultNodeAsync<TValueResult, TError>> Select<TValueSource, TError, TValueResult>(
+            this IQueryResultNodeAsync<TValueSource, TError> source,
+            Func<TValueSource, TValueResult> selector)
+        {
+            return await source
+                .SelectLeft(
+                    async element =>
+                        new SelectElementAsync<TValueSource, TError, TValueResult>(
+                            selector(element.Value),
+                            await element.Next().ConfigureAwait(false),
+                            selector))
+                .ToQueryResultNodeAsync()
+                .ConfigureAwait(false);
+        }
+
+        private sealed class SelectElementAsync<TValueSource, TError, TValueResult> : IElementAsync<TValueResult, TError>
+        {
+            private readonly IQueryResultNodeAsync<TValueSource, TError> next;
+            private readonly Func<TValueSource, TValueResult> selector;
+
+            public SelectElementAsync(
+                TValueResult value,
+                IQueryResultNodeAsync<TValueSource, TError> next,
+                Func<TValueSource, TValueResult> selector)
+            {
+                Value = value;
+                this.next = next;
+                this.selector = selector;
+            }
+
+            public TValueResult Value { get; }
+
+            public async ITask<IQueryResultNodeAsync<TValueResult, TError>> Next()
+            {
+                return await this.next.Select(this.selector).ConfigureAwait(false);
+            }
+        }
+    }
+}
