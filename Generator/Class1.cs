b8069c11242207468050c2121e4a40a6917bc1a0
ravudet <170215607+ravudet@users.noreply.github.com>
2025-07-07 04:35:58 -0700

    asdf

--- Generator/Class1.cs
+++ Generator/Class1.cs
@@ -2,11 +2,18 @@
 {
     using Microsoft.CodeAnalysis;
 
+#pragma warning disable RS1041 // Compiler extensions should be implemented in assemblies targeting netstandard2.0
     [Generator]
+#pragma warning restore RS1041 // Compiler extensions should be implemented in assemblies targeting netstandard2.0
+#pragma warning disable RS1042 // Implementations of this interface are not allowed
+#pragma warning disable RS1036 // Specify analyzer banned API enforcement setting
     public class Class1 : ISourceGenerator
+#pragma warning restore RS1036 // Specify analyzer banned API enforcement setting
+#pragma warning restore RS1042 // Implementations of this interface are not allowed
     {
         public void Execute(GeneratorExecutionContext context)
         {
+#pragma warning disable RS1035 // Specify analyzer banned API enforcement setting
             context.AddSource(
                 "ravudettest",
 """
@@ -22,6 +29,7 @@ namespace Foo
 }
 """
                 );
+#pragma warning restore RS1035 // Specify analyzer banned API enforcement setting
         }
 
         public void Initialize(GeneratorInitializationContext context)
