21496bc9ea83fcb506a1674cf33bf3ae82551431
ravudet <170215607+ravudet@users.noreply.github.com>
2025-07-07 04:07:32 -0700

    asdf

---
+++ Generator/Class1.cs
@@ -0,0 +1,32 @@
+﻿namespace Generator
+{
+    using Microsoft.CodeAnalysis;
+
+    [Generator]
+    public class Class1 : ISourceGenerator
+    {
+        public void Execute(GeneratorExecutionContext context)
+        {
+            context.AddSource(
+                "ravudettest",
+"""
+namespace Foo
+{
+    public class Bar
+    {
+        public void DoWork()
+        {
+            System.Console.WriteLine("hello");
+        }
+    }
+}
+"""
+                );
+        }
+
+        public void Initialize(GeneratorInitializationContext context)
+        {
+            //// TODO?
+        }
+    }
+}
