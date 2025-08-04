2955cff9e95a871bd45c7c1b36e7a16d6fc14d02
ravudet <170215607+ravudet@users.noreply.github.com>
2025-08-04 05:00:01 -0700

    Asdf

--- OddTrotter.Core/Stash/DeferredMonad.cs
+++ OddTrotter.Core/Stash/DeferredMonad.cs
@@ -109,6 +109,10 @@ namespace Stash
 
             public Map<TLeft, TRight, TResult, TContext, Future<TResult>.Async> Right<TRight>(Func<TRight, TContext, Task<TResult>> func) //// TODO you need to be able to take in more than `task` (also `valuetask`, `itask`, `future<tresult`, etc)
             {
+
+                //// TODO you are here
+                //// TODO you are trying to actually implement an either with this new pattern
+                //// TODO you need to be satisfied with all of the todos in here before you can move on
                 return default;
             }
         }
