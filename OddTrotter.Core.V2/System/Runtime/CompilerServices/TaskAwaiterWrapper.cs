50811281fc15bb12b667b0f006f7147851ed3f04
ravudet <170215607+ravudet@users.noreply.github.com>
2025-08-23 07:26:13 -0700

    asdf

---
+++ OddTrotter.Core.V2/System/Runtime/CompilerServices/TaskAwaiterWrapper.cs
@@ -0,0 +1,48 @@
+﻿/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+namespace System.Runtime.CompilerServices
+{
+    public sealed class TaskAwaiterWrapper<T> : ITaskAwaiter<T>
+    {
+        private readonly TaskAwaiter<T> taskAwaiter;
+
+        /// <summary>
+        /// placeholder
+        /// </summary>
+        /// <param name="taskAwaiter"></param>
+        public TaskAwaiterWrapper(TaskAwaiter<T> taskAwaiter)
+        {
+            this.taskAwaiter = taskAwaiter;
+        }
+
+        /// <inheritdoc/>
+        public bool IsCompleted
+        {
+            get
+            {
+                return this.taskAwaiter.IsCompleted;
+            }
+        }
+
+        /// <inheritdoc/>
+        public T GetResult()
+        {
+            return this.taskAwaiter.GetResult();
+        }
+
+        /// <inheritdoc/>
+        public void OnCompleted(Action continuation)
+        {
+            ArgumentNullException.ThrowIfNull(continuation);
+
+            this.taskAwaiter.OnCompleted(continuation);
+        }
+
+        /// <inheritdoc/>
+        public void UnsafeOnCompleted(Action continuation)
+        {
+            ArgumentNullException.ThrowIfNull(continuation);
+
+            this.taskAwaiter.UnsafeOnCompleted(continuation);
+        }
+    }
+}
