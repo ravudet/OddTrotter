15be8c6e804adafbc84ee28ca49381fb789062d0
ravudet <170215607+ravudet@users.noreply.github.com>
2025-08-23 07:28:02 -0700

    asdf

--- OddTrotter.MemoryCachingInteractionLibrary/OddTrotter/TodoList/TodoListService.cs
+++ OddTrotter.MemoryCachingInteractionLibrary/OddTrotter/TodoList/TodoListService.cs
@@ -426,6 +426,7 @@
             //// TODO https://github.com/dotnet/roslyn/blob/main/docs/features/task-types.md
             //// TODO https://learn.microsoft.com/en-us/dotnet/csharp/asynchronous-programming/async-return-types
 
+            //// TODO ieither should use itask for everything; document that this is for implicit type conversions (and provide examples)
             //// TODO ieither needs to have a single apply that takes both async, extensions should provide all combinations of sync + async, and there should be mixins that allow the implementer of ieither to overload the default `task.fromresult` behavior; also, do the code generation
 
             //// TODO you need to move the node extensions to v2, unit test them, move the result extensions to v2, unit test them, and move on
