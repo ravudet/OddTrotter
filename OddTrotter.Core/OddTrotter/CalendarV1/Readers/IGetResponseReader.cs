a850e118b0844e46c90977bf761571f1203d98fe
ravudet <170215607+ravudet@users.noreply.github.com>
2025-08-24 08:24:04 -0700

    asdf

---
+++ OddTrotter.Core/OddTrotter/CalendarV1/Readers/IGetResponseReader.cs
@@ -0,0 +1,53 @@
+﻿namespace OddTrotter.CalendarV1.Readers
+{
+    using System;
+
+    public interface IGetResponseReader : IReader<IGetResponseHeadersReader>
+    {
+    }
+
+    public interface IGetResponseHeadersReader : IReader<IGetResponseHeadersToken>
+    {
+    }
+
+    public interface IGetResponseHeadersToken
+    {
+        TResult Apply<TResult>(
+            Func<IGetResponseHeaderReader, TResult> getResponseHeaderReader,
+            Func<IGetResponseBodyReader, TResult> getResponseBodyReader);
+    }
+
+    public interface IGetResponseHeaderReader : IReader<IGetResponseHeaderToken>
+    {
+    }
+
+    public interface IGetResponseHeaderToken
+    {
+        TResult Apply<TResult>(
+            Func<ICustomHeaderReader<IGetResponseHeadersReader>, TResult> customHeaderReader);
+    }
+
+    public interface IGetResponseBodyReader : IReader<IOdataContextReader<IGetResponseBodyAfterOdataContextReader>>
+    {
+    }
+
+    public interface IGetResponseBodyAfterOdataContextReader : IReader<IGetResponseBodyAfterOdataContextToken>
+    {
+    }
+
+    public interface IGetResponseBodyAfterOdataContextToken
+    {
+        TResult Apply<TResult>(
+            Func<IControlInformationReader<IGetResponseBodyAfterOdataContextReader>, TResult> controlInformationReader,
+            Func<IPropertyReader<IGetResponseBodyAfterOdataContextToken>, TResult> propertyReader,
+            Func<System.Nothing, TResult> terminal);
+    }
+
+    public interface IControlInformationReader<out TNextReader>
+    {
+    }
+
+    public interface IPropertyReader<out TNextReader>
+    {
+    }
+}
---
+++ OddTrotter.Core/OddTrotter/CalendarV1/Readers/IReader.cs
@@ -0,0 +1,16 @@
+﻿namespace OddTrotter.CalendarV1.Readers
+{
+    using System.Threading.Tasks;
+
+    public interface IReader<out TNextReader>
+    {
+        ValueTask Read();
+
+        TNextReader TryMoveNext(out bool moved);
+    }
+
+    public interface IReader<out TNextReader, out TValue> : IReader<TNextReader>
+    {
+        TValue TryGetValue(out bool moved);
+    }
+}
