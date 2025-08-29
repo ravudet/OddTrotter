fca62cd2880a4fe5bd93bf66336e9bdc04fba867
ravudet <170215607+ravudet@users.noreply.github.com>
2025-08-29 08:06:10 -0700

    asdf

--- OddTrotter.Core/OddTrotter/CalendarV1/Readers/IGetResponseReader.cs
+++ OddTrotter.Core/OddTrotter/CalendarV1/Readers/IGetResponseReader.cs
@@ -1,6 +1,7 @@
 ﻿namespace OddTrotter.CalendarV1.Readers
 {
     using System;
+    using System.IO;
 
     public interface IGetResponseReader : IReader<IGetResponseHeadersReader>
     {
@@ -27,8 +28,12 @@
             Func<ICustomHeaderReader<IGetResponseHeadersReader>, TResult> customHeaderReader);
     }
 
-    public interface IGetResponseBodyReader : IReader<IOdataContextReader<IGetResponseBodyAfterOdataContextReader>> //// TODO document where in the standard this is
+    public interface IGetResponseBodyReader : IReader<IOdataContextReader<IGetResponseBodyAfterOdataContextReader>>
     {
+        // according to [the standard](https://docs.oasis-open.org/odata/odata-json-format/v4.01/odata-json-format-v4.01.html#sec_ControlInformationcontextodatacontex):
+        // > The context control information is not returned if metadata=none is requested. Otherwise it MUST be the first property of any JSON response.
+        // 
+        // the odata.context is the first control information, if present
     }
 
     public interface IGetResponseBodyAfterOdataContextReader : IReader<IGetResponseBodyAfterOdataContextToken>
@@ -38,10 +43,11 @@
     public interface IGetResponseBodyAfterOdataContextToken
     {
         TResult Apply<TResult>(
-            Func<IRootControlInformationReader<IGetResponseBodyAfterOdataContextReader>, TResult> controlInformationReader,
-            Func<IPropertyReader<IGetResponseBodyAfterOdataContextToken>, TResult> propertyReader,
-            //// TODO you are here
-            //// TODO add the other readers here
+            Func<IRootControlInformationReader<IGetResponseBodyAfterOdataContextReader>, TResult> controlInformationReader,,
+            Func<IRootAnnotationReader<IGetResponseBodyAfterOdataContextReader>, TResult> rootAnnotationReader,
+            Func<IPropertyControlInformationReader<IGetResponseBodyAfterOdataContextReader>, TResult> propertyControlInformationReader,
+            Func<IPropertyAnnotationReader<IGetResponseBodyAfterOdataContextReader>, TResult> propertyAnnotationReader,
+            Func<IPropertyReader<IGetResponseBodyAfterOdataContextReader>, TResult> propertyReader,
             Func<System.Nothing, TResult> terminal);
     }
 
@@ -73,11 +79,16 @@
 
 
 
+
+
+
+
+
+
+
     public interface IRootAnnotationReader<out TNextReader>
     {
     }
-
-
 
     public interface IPropertyControlInformationReader<out TNextReader>
     {
@@ -86,20 +97,6 @@
     public interface IPropertyAnnotationReader<out TNextReader>
     {
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
     public interface IPropertyReader<out TNextReader>
     {
