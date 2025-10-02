cc1e4c7909f3c846fd906a6e01127858a5fbe9a1
ravudet <170215607+ravudet@users.noreply.github.com>
2025-10-02 07:47:17 -0700

    asdf

--- OddTrotter.Core.V2/System/Threading/Tasks/ITask.cs
+++ OddTrotter.Core.V2/System/Threading/Tasks/ITask.cs
@@ -12,6 +12,8 @@ namespace System.Threading.Tasks
         /// <inheritdoc cref="Task{TResult}.ConfigureAwait(bool)"/>
         IConfiguredAwaitable<T> ConfigureAwait(bool continueOnCapturedContext);
 		
+        //// TODO put this on its own interface
+        //// TODO update the delegate parameter to not take `itask` but something that represents the actual result
 		ITask<TResult> ContinueWith<TResult>(Func<ITask<T>,TResult> continuationFunction) //// TODO this should take in like a "futureValue" or something that has the exception, canceled, value DU
 			where TResult : allows ref struct;
     }
