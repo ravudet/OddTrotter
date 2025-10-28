586dcb7b131128ce32b3eb7e2373f4f49bed6581
ravudet <170215607+ravudet@users.noreply.github.com>
2025-10-28 08:03:54 -0700

    asdf

---
+++ OddTrotter.Core.V3/IAwaitable.cs
@@ -0,0 +1,12 @@
+﻿namespace OddTrotter.Core.V3
+{
+    public interface IAwaitable<out T, out TAwaiter, out TConfiguredAwaitable>
+        where T : allows ref struct
+        where TAwaiter : allows ref struct
+        where TConfiguredAwaitable : allows ref struct
+    {
+        TAwaiter GetAWaiter();
+
+        TConfiguredAwaitable ConfigureAwait(bool continueOnCapturedContext);
+    }
+}
