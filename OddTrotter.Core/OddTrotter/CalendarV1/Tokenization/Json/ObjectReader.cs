8bf796fdc99a6baf0d84e80077977a65730937ef
ravudet <170215607+ravudet@users.noreply.github.com>
2025-10-31 10:11:13 -0700

    asdf

--- OddTrotter.Core/OddTrotter/CalendarV1/Tokenization/Json/ObjectReader.cs
+++ OddTrotter.Core/OddTrotter/CalendarV1/Tokenization/Json/ObjectReader.cs
@@ -40,29 +40,39 @@
         }
     }
 
-    public ref struct Reader : IReader<ReaderToken>
+    public static class Helpers
+    {
+        public static ValueReader<Nothing> StartReading()
+        {
+            throw new Exception("TODO");
+        }
+    }
+
+    public ref struct ValueReader<TNextReader> : IReader<ValueReaderToken<TNextReader>>
+        where TNextReader : allows ref struct
     {
         public ValueTask Read()
         {
             throw new NotImplementedException();
         }
 
-        public ReaderToken TryMoveNext(out bool moved)
+        public ValueReaderToken<TNextReader> TryMoveNext(out bool moved)
         {
             throw new NotImplementedException();
         }
     }
 
-    public ref struct ReaderToken
+    public ref struct ValueReaderToken<TNextReader>
+        where TNextReader : allows ref struct
     {
         public TResult Apply<TResult>(
-            Func<FalseReader<Nothing>, TResult> @false,
-            Func<NullReader<Nothing>, TResult> @null,
-            Func<TrueReader<Nothing>, TResult> @true,
-            Func<ObjectReader<Nothing>, TResult> @object,
-            Func<ArrayReader<Nothing>, TResult> array,
-            Func<NumberReader<Nothing>, TResult> number,
-            Func<StringReader<Nothing>, TResult> @string)
+            Func<FalseReader<TNextReader>, TResult> @false,
+            Func<NullReader<TNextReader>, TResult> @null,
+            Func<TrueReader<TNextReader>, TResult> @true,
+            Func<ObjectReader<TNextReader>, TResult> @object,
+            Func<ArrayReader<TNextReader>, TResult> array,
+            Func<NumberReader<TNextReader>, TResult> number,
+            Func<StringReader<TNextReader>, TResult> @string)
             where TResult : allows ref struct
         {
             throw new NotImplementedException();
@@ -70,6 +80,7 @@
     }
 
     public ref struct FalseReader<TNextReader> : IReader<TNextReader, FalseToken>
+        where TNextReader : allows ref struct
     {
         public ValueTask Read()
         {
@@ -92,6 +103,7 @@
     }
 
     public ref struct NullReader<TNextReader> : IReader<TNextReader, NullToken>
+        where TNextReader : allows ref struct
     {
         public ValueTask Read()
         {
@@ -114,6 +126,7 @@
     }
 
     public ref struct TrueReader<TNextReader> : IReader<TNextReader, TrueToken>
+        where TNextReader : allows ref struct
     {
         public ValueTask Read()
         {
@@ -135,14 +148,15 @@
     {
     }
 
-    public ref struct ObjectReader<TNextReader> : IReader<TNextReader, ObjectToken>
+    public ref struct ObjectReader<TNextReader> : IReader<TNextReader, ObjectToken<TNextReader>>
+        where TNextReader : allows ref struct
     {
         public ValueTask Read()
         {
             throw new NotImplementedException();
         }
 
-        public ObjectToken TryGetValue(out bool moved)
+        public ObjectToken<TNextReader> TryGetValue(out bool moved)
         {
             throw new NotImplementedException();
         }
@@ -153,18 +167,65 @@
         }
     }
 
-    public readonly ref struct ObjectToken
+    public readonly ref struct ObjectToken<TNextReader>
+        where TNextReader : allows ref struct
     {
+        public TResult Apply<TResult>(
+            Func<MemberReader<ObjectReader<TNextReader>>, TResult> member,
+            Func<TNextReader, TResult> endObject)
+            where TResult : allows ref struct
+        {
+            throw new NotImplementedException();
+        }
     }
 
-    public ref struct ArrayReader<TNextReader> : IReader<TNextReader, ArrayToken>
+    public ref struct MemberReader<TNextReader> : IReader<MemberNameReader<TNextReader>>
+        where TNextReader : allows ref struct
     {
         public ValueTask Read()
         {
             throw new NotImplementedException();
         }
 
-        public ArrayToken TryGetValue(out bool moved)
+        public MemberNameReader<TNextReader> TryMoveNext(out bool moved)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public ref struct MemberNameReader<TNextReader> : IReader<ValueReader<TNextReader>, MemberNameToken>
+        where TNextReader : allows ref struct
+    {
+        public ValueTask Read()
+        {
+            throw new NotImplementedException();
+        }
+
+        public MemberNameToken TryGetValue(out bool moved)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueReader<TNextReader> TryMoveNext(out bool moved)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public readonly ref struct MemberNameToken
+    {
+        public string Value { get; }
+    }
+
+    public ref struct ArrayReader<TNextReader> : IReader<TNextReader, ArrayToken<TNextReader>>
+        where TNextReader : allows ref struct
+    {
+        public ValueTask Read()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ArrayToken<TNextReader> TryGetValue(out bool moved)
         {
             throw new NotImplementedException();
         }
@@ -175,11 +236,20 @@
         }
     }
 
-    public readonly ref struct ArrayToken
+    public readonly ref struct ArrayToken<TNextReader>
+        where TNextReader : allows ref struct
     {
+        public TResult Apply<TResult>(
+            Func<ValueReader<ArrayReader<TNextReader>>, TResult> value,
+            Func<TNextReader, TResult> endArray)
+            where TResult : allows ref struct
+        {
+            throw new NotImplementedException();
+        }
     }
 
     public ref struct NumberReader<TNextReader> : IReader<TNextReader, NumberToken>
+        where TNextReader : allows ref struct
     {
         public ValueTask Read()
         {
@@ -202,6 +272,7 @@
     }
 
     public ref struct StringReader<TNextReader> : IReader<TNextReader, StringToken>
+        where TNextReader : allows ref struct
     {
         public ValueTask Read()
         {
