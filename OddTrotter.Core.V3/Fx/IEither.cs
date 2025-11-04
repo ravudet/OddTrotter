f50533c70ac213a7df3a79e6ba244cf8a10544ff
ravudet <170215607+ravudet@users.noreply.github.com>
2025-11-04 08:15:19 -0800

    asdf

--- OddTrotter.Core.V3/Fx/IEither.cs
+++ OddTrotter.Core.V3/Fx/IEither.cs
@@ -1,18 +1,20 @@
 ﻿namespace Fx
 {
     using System;
+    using System.Diagnostics.CodeAnalysis;
 
     public interface IEither<out TLeft, out TRight>
         where TLeft : allows ref struct
         where TRight : allows ref struct
     {
-        Realizable<TResult> Apply<TResult, TContext, TContinuable>(
-            AsyncRefContextualizedMap<TLeft, TContext, TContinuable, TResult> leftMap,
-            AsyncRefContextualizedMap<TRight, TContext, TContinuable, TResult> rightMap,
+        Realizable<TResult> Apply<TResult, TContext, TContinuable, TContinuableSource>(
+            AsyncRefContextualizedMap<TLeft, TContext, TContinuable, TContinuableSource, TResult> leftMap,
+            AsyncRefContextualizedMap<TRight, TContext, TContinuable, TContinuableSource, TResult> rightMap,
             ref TContext context)
             where TResult : allows ref struct
             where TContext : allows ref struct
-            where TContinuable : IContinuable<TResult>, allows ref struct;
+            where TContinuable : IContinuable<TResult, TContinuableSource>, allows ref struct
+            where TContinuableSource : IContinuableSource<TResult>, allows ref struct;
     }
 
     public readonly ref struct Realizable<T>
@@ -20,17 +22,28 @@
     {
     }
 
-    public interface IContinuable<out TSource>
+    public interface IContinuableSource<out TSource>
         where TSource : allows ref struct
     {
-        Realizable<TResult> ContinueWith<TResult>(Func<TSource, TResult> continuation)
+        TResult Apply<TResult>(
+            Func<TSource, TResult> source,
+            Func<Exception, TResult> exception,
+            Func<TResult> canceled);
+    }
+
+    public interface IContinuable<out TSource, out TContinuableSource>
+        where TSource : allows ref struct
+        where TContinuableSource : IContinuableSource<TSource>, allows ref struct
+    {
+        Realizable<TResult> ContinueWith<TResult>(Func<TContinuableSource, TResult> continuation)
             where TResult : allows ref struct;
     }
 
-    public delegate TContinuable AsyncRefContextualizedMap<in TValue, TContext, out TContinuable, out TResult>(TValue value, ref TContext context)
+    public delegate TContinuable AsyncRefContextualizedMap<in TValue, TContext, out TContinuable, out TContinuableSource, out TResult>(TValue value, ref TContext context)
         where TValue : allows ref struct
         where TContext : allows ref struct
-        where TContinuable : IContinuable<TResult>, allows ref struct
+        where TContinuable : IContinuable<TResult, TContinuableSource>, allows ref struct
+        where TContinuableSource : IContinuableSource<TResult>, allows ref struct
         where TResult : allows ref struct;
 
 
@@ -38,7 +51,80 @@
 
 
 
-    public readonly ref struct RefEither
+
+    public interface INullable<out T>
+        where T : allows ref struct
     {
+        T TryGetValue(out bool retrieved);
+    }
+
+
+    public readonly ref struct RefNullable<T>
+        where T : allows ref struct
+    {
+        private readonly T value;
+
+        private readonly bool hasValue;
+
+        public RefNullable(T value)
+        {
+            this.value = value;
+
+            this.hasValue = true;
+        }
+
+        public bool TryGetValue([MaybeNullWhen(false)] out T value)
+        {
+            if (this.hasValue)
+            {
+                value = this.value;
+                return true;
+            }
+            else
+            {
+                value = default;
+                return false;
+            }
+        }
+    }
+
+    public readonly ref struct RefEither<TLeft, TRight> : IEither<TLeft, TRight>
+        where TLeft : allows ref struct
+        where TRight : allows ref struct
+    {
+        private readonly RefNullable<TLeft> left;
+
+        private readonly RefNullable<TRight> right;
+
+        public RefEither(TLeft left)
+        {
+            this.left = new RefNullable<TLeft>(left);
+
+            this.right = new RefNullable<TRight>();
+        }
+
+        public RefEither(TRight right)
+        {
+            this.right = new RefNullable<TRight>(right);
+
+            this.left = new RefNullable<TLeft>();
+        }
+
+        public Realizable<TResult> Apply<TResult, TContext, TContinuable, TContinuableSource>(
+            AsyncRefContextualizedMap<TLeft, TContext, TContinuable, TContinuableSource, TResult> leftMap,
+            AsyncRefContextualizedMap<TRight, TContext, TContinuable, TContinuableSource, TResult> rightMap, 
+            ref TContext context)
+            where TResult : allows ref struct
+            where TContext : allows ref struct
+            where TContinuable : IContinuable<TResult, TContinuableSource>, allows ref struct
+            where TContinuableSource : IContinuableSource<TResult>, allows ref struct
+        {
+            if (this.left.TryGetValue(out var left))
+            {
+                return
+                    leftMap(left, ref context)
+                    .ContinueWith()
+            }
+        }
     }
 }
