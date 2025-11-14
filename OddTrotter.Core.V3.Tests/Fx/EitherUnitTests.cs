fdb78f29c5147cc205191597e7a2cfc2cf626dd7
ravudet <170215607+ravudet@users.noreply.github.com>
2025-11-14 05:59:36 -0800

    adding test project

---
+++ OddTrotter.Core.V3.Tests/Fx/EitherUnitTests.cs
@@ -0,0 +1,13 @@
+﻿namespace Fx
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public sealed class EitherUnitTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
