ddea589703483bd443c1b961b6ca06745b0cd4b4
ravudet <170215607+ravudet@users.noreply.github.com>
2025-11-17 07:56:13 -0800

    asdf

--- OddTrotter.Core.V3/Fx/IEither.cs
+++ OddTrotter.Core.V3/Fx/IEither.cs
@@ -14,7 +14,7 @@ namespace Fx
         //// TODO implement any unimplemented methods in these files, probably adding a test or two as you go
         //// TODO then, implement the bare minimum needed for oddtrotter to make sure you have a real POC
         //// TODO then, implement everything, ensuring that the oddtrotter POC still compiles
-
+        //// TODO it seems like you have determine that there's iawaitable, which both allows for a state machine that waits and gives the result; and then there's irealizable which can be continued and can have its value realized; maybe play with the idea that these are isomorphic and can be adapted and such
 
 
         public static Realizable<IEither<TLeftResult, TRightResult>> Select<TLeftSource, TRightSource, TLeftResult, TRightResult>(
@@ -382,7 +382,7 @@ namespace Fx
 
 
 
-
+    
 
 
 
@@ -521,7 +521,7 @@ namespace Fx
         }
     }
 
-    public interface ITask<out T> : IContinuable<T>
+    public interface ITask<out T> : IContinuable<T> //// TODO call this awaitable //// TODO probably have a configure await on that; if it's not directly on iawaitable, there should be a configurableawaitable or something //// TODO iawaitable should have two generics, one for the return value and another for the awaiter type
         where T : allows ref struct
     {
         IAwaiter<T> GetAwaiter();
