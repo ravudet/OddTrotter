b7706a808968ca855333b45a5353c348b6c198ce
ravudet <170215607+ravudet@users.noreply.github.com>
2025-11-19 06:05:32 -0800

    asdf

--- OddTrotter.Core.V3.Tests/Fx/EitherUnitTests.cs
+++ OddTrotter.Core.V3.Tests/Fx/EitherUnitTests.cs
@@ -9,6 +9,15 @@
 
     using Microsoft.VisualStudio.TestTools.UnitTesting;
 
+    //// TODO the test `applyrightmapexception` succeeds on its own, but fails when run with other tests... //// TODO this is because the task hasn't completed yet, so the check to in taskwrapper.awaiter.getresult for the exception is null; first of all, you need `getresult` to ensure that the task is completed before continuing; second, you should probably have no use task.run actually, and instead have an adapter method to uses task.fromresult, task.fromfault, task.fromcanceled
+    //// TODO implement a test with ref structs
+    //// TODO implement a test using actual async (like reading a file or something)
+    //// TODO implement any unimplemented methods in these files, probably adding a test or two as you go
+    //// TODO then, implement the bare minimum needed for oddtrotter to make sure you have a real POC
+    //// TODO go through oddtrotter.core.v2 to see if there's any ideas to pull from there
+    //// TODO then, implement everything, ensuring that the oddtrotter POC still compiles
+    //// TODO it seems like you have determine that there's iawaitable, which both allows for a state machine that waits and gives the result; and then there's irealizable which can be continued and can have its value realized; maybe play with the idea that these are isomorphic and can be adapted and such
+
     [TestClass]
     public sealed class EitherUnitTests
     {
