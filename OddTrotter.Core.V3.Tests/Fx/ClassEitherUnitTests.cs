2f006093d3cdcaf00343117ecccc1ec6105b9935
ravudet <170215607+ravudet@users.noreply.github.com>
2025-11-24 06:17:39 -0800

    asdf

--- OddTrotter.Core.V3.Tests/Fx/ClassEitherUnitTests.cs
+++ OddTrotter.Core.V3.Tests/Fx/ClassEitherUnitTests.cs
@@ -11,7 +11,6 @@
 
     using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-    //// TODO implement a test with ref structs values
     //// TODO implement a test using actual async (like reading a file or something)
     //// TODO implement any unimplemented methods in these files, probably adding a test or two as you go
     //// TODO implement any TODOs
