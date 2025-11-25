69ab3333027d53755cbb06839204bbd4277a9404
ravudet <170215607+ravudet@users.noreply.github.com>
2025-11-25 06:02:08 -0800

    asdf

---
+++ OddTrotter.Core.V3.Tests/Fx/AssertExtensions.cs
@@ -0,0 +1,48 @@
+﻿namespace Fx
+{
+    using System;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    public static class AssertExtensions
+    {
+        public static T ThrowsException<T>(this Assert assert, Action action)
+            where T : Exception
+        {
+            return Assert.ThrowsException<T>(action);
+        }
+
+        public readonly ref struct ThrowsExceptionBuilder<TState>
+            where TState : allows ref struct
+        {
+            private readonly Assert assert;
+            private readonly TState state;
+
+            public ThrowsExceptionBuilder(Assert assert, TState state)
+            {
+                this.assert = assert;
+                this.state = state;
+            }
+
+            public TException Commit<TException>(Action<TState> action)
+                where TException : Exception
+            {
+                try
+                {
+                    action(this.state);
+                    return this.assert.ThrowsException<TException>(() => { });
+                }
+                catch (Exception exception)
+                {
+                    return this.assert.ThrowsException<TException>(() => throw exception); //// TODO this loses the call stack
+                }
+            }
+        }
+
+        public static ThrowsExceptionBuilder<TState> ThrowsException<TState>(this Assert assert, TState state)
+            where TState : allows ref struct
+        {
+            return new ThrowsExceptionBuilder<TState>(assert, state);
+        }
+    }
+}
