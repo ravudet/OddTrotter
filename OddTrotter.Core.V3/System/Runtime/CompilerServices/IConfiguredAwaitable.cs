34e241f734466a8dcac494d840b9350997443192
ravudet <170215607+ravudet@users.noreply.github.com>
2025-12-05 06:25:40 -0800

    asdf

--- OddTrotter.Core.V3/System/Runtime/CompilerServices/IConfiguredAwaitable.cs
+++ OddTrotter.Core.V3/System/Runtime/CompilerServices/IConfiguredAwaitable.cs
@@ -1,6 +1,6 @@
 ﻿namespace System.Runtime.CompilerServices
 {
-    public interface IConfiguredAwaitable<out T>
+    public interface IConfiguredAwaitable<out T> //// TODO this is design like .NET does it, but i don't know if i like that; we could have `configureawait` return a new instance of `iawaitable` or something
         where T : allows ref struct
     {
         IAwaiter<T> GetAwaiter();
--- OddTrotter.Core.V3/System/Threading/Tasks/ITask.cs
+++ OddTrotter.Core.V3/System/Threading/Tasks/ITask.cs
@@ -3,7 +3,7 @@ using System.Runtime.CompilerServices;
 
 namespace System.Threading.Tasks
 {
-    public interface ITask<out T> : IContinuable<T> //// TODO call this awaitable //// TODO probably have a configure await on that; if it's not directly on iawaitable, there should be a configurableawaitable or something //// TODO iawaitable should have two generics, one for the return value and another for the awaiter type
+    public interface ITask<out T> : IContinuable<T> //// TODO call this awaitable //// TODO probably have a configure await on that; if it's not directly on iawaitable, there should be a configurableawaitable or something //// TODO iawaitable should have two generics, one for the return value and another for the awaiter type, this way you can accommodate value type awaiters
         where T : allows ref struct
     {
         IAwaiter<T> GetAwaiter();
