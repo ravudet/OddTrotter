e7fef72672875781de8aeffa06fbd268fc8f1bbe
ravudet <170215607+ravudet@users.noreply.github.com>
2025-12-10 05:50:22 -0800

    asdf

--- OddTrotter.Core.V3/System/Runtime/CompilerServices/AwaiterExtensions.cs
+++ OddTrotter.Core.V3/System/Runtime/CompilerServices/AwaiterExtensions.cs
@@ -8,5 +8,12 @@
         {
             return awaiter;
         }
+
+        /*public static TAwaiter GetAwaiter<TAwaiter>(this TAwaiter awaiter)
+        {
+            //// TODO i think this is interesting, but not sure if it's useful; it would let you await anything that is already duck-typed to be an awaiter (while the `iawaiter` variant only allow awaiting things that use my specific interface, which would sometimes require boxing); the "issue" i suppose with this method is that the error message is not as clear; for example, if you await `action<T>`, it doesn't yell at you about the fact that it's not awaitable, it yells at you because you've not implemented a specific awaitable requirement
+
+            return awaiter;
+        }*/
     }
 }
