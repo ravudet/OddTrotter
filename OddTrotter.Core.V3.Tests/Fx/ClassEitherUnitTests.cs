1cfce9ebe62fbf180822b24b9ec5ba641c84b3ea
ravudet <170215607+ravudet@users.noreply.github.com>
2025-12-18 08:18:16 -0800

    asdf

--- OddTrotter.Core.V3.Tests/Fx/ClassEitherUnitTests.cs
+++ OddTrotter.Core.V3.Tests/Fx/ClassEitherUnitTests.cs
@@ -10,11 +10,13 @@
 
     using Microsoft.VisualStudio.TestTools.UnitTesting;
 
+    //// TODO the "bare minimum" should be for that top-level "production" code; you will then do this recursively later for other files
     //// TODO implement the bare minimum needed for these tests; here, the bare minimum includes anything required for type inference
     //// TODO there should be a typeholder property (or extension) for each interface implemented; so, for example, `realizable<T>` should have `typeholder<realizable<T>, t, itask<T>> aseither` *and* `typeholder<realizable<T>, t> ascontinuable`
     //// TODO then, implement the bare minimum needed for oddtrotter to make sure you have a real POC; here, the bare minimum includes anything required for type inference; like, make everything look pretty, allo the way down
     //// TODO go through oddtrotter.core.v2 to see if there's any ideas to pull from there
     //// TODO implement assert extensions so you can always use assert.that
+    //// TODO for the current "bare minimum", make everything look really nice and complete; you don't need every overload and variation, but the ones that you do have should be complete
     //// TODO you need to implement "everything" (all of the extension variations and overloads; any renames that need to happen; implementing visitors and such; full code quality); but, you weren't very systematic the first time through with what you implemented, nor with what you tested; you should do that now
     //// TODO you could have a `class` implementation of `ieither` that takes delegates for left and right (where those delegates can return `ref struct`s); is this worth doing?
     //// TODO it seems like you have determined that there's iawaitable, which both allows for a state machine that waits and gives the result; and then there's irealizable which can be continued and can have its value realized; maybe play with the idea that these are isomorphic and can be adapted and such
