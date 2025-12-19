12b40c5cad1c6e61d08840b8bfcbc98829984047
ravudet <170215607+ravudet@users.noreply.github.com>
2025-12-19 10:42:06 -0800

    asdf

--- OddTrotter.Core.V3/Fx/Either/Either.cs
+++ OddTrotter.Core.V3/Fx/Either/Either.cs
@@ -3,6 +3,37 @@ namespace Fx.Either
 {
     public static class Either
     {
+        public readonly ref struct Empty<TLeft>
+        {
+            public Either<TLeft, TRight> Right<TRight>(TRight value)
+            {
+                return new Either<TLeft, TRight>.Right(value);
+            }
+        }
 
+        public static Empty<TLeft> Left<TLeft>()
+        {
+            return new Empty<TLeft>();
+        }
+
+        public readonly ref struct Full<TLeft>
+        {
+            private readonly TLeft left;
+
+            public Full(TLeft left)
+            {
+                this.left = left;
+            }
+
+            public Either<TLeft, TRight> Right<TRight>()
+            {
+                return new Either<TLeft, TRight>.Left(left);
+            }
+        }
+
+        public static Full<TLeft> Left<TLeft>(TLeft value)
+        {
+            return new Full<TLeft>(value);
+        }
     }
 }
