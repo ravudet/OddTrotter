ca862f03faa57b33bf74b463c6d3e8106fe63b1b
ravudet <170215607+ravudet@users.noreply.github.com>
2026-01-08 05:40:05 -0800

    asdf

--- OddTrotter.Core.V3/Fx/Either/RefEither.cs
+++ OddTrotter.Core.V3/Fx/Either/RefEither.cs
@@ -1,107 +1,31 @@
-﻿/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-namespace Fx.Either
+﻿namespace Fx.Either
 {
-    using System;
-    using System.Diagnostics.CodeAnalysis;
-    using System.Runtime.CompilerServices;
-    using System.Threading.Tasks;
-
-    using Fx;
-    using Fx.Either.Mixins;
-    using Fx.Realizable;
-
-    public readonly ref struct RefEither<TLeft, TRight> : IEither<RefEither<TLeft, TRight>, TLeft, TRight>, ICastable, IDecomposeMixin<RefEither<TLeft, TRight>, TLeft, TRight>
-        where TLeft : allows ref struct
-        where TRight : allows ref struct
+    public static class RefEither
     {
-        private readonly RefNullable<TLeft> left;
-
-        private readonly RefNullable<TRight> right;
-
-        public RefEither(TLeft left)
+        public readonly ref struct EmptyLeft<TLeft>
         {
-            this.left = new RefNullable<TLeft>(left);
-
-            right = new RefNullable<TRight>();
-        }
-
-        public TypeHolder<RefEither<TLeft, TRight>, TLeft, TRight> AsEither
-        {
-            get
+            public RefEither<TLeft, TRight> Right<TRight>(TRight value)
             {
-                return new TypeHolder<RefEither<TLeft, TRight>, TLeft, TRight>(this);
+                return new RefEither<TLeft, TRight>(value);
             }
         }
 
-        public RefEither(TRight right)
+        public static EmptyLeft<TLeft> Left<TLeft>()
         {
-            this.right = new RefNullable<TRight>(right);
-
-            left = new RefNullable<TLeft>();
+            return new EmptyLeft<TLeft>();
         }
 
-        public Realizable<TResult> Apply<TResult, TContext, TContinuable>(
-            AsyncRefContextualizedContinuableMap<TLeft, TContext, TContinuable, TResult> leftMap,
-            AsyncRefContextualizedContinuableMap<TRight, TContext, TContinuable, TResult> rightMap, 
-            ref TContext context)
-            where TResult : allows ref struct
-            where TContext : allows ref struct
-            where TContinuable : IContinuable<TResult>, allows ref struct
+        public readonly ref struct EmptyRight<TRight>
         {
-            if (this.left.TryGetValue(out var left))
+            public RefEither<TLeft, TRight> Left<TLeft>(TLeft value)
             {
-                return
-                    leftMap(left, ref context)
-                    .ContinueWith(
-                        result => result,
-                        exception => throw new LeftMapException(exception),
-                        canceled => throw canceled);
-            }
-            else if (this.right.TryGetValue(out var right))
-            {
-                return
-                    rightMap(right, ref context)
-                    .ContinueWith(
-                        result => result,
-                        exception => throw new RightMapException(exception),
-                        canceled => throw canceled);
-            }
-            else
-            {
-                throw new Exception("TODO bug");
+                return new RefEither<TLeft, TRight>(value);
             }
         }
 
-        bool IDecomposeMixin<RefEither<TLeft, TRight>, TLeft, TRight>.Decompose([MaybeNullWhen(false)] out TLeft value, [MaybeNullWhen(true)] out TRight future)
+        public static EmptyRight<TRight> Right<TRight>()
         {
-            if (left.TryGetValue(out value))
-            {
-                future = default;
-                return true;
-            }
-            else if (right.TryGetValue(out future))
-            {
-                value = default;
-                return false;
-            }
-            else
-            {
-                throw new Exception("TODO bug");
-            }
-        }
-
-        public bool TryCast<TCasted>([MaybeNullWhen(false)] out TCasted casted)
-            where TCasted : struct, allows ref struct
-        {
-            if (DecomposeMixin.TryCreate<TCasted, RefEither<TLeft, TRight>, TLeft, TRight>(
-                this,
-                out casted))
-            {
-                return true;
-            }
-
-            casted = default;
-            return false;
+            return new EmptyRight<TRight>();
         }
     }
 }
---
+++ OddTrotter.Core.V3/Fx/Either/RefEither{TLeft, TRight}.cs
@@ -0,0 +1,107 @@
+﻿/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+namespace Fx.Either
+{
+    using System;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Runtime.CompilerServices;
+    using System.Threading.Tasks;
+
+    using Fx;
+    using Fx.Either.Mixins;
+    using Fx.Realizable;
+
+    public readonly ref struct RefEither<TLeft, TRight> : IEither<RefEither<TLeft, TRight>, TLeft, TRight>, ICastable, IDecomposeMixin<RefEither<TLeft, TRight>, TLeft, TRight>
+        where TLeft : allows ref struct
+        where TRight : allows ref struct
+    {
+        private readonly RefNullable<TLeft> left;
+
+        private readonly RefNullable<TRight> right;
+
+        public RefEither(TLeft left)
+        {
+            this.left = new RefNullable<TLeft>(left);
+
+            right = new RefNullable<TRight>();
+        }
+
+        public TypeHolder<RefEither<TLeft, TRight>, TLeft, TRight> AsEither
+        {
+            get
+            {
+                return new TypeHolder<RefEither<TLeft, TRight>, TLeft, TRight>(this);
+            }
+        }
+
+        public RefEither(TRight right)
+        {
+            this.right = new RefNullable<TRight>(right);
+
+            left = new RefNullable<TLeft>();
+        }
+
+        public Realizable<TResult> Apply<TResult, TContext, TContinuable>(
+            AsyncRefContextualizedContinuableMap<TLeft, TContext, TContinuable, TResult> leftMap,
+            AsyncRefContextualizedContinuableMap<TRight, TContext, TContinuable, TResult> rightMap, 
+            ref TContext context)
+            where TResult : allows ref struct
+            where TContext : allows ref struct
+            where TContinuable : IContinuable<TResult>, allows ref struct
+        {
+            if (this.left.TryGetValue(out var left))
+            {
+                return
+                    leftMap(left, ref context)
+                    .ContinueWith(
+                        result => result,
+                        exception => throw new LeftMapException(exception),
+                        canceled => throw canceled);
+            }
+            else if (this.right.TryGetValue(out var right))
+            {
+                return
+                    rightMap(right, ref context)
+                    .ContinueWith(
+                        result => result,
+                        exception => throw new RightMapException(exception),
+                        canceled => throw canceled);
+            }
+            else
+            {
+                throw new Exception("TODO bug");
+            }
+        }
+
+        bool IDecomposeMixin<RefEither<TLeft, TRight>, TLeft, TRight>.Decompose([MaybeNullWhen(false)] out TLeft value, [MaybeNullWhen(true)] out TRight future)
+        {
+            if (left.TryGetValue(out value))
+            {
+                future = default;
+                return true;
+            }
+            else if (right.TryGetValue(out future))
+            {
+                value = default;
+                return false;
+            }
+            else
+            {
+                throw new Exception("TODO bug");
+            }
+        }
+
+        public bool TryCast<TCasted>([MaybeNullWhen(false)] out TCasted casted)
+            where TCasted : struct, allows ref struct
+        {
+            if (DecomposeMixin.TryCreate<TCasted, RefEither<TLeft, TRight>, TLeft, TRight>(
+                this,
+                out casted))
+            {
+                return true;
+            }
+
+            casted = default;
+            return false;
+        }
+    }
+}
