8817dbfbdf91cf92c07e6023e39820fef5885849
ravudet <170215607+ravudet@users.noreply.github.com>
2026-01-09 05:40:30 -0800

    asdf

--- OddTrotter.Core.V3.Tests/Fx/RefEitherUnitTests.cs
+++ OddTrotter.Core.V3.Tests/Fx/RefEitherUnitTests.cs
@@ -98,7 +98,7 @@
         private static Realizable<string> TestMethod1Impl(RefEither<int, Exception> either)
         {
             //// TODO you are here
-            //// TODO update this to use an `applyasync`; also update the other test class to do the same
+            //// TODO update this to use an `applyasync`; also update the other test class to do the same`
             return either.AsEither.Apply(
                 value => ToString(value),
                 exception => ToString(exception));
