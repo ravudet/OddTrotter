58c791c1d5bf9365ad2539245f81f3fef4bb22bc
ravudet <170215607+ravudet@users.noreply.github.com>
2026-01-19 09:27:48 -0800

    asdf

--- OddTrotter.Core.V3.Tests/Fx/ClassEitherUnitTests.cs
+++ OddTrotter.Core.V3.Tests/Fx/ClassEitherUnitTests.cs
@@ -21,6 +21,7 @@
     //// TODO for the current "bare minimum", make everything look really nice and complete; you don't need every overload and variation, but the ones that you do have should be complete
     //// TODO you need to implement "everything" (all of the extension variations and overloads; any renames that need to happen; implementing visitors and such; full code quality); but, you weren't very systematic the first time through with what you implemented, nor with what you tested; you should do that now
     //// TODO add `assert.that` to code quality
+    //// TODO add `await="true"` exception documentation to code quality
     //// TODO you have to use `is null` for null checks because `==` can be overridden; add this to code quality //// TODO but i think you already use object.referenceequal for this though?
     //// TODO you could have a `class` implementation of `ieither` that takes delegates for left and right (where those delegates can return `ref struct`s); is this worth doing?
     //// TODO it seems like you have determined that there's iawaitable, which both allows for a state machine that waits and gives the result; and then there's irealizable which can be continued and can have its value realized; maybe play with the idea that these are isomorphic and can be adapted and such
