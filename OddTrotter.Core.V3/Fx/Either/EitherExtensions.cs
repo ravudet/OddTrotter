c56e6f0e2d1b088205e0b4bd27c53f0f3b708f76
ravudet <170215607+ravudet@users.noreply.github.com>
2026-01-19 09:28:20 -0800

    asdf

--- OddTrotter.Core.V3/Fx/Either/EitherExtensions.cs
+++ OddTrotter.Core.V3/Fx/Either/EitherExtensions.cs
@@ -548,8 +548,6 @@ namespace Fx.Either
             where TContinuable : IContinuable<TResult>, allows ref struct
             where TResult : allows ref struct
         {
-            //// TODO you have the `torealizable` method above; you need something similar for the "async" overloads, like below; remmber, ultimately the mapping exceptions should be done *only* in the `ieither` implementation
-            //// TODO i think your `ieither` implementations actually need to be updated to catch if `leftmap` or `rightmap` throws //// TODO this really demonstrates an issue with "async" methods; they can throw when generating the task that gets awaited, and they can also throw when they are awaited; think about how you want to document these two cases //// TODO the async exception thing basically comes down to letting the caller know when they need to catch exceptions; this will come into play if you update the `ieither` implementations because they will need to correctly document if there are any mapexceptions upon calling `applyasync`, or if the exceptions will only ever be thrown when awaiting the `realizable` //// TODO maybe use this as an example: https://learn.microsoft.com/en-us/dotnet/api/system.io.stream.readasync?view=net-10.0#system-io-stream-readasync(system-byte()-system-int32-system-int32-system-threading-cancellationtoken)
             return either.ApplyAsync<TResult, bool, TContinuable>(
                 (TLeft left, ref bool context) => leftMap(left),
                 (TRight right, ref bool context) => rightMap(right),
