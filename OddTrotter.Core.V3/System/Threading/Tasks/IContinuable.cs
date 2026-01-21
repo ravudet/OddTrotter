31ee8ef48e5e86a4c4adce20e5acf952f669ba9a
ravudet <170215607+ravudet@users.noreply.github.com>
2026-01-21 07:31:40 -0800

    asdf

--- OddTrotter.Core.V3/System/Threading/Tasks/IContinuable.cs
+++ OddTrotter.Core.V3/System/Threading/Tasks/IContinuable.cs
@@ -11,7 +11,7 @@ namespace System.Threading.Tasks
         Realizable<TResult> ContinueWith<TResult>(
             Func<TSource, TResult> sourceContinuation,
             Func<Exception, TResult> exceptionContinuation,
-            Func<OperationCanceledException, TResult> canceledContinuation)
+            Func<OperationCanceledException, TResult> canceledContinuation) //// TODO should there be overloads that don't have the exception and cancelation stuff, that way the standalone `throw` statement can still preserve the stack?
             where TResult : allows ref struct;
     }
 }
