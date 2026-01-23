e3719329f6331395528de34d78b0e710f9b796b4
ravudet <170215607+ravudet@users.noreply.github.com>
2026-01-23 08:00:19 -0800

    asdf

--- OddTrotter.Core.V3/OddTrotter/Odata/v4_01/ConventionContext/IConventionContext.cs
+++ OddTrotter.Core.V3/OddTrotter/Odata/v4_01/ConventionContext/IConventionContext.cs
@@ -4,6 +4,13 @@
 
     internal interface IConventionContext
     {
+        /// <summary>
+        /// TODO error response //// TODO these should be individual exceptions (that *maybe* have a base type)
+        /// TODO not odata response
+        /// TODO not collection response
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
         Task<GetCollectionResponse> GetCollection(GetCollectionRequest request);
     }
 }
