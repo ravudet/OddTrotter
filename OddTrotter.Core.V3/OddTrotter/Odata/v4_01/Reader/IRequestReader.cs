eaaa261c1f11e82306887896cc16812592f91843
ravudet <170215607+ravudet@users.noreply.github.com>
2026-01-24 13:59:38 -0800

    asdf

---
+++ OddTrotter.Core.V3/OddTrotter/Odata/v4_01/Reader/IRequestReader.cs
@@ -0,0 +1,56 @@
+﻿namespace OddTrotter.Odata.v4_01.Reader
+{
+    internal interface IRequestReader
+    {
+        IVerbReader Read();
+    }
+
+    internal interface IVerbReader
+    {
+        IUrlReader Read(out HttpVerb httpVerb);
+    }
+
+    internal interface IUrlReader
+    {
+    }
+
+    internal interface IUrlSchemeReader
+    {
+    }
+
+    internal interface IUrlDomainReader
+    {
+    }
+
+    internal interface IUrlPathReader
+    {
+    }
+
+    internal interface IUrlPathSegmentReader
+    {
+    }
+
+    internal interface IUrlQueryReader
+    {
+    }
+
+    internal interface IUrlQueryKvpReader
+    {
+    }
+
+    internal interface IUrlQueryNameReader
+    {
+    }
+
+    internal interface IUrlQueryValueReader
+    {
+    }
+
+    internal interface IHeadersReader
+    {
+    }
+
+    internal interface IBodyReader
+    {
+    }
+}
--- OddTrotter.Core.V3/OddTrotter/Odata/v4_01/Reader/RequestReaderWriter.cs
+++ OddTrotter.Core.V3/OddTrotter/Odata/v4_01/Reader/RequestReaderWriter.cs
@@ -1,6 +1,17 @@
 ﻿namespace OddTrotter.Odata.v4_01.Reader
 {
-    internal class RequestReaderWriter
+    internal sealed class RequestReaderWriter : IRequestWriter
     {
+        private readonly IRequestReader requestReader;
+
+        public RequestReaderWriter(IRequestReader requestReader)
+        {
+            this.requestReader = requestReader;
+        }
+
+        public IVerbWriter Write()
+        {
+            requestReader.Move()
+        }
     }
 }
