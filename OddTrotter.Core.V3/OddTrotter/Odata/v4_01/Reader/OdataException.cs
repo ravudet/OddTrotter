a6a49d8eb2c458b3f3e872ff52cf90f973288604
ravudet <170215607+ravudet@users.noreply.github.com>
2026-01-25 07:32:36 -0800

    asdf

---
+++ OddTrotter.Core.V3/OddTrotter/Odata/v4_01/Reader/OdataException.cs
@@ -0,0 +1,17 @@
+﻿namespace OddTrotter.Odata.v4_01.Reader
+{
+    using System;
+
+    internal sealed class OdataException : Exception
+    {
+        public OdataException(string message)
+            : base(message)
+        {
+        }
+
+        public OdataException(string message, Exception exception)
+            : base(message, exception)
+        {
+        }
+    }
+}
--- OddTrotter.Core.V3/OddTrotter/Odata/v4_01/Reader/RequestReader.cs
+++ OddTrotter.Core.V3/OddTrotter/Odata/v4_01/Reader/RequestReader.cs
@@ -2,6 +2,8 @@
 {
     using System.Net.Http;
 
+    using Fx.Either;
+
     internal sealed class RequestReader : IRequestReader
     {
         private readonly HttpRequestMessage httpRequestMessage;
@@ -13,52 +15,133 @@
 
         public IVerbReader Read()
         {
-            return new VerbReader();
+            return new VerbReader(this.httpRequestMessage);
         }
     }
 
     internal sealed class VerbReader : IVerbReader
     {
+        private readonly HttpRequestMessage httpRequestMessage;
+
+        internal VerbReader(HttpRequestMessage httpRequestMessage)
+        {
+            this.httpRequestMessage = httpRequestMessage;
+        }
+
         public IUrlReader Read(out HttpVerb httpVerb)
         {
-            throw new System.NotImplementedException();
+            httpVerb = new HttpVerb(this.httpRequestMessage.Method.Method); //// TODO not all methods are supported by odata
+            return new UrlReader(this.httpRequestMessage);
         }
     }
 
     internal sealed class UrlReader : IUrlReader
     {
+        private readonly HttpRequestMessage httpRequestMessage;
+
+        internal UrlReader(HttpRequestMessage httpRequestMessage)
+        {
+            this.httpRequestMessage = httpRequestMessage;
+        }
+
         public IUrlSchemeReader Read()
         {
-            throw new System.NotImplementedException();
+            return new UrlSchemeReader(this.httpRequestMessage);
         }
     }
 
     internal sealed class UrlSchemeReader : IUrlSchemeReader
     {
+        private readonly HttpRequestMessage httpRequestMessage;
+
+        internal UrlSchemeReader(HttpRequestMessage httpRequestMessage)
+        {
+            this.httpRequestMessage = httpRequestMessage;
+        }
+
         public IUrlDomainReader Read(out UrlScheme urlScheme)
         {
-            throw new System.NotImplementedException();
+            var requestUri = this.httpRequestMessage.RequestUri;
+            if (requestUri == null)
+            {
+                throw new OdataException("TODO can this actually be null?");
+            }
+
+            urlScheme = new UrlScheme(requestUri.Scheme);
+            return new UrlDomainReader(this.httpRequestMessage);
         }
     }
 
     internal sealed class UrlDomainReader : IUrlDomainReader
     {
+        private readonly HttpRequestMessage httpRequestMessage;
+
+        internal UrlDomainReader(HttpRequestMessage httpRequestMessage)
+        {
+            this.httpRequestMessage = httpRequestMessage;
+        }
+
         public IUrlPathReader Read(out UrlDomain urlDomain)
         {
-            throw new System.NotImplementedException();
+            var requestUri = this.httpRequestMessage.RequestUri;
+            if (requestUri == null)
+            {
+                throw new OdataException("TODO can this actually be null?");
+            }
+
+            urlDomain = new UrlDomain(requestUri.Host);
+            return new UrlPathReader(this.httpRequestMessage);
         }
     }
 
     internal sealed class UrlPathReader : IUrlPathReader
     {
+        private readonly HttpRequestMessage httpRequestMessage;
+        private readonly int segment;
+
+        internal UrlPathReader(HttpRequestMessage httpRequestMessage)
+            : this(httpRequestMessage, 0)
+        {
+        }
+
+        internal UrlPathReader(HttpRequestMessage httpRequestMessage, int segment)
+        {
+            this.httpRequestMessage = httpRequestMessage;
+            this.segment = segment;
+        }
+
         public UrlPathToken Read()
         {
-            throw new System.NotImplementedException();
+            //// TODO we should probably confirm that there is no fragment (and anything else that odata doesn't leverage)
+
+            var requestUri = this.httpRequestMessage.RequestUri;
+            if (requestUri == null)
+            {
+                throw new OdataException("TODO can this actually be null?");
+            }
+
+            if (requestUri.Segments.Length == 0)
+            {
+                return new UrlPathToken.Query(new UrlQueryReader(this.httpRequestMessage));
+            }
+            else
+            {
+                return new UrlPathToken.PathSegment(new UrlPathSegmentReader(this.httpRequestMessage, this.segment));
+            }
         }
     }
 
     internal sealed class UrlPathSegmentReader : IUrlPathSegmentReader
     {
+        private readonly HttpRequestMessage httpRequestMessage;
+        private readonly int segment;
+
+        internal UrlPathSegmentReader(HttpRequestMessage httpRequestMessage, int segment)
+        {
+            this.httpRequestMessage = httpRequestMessage;
+            this.segment = segment;
+        }
+
         public IUrlPathReader Read(out UrlPathSegment urlPathSegment)
         {
             throw new System.NotImplementedException();
@@ -67,6 +150,13 @@
 
     internal sealed class UrlQueryReader : IUrlQueryReader
     {
+        private readonly HttpRequestMessage httpRequestMessage;
+
+        internal UrlQueryReader(HttpRequestMessage httpRequestMessage)
+        {
+            this.httpRequestMessage = httpRequestMessage;
+        }
+
         public UrlQueryToken Read()
         {
             throw new System.NotImplementedException();
