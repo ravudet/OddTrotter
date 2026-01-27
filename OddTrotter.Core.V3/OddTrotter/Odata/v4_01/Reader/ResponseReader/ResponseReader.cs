8670511726c9f542f7ea6604ae91af7fb9e78c60
ravudet <170215607+ravudet@users.noreply.github.com>
2026-01-27 10:43:47 -0800

    asdf

--- OddTrotter.Core.V3/OddTrotter/Odata/v4_01/Reader/ResponseReader/ResponseReader.cs
+++ OddTrotter.Core.V3/OddTrotter/Odata/v4_01/Reader/ResponseReader/ResponseReader.cs
@@ -1,5 +1,6 @@
 ﻿namespace OddTrotter.Odata.v4_01.Reader.ResponseReader
 {
+    using System.Collections.Generic;
     using System.Net.Http;
 
     internal sealed class ResponseReader : IResponseReader
@@ -13,13 +14,143 @@
 
         public IHeadersReader Read()
         {
-            throw new System.NotImplementedException();
+            return new HeadersReader(this.httpResponseMessage, this.httpResponseMessage.Headers.GetEnumerator());
         }
     }
 
     internal sealed class HeadersReader : IHeadersReader
     {
+        private readonly HttpResponseMessage httpResponseMessage;
+        private readonly IEnumerator<KeyValuePair<string, IEnumerable<string>>> headers;
+
+        internal HeadersReader(
+            HttpResponseMessage httpResponseMessage, 
+            IEnumerator<KeyValuePair<string, IEnumerable<string>>> headers)
+        {
+            this.httpResponseMessage = httpResponseMessage;
+            this.headers = headers;
+        }
+
         public HeadersToken Read()
+        {
+            if (!this.headers.MoveNext())
+            {
+                return new HeadersToken.Body(new BodyReader());
+            }
+            else
+            {
+                return new HeadersToken.Header(new HeaderReader(this.httpResponseMessage, this.headers);
+            }
+        }
+    }
+
+    internal sealed class HeaderReader : IHeaderReader
+    {
+        private readonly HttpResponseMessage httpResponseMessage;
+        private readonly IEnumerator<KeyValuePair<string, IEnumerable<string>>> headers;
+
+        internal HeaderReader(
+            HttpResponseMessage httpResponseMessage,
+            IEnumerator<KeyValuePair<string, IEnumerable<string>>> headers)
+        {
+            this.httpResponseMessage = httpResponseMessage;
+            this.headers = headers;
+        }
+
+        public IHeaderKvpReader Read()
+        {
+            return new HeaderKvpReader(this.httpResponseMessage, this.headers);
+        }
+    }
+
+    internal sealed class HeaderKvpReader : IHeaderKvpReader
+    {
+        private readonly HttpResponseMessage httpResponseMessage;
+        private readonly IEnumerator<KeyValuePair<string, IEnumerable<string>>> headers;
+
+        internal HeaderKvpReader(
+            HttpResponseMessage httpResponseMessage,
+            IEnumerator<KeyValuePair<string, IEnumerable<string>>> headers)
+        {
+            this.httpResponseMessage = httpResponseMessage;
+            this.headers = headers;
+        }
+
+        public IHeaderKeyReader Read()
+        {
+            return new HeaderKeyReader(this.httpResponseMessage, this.headers);
+        }
+    }
+
+    internal sealed class HeaderKeyReader : IHeaderKeyReader
+    {
+        private readonly HttpResponseMessage httpResponseMessage;
+        private readonly IEnumerator<KeyValuePair<string, IEnumerable<string>>> headers;
+
+        internal HeaderKeyReader(
+            HttpResponseMessage httpResponseMessage,
+            IEnumerator<KeyValuePair<string, IEnumerable<string>>> headers)
+        {
+            this.httpResponseMessage = httpResponseMessage;
+            this.headers = headers;
+        }
+
+        public HeaderKeyToken Read(out HeaderKey headerKey)
+        {
+            var header = this.headers.Current;
+            headerKey = new HeaderKey(header.Key);
+
+            var values = header.Value.GetEnumerator();
+            if (!values.MoveNext())
+            {
+                return new HeaderKeyToken.Headers(new HeadersReader(this.httpResponseMessage, this.headers));
+            }
+            else
+            {
+                return new HeaderKeyToken.HeaderValue(new HeaderValueReader(this.httpResponseMessage, this.headers, values));
+            }
+        }
+    }
+
+    internal sealed class HeaderValueReader : IHeaderValueReader
+    {
+        private readonly HttpResponseMessage httpResponseMessage;
+        private readonly IEnumerator<KeyValuePair<string, IEnumerable<string>>> headers;
+        private readonly IEnumerator<string> values;
+
+        internal HeaderValueReader(
+            HttpResponseMessage httpResponseMessage,
+            IEnumerator<KeyValuePair<string, IEnumerable<string>>> headers,
+            IEnumerator<string> values)
+        {
+            this.httpResponseMessage = httpResponseMessage;
+            this.headers = headers;
+            this.values = values;
+        }
+
+        public HeaderValueToken Read(out HeaderValue headerValue)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    internal sealed class BodyReader : IBodyReader
+    {
+        public BodyToken Read()
         {
             throw new System.NotImplementedException();
         }
