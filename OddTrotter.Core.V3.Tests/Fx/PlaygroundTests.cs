4f6c750fdcf2f257ca8aa5b0fca1eaa17c3a2062
ravudet <170215607+ravudet@users.noreply.github.com>
2026-01-30 08:15:15 -0800

    asdf

---
+++ OddTrotter.Core.V3.Tests/Fx/PlaygroundTests.cs
@@ -0,0 +1,30 @@
+﻿namespace Fx
+{
+    using System.Net.Http;
+    using System.Threading.Tasks;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public sealed class PlaygroundTests
+    {
+        [TestMethod]
+        public async Task ReadingFromDeadNetworkStream()
+        {
+            using (var httpClient = new HttpClient())
+            {
+                using (var httpResponse = await httpClient.GetAsync("https://www.google.com").ConfigureAwait(false))
+                {
+                    using (var contentStream = await httpResponse.Content.ReadAsStreamAsync().ConfigureAwait(false))
+                    {
+                        var buffer = new byte[1024];
+                        int read;
+                        while ((read = await contentStream.ReadAsync(buffer, 0, buffer.Length).ConfigureAwait(false)) != 0)
+                        {
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
