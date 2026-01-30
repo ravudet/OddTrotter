c011a0c091265166b0cdbbabbdd2ad06ec7efe9f
ravudet <170215607+ravudet@users.noreply.github.com>
2026-01-30 08:04:38 -0800

    sdfg

--- OddTrotter.Core.V3/OddTrotter/Odata/v4_01/WeakConventionContext/IWeakConventionContext.cs
+++ OddTrotter.Core.V3/OddTrotter/Odata/v4_01/WeakConventionContext/IWeakConventionContext.cs
@@ -6,6 +6,8 @@
     /// TODO "convention" is around semantics (e.g. we expected a collection but didn't receive one)
     /// 
     /// TODO "weak" means that we use .net types with odata language (e.g. nextlink, property names, etc)
+    /// 
+    /// TODO when do you want to apply an iedmmodel? i think you could have a model-based weakconventioncontext which recognizes that, e.g., the getcollectionrequest is a request to a single-valued endpoint; so maybe "model-based" is actually an implementation-specific construct and not present in the interface; double check this rationale with `istrongconventioncontext` as well (though at the "strong" level you would need something that maps the edmmodel to the generic type parameter probably)
     /// </summary>
     internal interface IWeakConventionContext
     {
