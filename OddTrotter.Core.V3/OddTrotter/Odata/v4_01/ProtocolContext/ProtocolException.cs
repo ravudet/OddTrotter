0efe4afed8062e06e85fb586699b5202bb33feb1
ravudet <170215607+ravudet@users.noreply.github.com>
2026-02-03 07:24:56 -0800

    sdaf

---
+++ OddTrotter.Core.V3/OddTrotter/Odata/v4_01/ProtocolContext/ProtocolException.cs
@@ -0,0 +1,17 @@
+﻿namespace OddTrotter.Odata.v4_01.ProtocolContext
+{
+    using System;
+
+    internal sealed class ProtocolException : Exception
+    {
+        public ProtocolException(string message)
+            : base(message)
+        {
+        }
+
+        public ProtocolException(string message, Exception exception)
+            : base(message, exception)
+        {
+        }
+    }
+}
