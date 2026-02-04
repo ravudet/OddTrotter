6ca3b8ea6cb81a5904db21677f796117b4078369
ravudet <170215607+ravudet@users.noreply.github.com>
2026-02-04 08:38:40 -0800

    asdf

--- OddTrotter.Core.V3/OddTrotter/Odata/v4_01/ProtocolContext/OdataResponse.cs
+++ OddTrotter.Core.V3/OddTrotter/Odata/v4_01/ProtocolContext/OdataResponse.cs
@@ -13,8 +13,8 @@
             Properties = properties;
         }
 
-        public string HttpStatusCode { get; }
-        public IEnumerable<HttpHeader> Headers { get; }
+        public string HttpStatusCode { get; } //// TODO this should be strongly typed
+        public IEnumerable<HttpHeader> Headers { get; } //// TODO make sure `httpheader` properly validates; another option would be to make it very strongly typed //// TODO actually, should there be "odataheader"s? this wouldn't just be headers specific to odata (like odataversion), but also headers that odata takes a strong opinion on (like accept headers)
         public IEnumerable<OdataProperty> Properties { get; }
     }
 
@@ -22,6 +22,8 @@
     {
         public OdataProperty(string name, string value)
         {
+            //// TODO this needs to either validate `name` and `value`, or it needs to make them strongly typed
+
             Name = name;
             Value = value;
         }
