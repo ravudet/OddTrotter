f64e9a05b94c19ceb44034f3894b89eb8162df60
ravudet <170215607+ravudet@users.noreply.github.com>
2026-02-05 11:44:41 -0800

    sadf

--- OddTrotter.Core.V3/OddTrotter/Odata/v4_01/WeakConventionContext/IWeakConventionContext.cs
+++ OddTrotter.Core.V3/OddTrotter/Odata/v4_01/WeakConventionContext/IWeakConventionContext.cs
@@ -1,5 +1,6 @@
 ﻿namespace OddTrotter.Odata.v4_01.WeakConventionContext
 {
+    using System.Net.Http;
     using System.Threading.Tasks;
 
     /// <summary>
@@ -18,6 +19,10 @@
         /// </summary>
         /// <param name="request"></param>
         /// <returns></returns>
+        /// <exception cref="WriteException">Thrown if an error occurred writing to the underlying stream</exception>
+        /// <exception cref="HttpRequestException">Thrown if an error occurred trasmitting data between the client and the service</exception> //// TODO do you want to split this into 2 exceptions, one for read and one for write? //// TODO i'm not sure you can always differentiate, and if you can, i'm not sure there is an actionable difference
+        /// <exception cref="ReadException">Thrown if an error occurred reading from the underlying stream</exception>
+        /// <exception cref="WeakConventionContext">Thrown if the underlying response payload is not valid OData or does not represent a collection response</exception>
         Task<GetCollectionResponse> GetCollection(GetCollectionRequest request);
     }
 }
