1c4f76678128ffe0a18d4fd74b267063108bf546
ravudet <170215607+ravudet@users.noreply.github.com>
2026-02-12 08:05:59 -0800

    asdf

--- OddTrotter.Core.V3.Tests/Fx/ClassEitherUnitTests.cs
+++ OddTrotter.Core.V3.Tests/Fx/ClassEitherUnitTests.cs
@@ -43,7 +43,6 @@
     //// TODO i don't know if i like `calendareventdeserializer.extensions.try`; get this right as part of the "bare minimum" before moving forward
     //// TODO move any extensions or helpers to their appropriate "production" places
     //// TODO you implemented the odata interfaces, in service of the next TODO item; you now need to do the graph related stuff
-        //// TODO you have "done" graph.CalendarEventsContext, but you need the filter option stuff too
         //// TODO then you need to do the calendar events *source*
         //// TODO then you need to do the odd trotter context
     //// TODO MAKE SURE TO GO ALL THE WAY TO THE UI LEVEL!
