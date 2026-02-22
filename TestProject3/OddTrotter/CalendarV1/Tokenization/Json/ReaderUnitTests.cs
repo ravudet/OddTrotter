3b83385eefd52f3c9251a652e3ba5e0542a2130f
ravudet <170215607+ravudet@users.noreply.github.com>
2026-02-22 12:43:00 -0800

    asdf

--- TestProject3/OddTrotter/CalendarV1/Tokenization/Json/ReaderUnitTests.cs
+++ TestProject3/OddTrotter/CalendarV1/Tokenization/Json/ReaderUnitTests.cs
@@ -282,9 +282,6 @@
         [TestMethod]
         public async Task V2Broad()
         {
-            //// TODO consolidate the `readtoend` implementations
-            //// TODO it feels like the readtoend implementations are *too* consolidated...
-
             //// TODO implement buffers for readers
             //// TODO change reader interface so that async is only used when the buffer is expanded
             //// TODO change reader to use ref struct somehow (maybe there's a step between `trymove` and `ref struct` that is just `struct`
