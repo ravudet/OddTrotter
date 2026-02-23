1f164ecf4b3dd3992f25ec846f8b4c409551f2c2
ravudet <170215607+ravudet@users.noreply.github.com>
2026-02-22 19:32:02 -0800

    asdf

--- OddTrotter.Core/OddTrotter/CalendarV1/Tokenization/Json2/JsonReader.cs
+++ OddTrotter.Core/OddTrotter/CalendarV1/Tokenization/Json2/JsonReader.cs
@@ -1407,37 +1407,48 @@
 
     public sealed class SubsequentArrayElementsReader<TNextReader> : IReader<SubsequentArrayElementsToken<TNextReader>>
     {
-        private readonly PeekableStream stream;
+        private readonly Stream stream;
         private readonly byte[] buffer;
-        private readonly int validBytes;
-        private readonly Func<PeekableStream, byte[], int, TNextReader> nextReaderFactory;
+        private int currentByteIndex;
+        private int validBytes;
+        private readonly Func<Stream, byte[], int, int, TNextReader> nextReaderFactory;
 
         public SubsequentArrayElementsReader(
-            PeekableStream stream,
+            Stream stream,
             byte[] buffer,
+            int currentByteIndex,
             int validBytes,
-            Func<PeekableStream, byte[], int, TNextReader> nextReaderFactory)
+            Func<Stream, byte[], int, int, TNextReader> nextReaderFactory)
         {
             this.stream = stream;
             this.buffer = buffer;
+            this.currentByteIndex = currentByteIndex;
             this.validBytes = validBytes;
             this.nextReaderFactory = nextReaderFactory;
         }
 
         public async ITask<SubsequentArrayElementsToken<TNextReader>> Move()
         {
-            var peeked = await this.stream.PeekAsync().ConfigureAwait(false);
-            if (peeked == null)
+            if (this.currentByteIndex >= this.validBytes)
+            {
+                this.validBytes = await this.stream.ReadAsync(this.buffer, 0, this.buffer.Length).ConfigureAwait(false);
+                this.currentByteIndex = 0;
+            }
+
+            if (this.validBytes == 0)
             {
                 throw new Exception("TODO invalid JSON");
             }
 
-            if (peeked != ',')
+            var currentByte = this.buffer[this.currentByteIndex];
+            ++this.currentByteIndex;
+            if (currentByte != ',')
             {
                 return new SubsequentArrayElementsToken<TNextReader>.None(
                     this.nextReaderFactory(
                         this.stream,
                         this.buffer,
+                        this.currentByteIndex,
                         this.validBytes));
             }
 
@@ -1483,19 +1494,22 @@
 
     public sealed class SubsequentArrayElementReader<TNextReader> : IReader<CommaReader<WhitespaceReader<ArrayElementReader<TNextReader>>>>
     {
-        private readonly PeekableStream stream;
+        private readonly Stream stream;
         private readonly byte[] buffer;
-        private readonly int validBytes;
-        private readonly Func<PeekableStream, byte[], int, TNextReader> nextReaderFactory;
+        private int currentByteIndex;
+        private int validBytes;
+        private readonly Func<Stream, byte[], int, int, TNextReader> nextReaderFactory;
 
         public SubsequentArrayElementReader(
-            PeekableStream stream,
+            Stream stream,
             byte[] buffer,
+            int currentByteIndex,
             int validBytes,
-            Func<PeekableStream, byte[], int, TNextReader> nextReaderFactory)
+            Func<Stream, byte[], int, int, TNextReader> nextReaderFactory)
         {
             this.stream = stream;
             this.buffer = buffer;
+            this.currentByteIndex = currentByteIndex;
             this.validBytes = validBytes;
             this.nextReaderFactory = nextReaderFactory;
         }
@@ -1506,14 +1520,17 @@
                 new CommaReader<WhitespaceReader<ArrayElementReader<TNextReader>>>(
                     this.stream,
                     this.buffer,
+                    this.currentByteIndex,
                     this.validBytes,
-                    (stream, buffer, validBytes) => new WhitespaceReader<ArrayElementReader<TNextReader>>(
+                    (stream, buffer, currentByteIndex, validBytes) => new WhitespaceReader<ArrayElementReader<TNextReader>>(
                         stream,
                         buffer,
+                        currentByteIndex,
                         validBytes,
-                        (stream, buffer, validBytes) => new ArrayElementReader<TNextReader>(
+                        (stream, buffer, currentByteIndex, validBytes) => new ArrayElementReader<TNextReader>(
                             stream,
                             buffer,
+                            currentByteIndex,
                             validBytes,
                             this.nextReaderFactory))))
                 .ConfigureAwait(false);
