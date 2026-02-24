562ece760bb3e397d9c2f8ccfb45d286e2f913d0
ravudet <170215607+ravudet@users.noreply.github.com>
2026-02-24 15:49:48 -0800

    asdf

--- TestProject3/OddTrotter/CalendarV1/Tokenization/Json/ReaderUnitTests.cs
+++ TestProject3/OddTrotter/CalendarV1/Tokenization/Json/ReaderUnitTests.cs
@@ -323,7 +323,7 @@
         {
             //// TODO `move` implementations should also be single-execution
             
-            //// TODO change reader interface so that async is only used when the buffer is expanded //// TODO you are at nullreader
+            //// TODO change reader interface so that async is only used when the buffer is expanded //// TODO you are at object reader
             //// TODO they shouldn't be allowed to call `read` unless `false` was previously returned
             //// TODO the `trygetvalue` implementations need to follow the whitespace pattern of `finished
             
