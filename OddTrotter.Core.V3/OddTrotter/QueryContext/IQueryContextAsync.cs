7895f01a9b4d0b840b20fb3348b9fc02af75f404
ravudet <170215607+ravudet@users.noreply.github.com>
2026-02-27 06:38:51 -0800

    asdf

--- OddTrotter.Core.V3/OddTrotter/QueryContext/IQueryContextAsync.cs
+++ OddTrotter.Core.V3/OddTrotter/QueryContext/IQueryContextAsync.cs
@@ -9,6 +9,7 @@ namespace Fx.QueryContext
         /// placeholder
         /// </summary>
         /// <returns></returns>
+        /// <exception cref="EvaluationException{TError}">Thrown if the initial call to the backing data store resulting in a <typeparamref name="TError"/></exception>
         ITask<IQueryResultAsync<TClientValue, TError>> Evaluate();
     }
 }
---
+++ OddTrotter.Core.V3/OddTrotter/QueryContext/ReadException.cs
@@ -0,0 +1,17 @@
+﻿namespace Fx.QueryContext
+{
+    using System;
+
+    internal sealed class EvaluationException<T> : Exception
+    {
+        public EvaluationException(string message)
+            : base(message)
+        {
+        }
+
+        public EvaluationException(string message, Exception exception)
+            : base(message, exception)
+        {
+        }
+    }
+}
