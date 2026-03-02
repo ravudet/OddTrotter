17fd36edb0f30f7b9094005addae5265cb1f6588
ravudet <170215607+ravudet@users.noreply.github.com>
2026-03-02 05:33:18 -0800

    asdf

--- OddTrotter.Core.V3/OddTrotter/Odata/v4_01/ProtocolContext/OdataRequest.cs
+++ OddTrotter.Core.V3/OddTrotter/Odata/v4_01/ProtocolContext/OdataRequest.cs
@@ -1,20 +1,22 @@
 ﻿namespace OddTrotter.Odata.v4_01.ProtocolContext
 {
+    using System;
     using System.Collections.Generic;
+    using System.Net.Http;
 
     using OddTrotter.Calendar;
 
     internal sealed class OdataRequest
     {
-        internal OdataRequest(string httpVerb, string url, IEnumerable<HttpHeader> headers)
+        internal OdataRequest(HttpMethod httpMethod, Uri uri, IEnumerable<HttpHeader> headers)
         {
-            HttpVerb = httpVerb;
-            Url = url;
+            HttpMethod = httpMethod;
+            Uri = uri;
             Headers = headers;
         }
 
-        internal string HttpVerb { get; }       
-        internal string Url { get; }
+        internal HttpMethod HttpMethod { get; }       
+        internal Uri Uri { get; }
         public IEnumerable<HttpHeader> Headers { get; }
     }
 }
