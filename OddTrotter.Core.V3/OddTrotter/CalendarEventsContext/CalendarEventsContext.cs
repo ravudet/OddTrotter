4b1ccf027ed8273537e1d45ac8f83d7efc58d350
ravudet <170215607+ravudet@users.noreply.github.com>
2026-03-08 15:48:02 -0700

    asdf

--- OddTrotter.Core.V3/OddTrotter/CalendarEventsContext/CalendarEventsContext.cs
+++ OddTrotter.Core.V3/OddTrotter/CalendarEventsContext/CalendarEventsContext.cs
@@ -187,7 +187,7 @@
                             {
                                 //// TODO you are here
 
-                                //// TODO you should actually get the first *non-error* instance; however, keep in mind that, for an unending series, if there's a bug in deserializing, you won't ever first a non-error instance
+                                //// TODO you should actually get the first *non-error* instance; however, keep in mind that, for an unending series, if there's a bug in deserializing, you won't ever find a non-error instance
                                 var instances = await this.GetInstancesInSeries(seriesMaster.Id).ConfigureAwait(false);
                                 return (SeriesMaster: seriesMaster, PotentialFirstInstance: await instances.FirstOrDefault(new Nothing()).SelectLeft(_ => _.AsEither()).ConfigureAwait(false)); //// TODO you need aseither because `foo2` below uses tuples which don't have covariance
                             })
