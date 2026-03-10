549d6ca722dcd31030b1cd30fd51b077cc735399
ravudet <170215607+ravudet@users.noreply.github.com>
2026-03-10 08:41:17 -0700

    asdf

--- TestProject3/OddTrotter/CalendarV1/Tokenization/Json/ReaderUnitTests.cs
+++ TestProject3/OddTrotter/CalendarV1/Tokenization/Json/ReaderUnitTests.cs
@@ -1088,6 +1088,7 @@
             //// TODO the `trygetvalue` implementations need to follow the whitespace pattern of `finished`
 
 
+            //// TODO it's in an infinite loop
             //// TODO "unit" readers like `objectreader` should have `trygetvalue` which returns the "known reader" chain, and then `trymove` *only* returns the "next reader"
             //// TODO change reader to use ref struct somehow (maybe there's a step between `trymove` and `ref struct` that is just `struct`
             //// TODO make sure the check the perf after the fact; it should get better with the ref structs, right?
