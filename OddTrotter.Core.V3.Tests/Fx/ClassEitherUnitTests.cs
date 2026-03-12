9e0094cead32073418b1f2826a12c736b57fc9b4
ravudet <170215607+ravudet@users.noreply.github.com>
2026-03-12 06:03:50 -0700

    sdf

--- OddTrotter.Core.V3.Tests/Fx/ClassEitherUnitTests.cs
+++ OddTrotter.Core.V3.Tests/Fx/ClassEitherUnitTests.cs
@@ -16,7 +16,7 @@
 
 
     //// TODO do you actually like the way that all of the code looks in the v3 oddtrotter stuff?
-    ////    TODO OddTrotter.graph.CalendarEventsContext.calendareventscontext
+    ////    TODO OddTrotter.linq.EnumerableExtensions
     //// TODO delete any newly dead code
 
 
