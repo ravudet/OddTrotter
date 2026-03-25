e3e20fb2f44254beaab85ff8854b6d46357a8754
ravudet <170215607+ravudet@users.noreply.github.com>
2026-03-25 07:52:08 -0700

    asdf

--- TestProject3/OddTrotter/CalendarV1/Tokenization/Json/ReaderUnitTests.cs
+++ TestProject3/OddTrotter/CalendarV1/Tokenization/Json/ReaderUnitTests.cs
@@ -1483,12 +1483,12 @@
             //var value4 = await whitespace10.AsReader.MoveInternal3(whitespace10.Factory).ConfigureAwait(false);
             var value4 = await whitespace10.MoveInternal1().ConfigureAwait(false);
             var valueToken4 = await value4.MoveInternal1().ConfigureAwait(false);
-            /*if (!(valueToken4 is ValueToken<SubsequentMembersReader<WhitespaceReader<ObjectEndReader<WhitespaceReader<Nothing>>>>>.Number number))
+            if (!(valueToken4 is ValueToken<SubsequentMembersReader<WhitespaceReader<ObjectEndReader<WhitespaceReader2<Nothing>>>>>.Number number))
             {
                 throw new Exception("TODO");
             }
 
-            var sign = await number.Reader.MoveInternal1().ConfigureAwait(false);
+            /*var sign = await number.Reader.MoveInternal1().ConfigureAwait(false);
             var @int = await sign.MoveInternal1().ConfigureAwait(false);
             var frac = await @int.MoveInternal1().ConfigureAwait(false);
             var exp = await frac.MoveInternal1().ConfigureAwait(false);
