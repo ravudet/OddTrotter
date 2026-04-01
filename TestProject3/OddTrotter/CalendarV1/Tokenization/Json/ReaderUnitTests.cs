cfc97fef512120cdc7038776ea42d1b921bef7ef
ravudet <170215607+ravudet@users.noreply.github.com>
2026-04-01 08:38:43 -0700

    asdf

--- TestProject3/OddTrotter/CalendarV1/Tokenization/Json/ReaderUnitTests.cs
+++ TestProject3/OddTrotter/CalendarV1/Tokenization/Json/ReaderUnitTests.cs
@@ -1805,12 +1805,21 @@
             //var value4 = await whitespace10.AsReader.MoveInternal3(whitespace10.Factory).ConfigureAwait(false);
             var value4 = await whitespace10.MoveInternal1().ConfigureAwait(false);
             var valueToken4 = await value4.MoveInternal1().ConfigureAwait(false);
-            if (!(valueToken4 is ValueToken<SubsequentMembersReader<WhitespaceReader<ObjectEndReader<WhitespaceReader2<Nothing>>>>>.Number number))
+            if (!valueToken4
+                .Apply(
+                    @false => default,
+                    @null => default,
+                    @true => default,
+                    @object => default,
+                    array => default,
+                    number => RefNullable.Create(number),
+                    @string => default)
+                .TryGetValue(out var number))
             {
                 throw new Exception("TODO");
             }
 
-            var sign = await number.Reader.MoveInternal1().ConfigureAwait(false);
+            var sign = await number.MoveInternal1().ConfigureAwait(false);
             var @int = await sign.MoveInternal1().ConfigureAwait(false);
             var frac = await @int.MoveInternal1().ConfigureAwait(false);
             var exp = await frac.MoveInternal1().ConfigureAwait(false);
@@ -1851,12 +1860,21 @@
             //var value5 = await whitespace13.AsReader.MoveInternal3(whitespace13.Factory).ConfigureAwait(false);
             var value5 = await whitespace13.MoveInternal1().ConfigureAwait(false);
             var valueToken5 = await value5.MoveInternal1().ConfigureAwait(false);
-            if (!(valueToken5 is ValueToken<SubsequentMembersReader<WhitespaceReader<ObjectEndReader<WhitespaceReader2<Nothing>>>>>.String @string5))
+            if (!valueToken5
+                .Apply(
+                    @false => default,
+                    @null => default,
+                    @true => default,
+                    @object => default,
+                    array => default,
+                    number => default,
+                    @string => RefNullable.Create(@string))
+                .TryGetValue(out var @string5))
             {
                 throw new Exception("TODO");
             }
 
-            var stringDelimiter9 = await @string5.Reader.MoveInternal1().ConfigureAwait(false);
+            var stringDelimiter9 = await @string5.MoveInternal1().ConfigureAwait(false);
             var chars5 = await stringDelimiter9.MoveInternal1().ConfigureAwait(false);
             var stringDelimiter10 = await chars5.MoveInternal1().ConfigureAwait(false);
             var subsequentMembers4 = await stringDelimiter10.MoveInternal1().ConfigureAwait(false);
