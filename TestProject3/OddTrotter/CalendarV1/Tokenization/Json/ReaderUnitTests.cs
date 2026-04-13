c9d861bae618121f3c0ef874cd087119c4c9eb12
ravudet <170215607+ravudet@users.noreply.github.com>
2026-04-13 07:30:15 -0700

    asdf

--- TestProject3/OddTrotter/CalendarV1/Tokenization/Json/ReaderUnitTests.cs
+++ TestProject3/OddTrotter/CalendarV1/Tokenization/Json/ReaderUnitTests.cs
@@ -1621,6 +1621,7 @@
             //// TODO after updating to valuetask and stuff, you are now generally at 207, as low as 206
         
             //// TODO you are realizing the the "nextfactory" can't be re-used to create the reader because, for something like whitespace or false (and a bunch more), there's state in the current reader instance (like the whitespaces that have already been read or which character in "false" we are at)
+            //// TODO once you've removed all of the state, the only thing left is the next reader factory, which itself can be removed if you have a generic type constraint requiring a `new` default constructor
             //// TODO does readercontext.read2() make sense now with valuetask?
 
             //// TODO "unit" readers like `objectreader` should have `trygetvalue` which returns the "known reader" chain, and then `trymove` *only* returns the "next reader"
