9b6ceeb5a4f0f4cafd781193d3efa54c18243338
ravudet <170215607+ravudet@users.noreply.github.com>
2026-05-12 15:08:27 -0700

    asdf

--- OddTrotter.Core/OddTrotter/CalendarV1/Tokenization/Json5/Extensions.cs
+++ OddTrotter.Core/OddTrotter/CalendarV1/Tokenization/Json5/Extensions.cs
@@ -2,7 +2,20 @@
 {
     using System.Threading.Tasks;
 
+    using OddTrotter.CalendarV1.Tokenization.Json6;
+
     public static partial class Extensions
     {
+        public static ValueTask<TNextReader> Move<TNextReader>(
+            this WhitespaceReader<TNextReader> whitespaceReader, 
+            ref ReaderContext readerContext)
+        {
+            if (whitespaceReader.TryMove(ref readerContext, out var nextReader, out _))
+            {
+                return ValueTask.FromResult(nextReader);
+            }
+
+            // can't use `valuetask`, have to roll your own; which makes abstraction with a static interface very attractive
+        }
     }
 }
