3fcc11c84115121938a4664e6209e66a9b6eb297
ravudet <170215607+ravudet@users.noreply.github.com>
2026-05-22 08:45:07 -0700

    asdf

--- TestProject3/OddTrotter/CalendarV1/Tokenization/Json/ReaderUnitTests.cs
+++ TestProject3/OddTrotter/CalendarV1/Tokenization/Json/ReaderUnitTests.cs
@@ -2880,8 +2880,8 @@
             var subsequentMembersToken10 = await subsequentMembers10.Move31(context).ConfigureAwait(false);
             Assert.IsTrue(subsequentMembersToken10.TryNone(out var whitespace37));
             //var objectEnd2 = await whitespace37.AsReader.MoveInternal3(whitespace37.Factory).ConfigureAwait(false);
-            /*(context, var objectEnd2) = await whitespace37.Move2(context).ConfigureAwait(false);
-            var whitespace38 = await objectEnd2.Move4(context).ConfigureAwait(false);
+            (context, var objectEnd2) = await whitespace37.Move2(context).ConfigureAwait(false);
+            /*var whitespace38 = await objectEnd2.Move4(context).ConfigureAwait(false);
             //// TODO remove this when you fix moveinternal3
             ////context.Read().ConfigureAwait(false).GetAwaiter().GetResult();
             (context, var nothing) = await whitespace38.Move2(context).ConfigureAwait(false);
@@ -3774,8 +3774,8 @@
                 _ => _,
                 _ => throw new Exception("TODO"));
             //var objectEnd2 = await whitespace37.AsReader.MoveInternal3(whitespace37.Factory).ConfigureAwait(false);
-            /*var objectEnd2 = await whitespace37.MoveInternal1().ConfigureAwait(false);
-            var whitespace38 = await objectEnd2.MoveInternal1().ConfigureAwait(false);
+            var objectEnd2 = await whitespace37.MoveInternal1().ConfigureAwait(false);
+            /*var whitespace38 = await objectEnd2.MoveInternal1().ConfigureAwait(false);
             //// TODO remove this when you fix moveinternal3
             context.Read().ConfigureAwait(false).GetAwaiter().GetResult();
             var nothingFactory = await whitespace38.AsReader.MoveInternal3(ref context, Foo).ConfigureAwait(false);
