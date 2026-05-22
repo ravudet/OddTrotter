341c5864453349c6746fc8f3af3098ed2b82e4e5
ravudet <170215607+ravudet@users.noreply.github.com>
2026-05-22 09:39:42 -0700

    asdf

--- OddTrotter.Core.V3.Tests/Playground/TopLayer/Odata/IMetadataSource.cs
+++ OddTrotter.Core.V3.Tests/Playground/TopLayer/Odata/IMetadataSource.cs
@@ -2,14 +2,21 @@
 {
     using System.Threading.Tasks;
 
+    //// TODO you are here
+    //// TODO finish this file, including todos
+
     public interface IMetadataSource<TSchemaVersion>
     {
         IMetadataContext<TSchemaVersion> Get();
+
+        //// TODO other verbs
     }
 
     public interface IMetadataContext<TSchemaVersion>
     {
-        ITask<MetadataDto> Evaluate();
+        ITask<MetadataDto> Evaluate(); //// TODO can't just return the dto, need to have control information, headers, etc.
+
+        //// TODO headers
 
         IMetadataContext<TSchemaVersion> SchemaVersion(TSchemaVersion schemaVersion); //// TODO lots of implications, read through this: https://docs.oasis-open.org/odata/odata/v4.01/odata-v4.01-part1-protocol.html#sec_SystemQueryOptionschemaversion
     }
