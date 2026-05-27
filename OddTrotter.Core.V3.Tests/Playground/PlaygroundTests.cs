ee5e184260565a039c53286a05ea1b79ea66f48d
ravudet <170215607+ravudet@users.noreply.github.com>
2026-05-27 10:51:43 -0700

    asdf

--- OddTrotter.Core.V3.Tests/Playground/PlaygroundTests.cs
+++ OddTrotter.Core.V3.Tests/Playground/PlaygroundTests.cs
@@ -719,8 +719,7 @@ namespace Playground
                 // true
                 var memberReader = firstMemberReader.MoveTry1(context);
                 var whitespaceReader3 = memberReader.MoveTry1(context);
-                var whitespaceReader4 = whitespaceReader3.MoveTry2(context);
-                var valueReader2 = whitespaceReader4.MoveTry2(context);
+                var valueReader2 = whitespaceReader3.MoveTry2(context);
                 var valueToken2 = valueReader2.MoveTry3(context);
                 Assert.IsFalse(valueToken2.TryTrue(out var @true)); //// TODO should be true
             }
@@ -1120,9 +1119,9 @@ namespace Playground
         }
     }
 
-    public sealed class MemberReader<TNextReader> : IMoveReader<MemberReader<TNextReader>, WhitespaceReader<WhitespaceReader<ValueReader<TNextReader>>>>
+    public sealed class MemberReader<TNextReader> : IMoveReader<MemberReader<TNextReader>, WhitespaceReader<ValueReader<TNextReader>>>
     {
-        public static bool TryMove(ReaderContext readerContext, out WhitespaceReader<WhitespaceReader<ValueReader<TNextReader>>> nextReader)
+        public static bool TryMove(ReaderContext readerContext, out WhitespaceReader<ValueReader<TNextReader>> nextReader)
         {
             nextReader = default!; //// TODO !
             return true;
