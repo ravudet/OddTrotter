89bc42c68fc0d40a026ffffee3597d9732f58551
ravudet <170215607+ravudet@users.noreply.github.com>
2026-06-05 10:21:35 -0700

    starting to implement the full design now

---
+++ OddTrotter.Core/Fx/Parsing/Reader/Context.cs
@@ -0,0 +1,32 @@
+﻿namespace Fx.Parsing.Reader
+{
+    using System.IO;
+    using System.Threading.Tasks;
+
+    public sealed class Context
+    {
+        private Context(
+            Stream stream,
+            byte[] buffer,
+            int currentByteIndex,
+            int validBytes)
+        {
+            Stream = stream;
+            Buffer = buffer;
+            CurrentByteIndex = currentByteIndex;
+            ValidBytes = validBytes;
+        }
+
+        public Stream Stream { get; }
+        public byte[] Buffer { get; }
+        public int CurrentByteIndex { get; set; }
+        public int ValidBytes { get; set; }
+
+        public static async Task<Context> FromStream(Stream stream, byte[] buffer)
+        {
+            var context = new Context(stream, buffer, 0, 0);
+            await context.Read().ConfigureAwait(false);
+            return context;
+        }
+    }
+}
---
+++ OddTrotter.Core/Fx/Parsing/Reader/ContextExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace Fx.Parsing.Reader
+{
+    using System.Threading.Tasks;
+
+    public static class ContextExtensions
+    {
+        public static async ValueTask Read(this Context context)
+        {
+            context.ValidBytes = await context.Stream.ReadAsync(context.Buffer).ConfigureAwait(false);
+            context.CurrentByteIndex = 0;
+        }
+    }
+}
