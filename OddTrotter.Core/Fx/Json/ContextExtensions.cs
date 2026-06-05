b95753f07ad79f7c3df4562ebc03fad7bcde05bb
ravudet <170215607+ravudet@users.noreply.github.com>
2026-06-05 10:41:47 -0700

    asdf

---
+++ OddTrotter.Core/Fx/Json/ContextExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace Fx.Json
+{
+    using Fx.Parsing.Reader;
+
+    public static class ContextExtensions
+    {
+        public static Reader? Json(this Context context)
+        {
+            return null;
+        }
+    }
+}
