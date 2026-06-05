fcc1ef9fcaf9dd4f2b7a643aedf4ebc7f060135f
ravudet <170215607+ravudet@users.noreply.github.com>
2026-06-05 10:50:07 -0700

    asdf

--- OddTrotter.Core/Fx/Json/ValueReader.cs
+++ OddTrotter.Core/Fx/Json/ValueReader.cs
@@ -1,6 +1,89 @@
 ﻿namespace Fx.Json
 {
-    public sealed class ValueReader<TNextReader>
+    using System.Diagnostics.CodeAnalysis;
+
+    using Fx.Parsing.Reader;
+
+    public sealed class ValueReader<TNextReader> : ICategoryReader<ValueReader<TNextReader>, ValueCategory<TNextReader>>
     {
+        public static bool TryMove(Context context, [MaybeNullWhen(false)] out ValueCategory<TNextReader> category)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public readonly ref struct ValueCategory<TNextReader>
+    {
+        private enum Type
+        {
+            False = 1, // since 0 is the default value, and we need to be able to distinguish an uninitialized instance
+            Null,
+            True,
+            Object,
+            Array,
+            Number,
+            String,
+        }
+
+        private Type type { get; init; }
+
+        public static ValueCategory<TNextReader> False()
+        {
+            return new ValueCategory<TNextReader>()
+            {
+                type = Type.False,
+            };
+        }
+
+        public static ValueCategory<TNextReader> Null()
+        {
+            return new ValueCategory<TNextReader>()
+            {
+                type = Type.Null,
+            };
+        }
+
+        public static ValueCategory<TNextReader> True()
+        {
+            return new ValueCategory<TNextReader>()
+            {
+                type = Type.True,
+            };
+        }
+
+        public static ValueCategory<TNextReader> Object()
+        {
+            return new ValueCategory<TNextReader>()
+            {
+                type = Type.Object,
+            };
+        }
+
+        public static ValueCategory<TNextReader> Array()
+        {
+            return new ValueCategory<TNextReader>()
+            {
+                type = Type.Array,
+            };
+        }
+
+        public static ValueCategory<TNextReader> Number()
+        {
+            return new ValueCategory<TNextReader>()
+            {
+                type = Type.Number,
+            };
+        }
+
+        public static ValueCategory<TNextReader> String()
+        {
+            return new ValueCategory<TNextReader>()
+            {
+                type = Type.String,
+            };
+        }
+
+        public TResult Apply<TResult>
+            
     }
 }
--- OddTrotter.Core/Fx/Parsing/Reader/ICategoryReader.cs
+++ OddTrotter.Core/Fx/Parsing/Reader/ICategoryReader.cs
@@ -4,6 +4,7 @@ namespace Fx.Parsing.Reader
 {
     public interface ICategoryReader<out TSelf, TCategory>
         where TSelf : ICategoryReader<TSelf, TCategory>
+        where TCategory : allows ref struct
     {
         static abstract bool TryMove(Context context, [MaybeNullWhen(false)] out TCategory category);
     }
