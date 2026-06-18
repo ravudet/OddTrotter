2f66ff4cb7d9ccbd6d3f5f16f820d8f35435f1df
ravudet <170215607+ravudet@users.noreply.github.com>
2026-06-18 09:22:39 -0700

    asdf

--- OddTrotter.Core/Fx/Json/MembersReader.cs
+++ OddTrotter.Core/Fx/Json/MembersReader.cs
@@ -1,52 +1,6 @@
-﻿using System;
-
-namespace Fx.Json
+﻿namespace Fx.Json
 {
     public sealed class MembersReader<TNextReader>
     {
     }
-
-    public readonly ref struct MembersCategory<TNextReader>
-    {
-        private enum Type
-        {
-            None = 1,
-            Some,
-        }
-
-        private Type type { get; init; }
-
-        public static MembersCategory<TNextReader> None()
-        {
-            return new MembersCategory<TNextReader>()
-            {
-                type = Type.None,
-            };
-        }
-
-        public static MembersCategory<TNextReader> Some()
-        {
-            return new MembersCategory<TNextReader>()
-            {
-                type = Type.Some,
-            };
-        }
-
-        public TResult Apply<TResult>(
-            Func<TNextReader?, TResult> noneReader,
-            Func<FirstMemberReader<TNextReader>?, TResult> someReader)
-        {
-            switch (this.type)
-            {
-                case Type.None:
-                    return noneReader(default);
-            }
-        }
-    }
-
-
-    public sealed class FirstMemberReader<TNextReader>
-    {
-    }
-
 }
