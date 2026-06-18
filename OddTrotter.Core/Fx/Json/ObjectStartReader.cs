aa1162a4fec80addcd01ab166e2f7af8749a5182
ravudet <170215607+ravudet@users.noreply.github.com>
2026-06-18 08:18:37 -0700

    asdf

--- OddTrotter.Core/Fx/Json/ObjectStartReader.cs
+++ OddTrotter.Core/Fx/Json/ObjectStartReader.cs
@@ -1,6 +1,20 @@
 ﻿namespace Fx.Json
 {
-    public sealed class ObjectStartReader<TNextReader>
+    using System.Diagnostics.CodeAnalysis;
+
+    using Fx.Parsing.Reader;
+
+    public sealed class ObjectStartReader<TNextReader> : IValueReader<ObjectStartReader<TNextReader>, TNextReader, ObjectStartToken>
     {
+        public static bool TryMove(Context context, out TNextReader? nextReader, [MaybeNullWhen(false)] out ObjectStartToken value)
+        {
+            Helpers.EnsureValidBytes(context);
+            if (Helpers.NeedsMoreBytes(context, out nextReader, out value))
+            {
+                return false;
+            }
+
+            return Helpers.TryReadChar(context, '{');
+        }
     }
 }
---
+++ OddTrotter.Core/Fx/Json/ObjectStartToken.cs
@@ -0,0 +1,6 @@
+﻿namespace Fx.Json
+{
+    public readonly struct ObjectStartToken
+    {
+    }
+}
