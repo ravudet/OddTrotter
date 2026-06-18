abf8665c2a0ed97dbf6a5a191d4c23ae4d9ef875
ravudet <170215607+ravudet@users.noreply.github.com>
2026-06-18 08:05:09 -0700

    asdf

--- OddTrotter.Core/Fx/Json/FalseReader.cs
+++ OddTrotter.Core/Fx/Json/FalseReader.cs
@@ -1,8 +1,6 @@
 ﻿namespace Fx.Json
 {
-    using System;
     using System.Diagnostics.CodeAnalysis;
-    using System.Diagnostics.Contracts;
 
     using Fx.Parsing.Reader;
 
@@ -35,47 +33,5 @@
 
     public readonly struct FalseToken
     {
-    }
-
-    public static class Helpers
-    {
-        public static bool NeedsMoreBytes<TCategory>(
-            Context context,
-            [MaybeNull] out TCategory category)
-            where TCategory : allows ref struct
-        {
-            category = default;
-            return context.CurrentByteIndex >= context.ValidBytes;
-        }
-
-        public static bool NeedsMoreBytes<TNextReader, TValue>(Context context, [MaybeNull] out TNextReader nextReader, [MaybeNull] out TValue value)
-            where TValue : allows ref struct
-        {
-            nextReader = default;
-            value = default;
-            return context.CurrentByteIndex >= context.ValidBytes;
-        }
-
-        public static bool NeedsMoreBytes<TNextReader, TValue, TContinuationToken>(
-            Context context, 
-            TContinuationToken continuationToken,
-            out TNextReader? nextReader,
-            [MaybeNull] out TValue value,
-            out TContinuationToken nextContinuationToken)
-            where TValue : allows ref struct
-        {
-            nextReader = default;
-            value = default;
-            nextContinuationToken = continuationToken;
-            return context.CurrentByteIndex >= context.ValidBytes;
-        }
-
-        public static void EnsureValidBytes(Context context)
-        {
-            if (context.ValidBytes == 0)
-            {
-                throw new InvalidPayloadException("TODO invalid JSON");
-            }
-        }
     }
 }
---
+++ OddTrotter.Core/Fx/Json/Helpers.cs
@@ -0,0 +1,48 @@
+﻿namespace Fx.Json
+{
+    using System.Diagnostics.CodeAnalysis;
+
+    using Fx.Parsing.Reader;
+
+    public static class Helpers
+    {
+        public static bool NeedsMoreBytes<TCategory>(
+            Context context,
+            [MaybeNull] out TCategory category)
+            where TCategory : allows ref struct
+        {
+            category = default;
+            return context.CurrentByteIndex >= context.ValidBytes;
+        }
+
+        public static bool NeedsMoreBytes<TNextReader, TValue>(Context context, [MaybeNull] out TNextReader nextReader, [MaybeNull] out TValue value)
+            where TValue : allows ref struct
+        {
+            nextReader = default;
+            value = default;
+            return context.CurrentByteIndex >= context.ValidBytes;
+        }
+
+        public static bool NeedsMoreBytes<TNextReader, TValue, TContinuationToken>(
+            Context context, 
+            TContinuationToken continuationToken,
+            out TNextReader? nextReader,
+            [MaybeNull] out TValue value,
+            out TContinuationToken nextContinuationToken)
+            where TValue : allows ref struct
+        {
+            nextReader = default;
+            value = default;
+            nextContinuationToken = continuationToken;
+            return context.CurrentByteIndex >= context.ValidBytes;
+        }
+
+        public static void EnsureValidBytes(Context context)
+        {
+            if (context.ValidBytes == 0)
+            {
+                throw new InvalidPayloadException("TODO invalid JSON");
+            }
+        }
+    }
+}
