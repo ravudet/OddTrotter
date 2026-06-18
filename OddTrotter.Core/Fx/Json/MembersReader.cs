92e92e543e15ebd24fe8c876772b26e59326f414
ravudet <170215607+ravudet@users.noreply.github.com>
2026-06-18 09:25:00 -0700

    asdf

--- OddTrotter.Core/Fx/Json/MembersReader.cs
+++ OddTrotter.Core/Fx/Json/MembersReader.cs
@@ -1,6 +1,29 @@
 ﻿namespace Fx.Json
 {
-    public sealed class MembersReader<TNextReader>
+    using System.Diagnostics.CodeAnalysis;
+
+    using Fx.Parsing.Reader;
+
+    public sealed class MembersReader<TNextReader> : ICategoryReader<MembersReader<TNextReader>, MembersCategory<TNextReader>>
     {
+        public static bool TryMove(Context context, [MaybeNullWhen(false)] out MembersCategory<TNextReader> category)
+        {
+            Helpers.EnsureValidBytes(context);
+            if (Helpers.NeedsMoreBytes(context, out category))
+            {
+                return false;
+            }
+
+            if (context.Buffer[context.CurrentByteIndex] == '"')
+            {
+                category = MembersCategory<TNextReader>.Some();
+            }
+            else
+            {
+                category = MembersCategory<TNextReader>.None();
+            }
+
+            return true;
+        }
     }
 }
