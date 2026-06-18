a32dcae539372377709de94af6406a7e491c6d9f
ravudet <170215607+ravudet@users.noreply.github.com>
2026-06-18 08:16:13 -0700

    asdf

---
+++ OddTrotter.Core/Fx/Json/ObjectEndReader.cs
@@ -0,0 +1,6 @@
+﻿namespace Fx.Json
+{
+    public sealed class ObjectEndReader<TNextReader>
+    {
+    }
+}
--- OddTrotter.Core/Fx/Json/ObjectReader.cs
+++ OddTrotter.Core/Fx/Json/ObjectReader.cs
@@ -1,6 +1,13 @@
 ﻿namespace Fx.Json
 {
-    public sealed class ObjectReader<TNextReader>
+    using Fx.Parsing.Reader;
+
+    public sealed class ObjectReader<TNextReader> : IMoveReader<ObjectReader<TNextReader>, ObjectStartReader<WhitespaceReader<MembersReader<WhitespaceReader<ObjectEndReader<TNextReader>>>>>>
     {
+        public static bool TryMove(Context context, out ObjectStartReader<WhitespaceReader<MembersReader<WhitespaceReader<ObjectEndReader<TNextReader>>>>>? nextReader)
+        {
+            nextReader = default;
+            return true;
+        }
     }
 }
