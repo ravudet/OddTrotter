8961883a2f8959a7c7f96ee0c76b359d2de38c9c
ravudet <170215607+ravudet@users.noreply.github.com>
2026-06-18 09:41:35 -0700

    asdf

---
+++ OddTrotter.Core/Fx/Json/ArrayElementsCategory.cs
@@ -0,0 +1,47 @@
+﻿namespace Fx.Json
+{
+    using System;
+
+    public readonly ref struct ArrayElementsCategory<TNextReader>
+    {
+        private enum Type
+        {
+            None = 1,
+            Some,
+        }
+
+        private Type type { get; init; }
+
+        public static ArrayElementsCategory<TNextReader> None()
+        {
+            return new ArrayElementsCategory<TNextReader>()
+            {
+                type = Type.None,
+            };
+        }
+
+        public static ArrayElementsCategory<TNextReader> Some()
+        {
+            return new ArrayElementsCategory<TNextReader>()
+            {
+                type = Type.Some,
+            };
+        }
+
+        public TResult Apply<TResult>(
+            Func<TNextReader?, TResult> noneReader,
+            Func<ArrayElementReader<ArrayElementsReader<TNextReader>>?, TResult> someReader)
+        {
+            switch (this.type)
+            {
+                case Type.None:
+                    return noneReader(default);
+                case Type.Some:
+                    return someReader(default);
+                default:
+                    throw new Exception("TODO bug");
+            }
+        }
+    }
+
+}
--- OddTrotter.Core/Fx/Json/ArrayElementsReader.cs
+++ OddTrotter.Core/Fx/Json/ArrayElementsReader.cs
@@ -1,6 +1,29 @@
 ﻿namespace Fx.Json
 {
-    public sealed class ArrayElementsReader<TNextReader>
+    using System.Diagnostics.CodeAnalysis;
+
+    using Fx.Parsing.Reader;
+
+    public sealed class ArrayElementsReader<TNextReader> : ICategoryReader<ArrayElementsReader<TNextReader>, ArrayElementsCategory<TNextReader>>
     {
+        public static bool TryMove(Context context, [MaybeNullWhen(false)] out ArrayElementsCategory<TNextReader> category)
+        {
+            Helpers.EnsureValidBytes(context);
+            if (Helpers.NeedsMoreBytes(context, out category))
+            {
+                return false;
+            }
+
+            if (context.Buffer[context.CurrentByteIndex] == ']')
+            {
+                category = ArrayElementsCategory<TNextReader>.None();
+            }
+            else
+            {
+                category = ArrayElementsCategory<TNextReader>.Some();
+            }
+
+            return true;
+        }
     }
 }
