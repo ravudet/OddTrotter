f688b13f9152dafb761cae8d0032a44719d9c12d
ravudet <170215607+ravudet@users.noreply.github.com>
2026-06-19 09:53:48 -0700

    asdf

--- OddTrotter.Core/Fx/Json/ExpReader.cs
+++ OddTrotter.Core/Fx/Json/ExpReader.cs
@@ -1,6 +1,95 @@
 ﻿namespace Fx.Json
 {
-    public sealed class ExpReader<TNextReader>
+    using System;
+    using System.Diagnostics.CodeAnalysis;
+
+    using Fx.Parsing.Reader;
+
+    public sealed class ExpReader<TNextReader> : ICategoryReader<ExpReader<TNextReader>, ExpCategory<TNextReader>>
+    {
+        public static bool TryMove(Context context, [MaybeNullWhen(false)] out ExpCategory<TNextReader> category)
+        {
+            if (context.ValidBytes == 0)
+            {
+                category = ExpCategory<TNextReader>.Absent();
+                return true;
+            }
+
+            if (Helpers.NeedsMoreBytes(context, out category))
+            {
+                return false;
+            }
+        }
+    }
+
+    public readonly ref struct ExpCategory<TNextReader>
+    {
+        private enum Type
+        {
+            Absent = 1,
+            Present,
+        }
+
+        private Type type { get; init; }
+
+        public static ExpCategory<TNextReader> Absent()
+        {
+            return new ExpCategory<TNextReader>()
+            {
+                type = Type.Absent,
+            };
+        }
+
+        public static ExpCategory<TNextReader> Present()
+        {
+            return new ExpCategory<TNextReader>()
+            {
+                type = Type.Present,
+            };
+        }
+
+        public TResult Apply<TResult>(
+            Func<TNextReader?, TResult> absent,
+            Func<EReader<ExpSignReader<DigitReader<DigitsReader<TNextReader>>>>?, TResult> present)
+        {
+            switch (this.type)
+            {
+                case Type.Absent:
+                    return absent(default);
+                case Type.Present:
+                    return present(default);
+                default:
+                    throw new Exception("TODO bug");
+            }
+        }
+    }
+
+    public sealed class EReader<TNextReader>
+    {
+    }
+
+    public readonly ref struct EToken
+    {
+        public static bool TryCreate(byte digit, out EToken eToken)
+        {
+            if (digit < '1' || digit > '9')
+            {
+                eToken = default;
+                return false;
+            }
+
+            eToken = new EToken(digit);
+            return true;
+        }
+
+        private EToken(byte e)
+        {
+            E = e;
+        }
+        public byte E { get; }
+    }
+
+    public sealed class ExpSignReader<TNextReader>
     {
     }
 }
--- OddTrotter.Core/Fx/Json/ValueReader.cs
+++ OddTrotter.Core/Fx/Json/ValueReader.cs
@@ -4,6 +4,7 @@
 
     using Fx.Parsing.Reader;
 
+    //// TODO address todo in fracreader
     //// TODO you need a todo list somewhere
     //// TODO see if it's better to remove the static interfaces and just use extension methods; maybe generics are getting in the way of the existing extension methods too
 
