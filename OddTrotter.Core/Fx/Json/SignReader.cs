8a57daaa00c19a6ced195e87cbd24f510ee9be91
ravudet <170215607+ravudet@users.noreply.github.com>
2026-06-19 08:25:05 -0700

    asdf

--- OddTrotter.Core/Fx/Json/SignReader.cs
+++ OddTrotter.Core/Fx/Json/SignReader.cs
@@ -1,6 +1,41 @@
 ﻿namespace Fx.Json
 {
-    public sealed class SignReader<TNextReader>
+    using System.Diagnostics.CodeAnalysis;
+
+    using Fx.Parsing.Reader;
+
+    public sealed class SignReader<TNextReader> : IValueReader<SignReader<TNextReader>, TNextReader, SignToken>
     {
+        public static bool TryMove(Context context, out TNextReader? nextReader, [MaybeNullWhen(false)] out SignToken value)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public readonly ref struct SignToken
+    {
+        private enum Type
+        {
+            Absent = 1,
+            Present,
+        }
+
+        private Type type { get; init; }
+
+        public static SignToken Absent()
+        {
+            return new SignToken()
+            {
+                type = Type.Absent,
+            };
+        }
+
+        public static SignToken Present()
+        {
+            return new SignToken()
+            {
+                type = Type.Present,
+            };
+        }
     }
 }
