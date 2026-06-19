0a9d25f2fec19fd401cd6870f98430c06bcf7b30
ravudet <170215607+ravudet@users.noreply.github.com>
2026-06-19 08:10:25 -0700

    asdf

--- OddTrotter.Core/Fx/Json/ArrayEndReader.cs
+++ OddTrotter.Core/Fx/Json/ArrayEndReader.cs
@@ -1,6 +1,20 @@
 ﻿namespace Fx.Json
 {
-    public sealed class ArrayEndReader<TNextReader>
+    using System.Diagnostics.CodeAnalysis;
+
+    using Fx.Parsing.Reader;
+
+    public sealed class ArrayEndReader<TNextReader> : IValueReader<ArrayEndReader<TNextReader>, TNextReader, ArrayEndToken>
     {
+        public static bool TryMove(Context context, out TNextReader? nextReader, [MaybeNullWhen(false)] out ArrayEndToken value)
+        {
+            Helpers.EnsureValidBytes(context);
+            if (Helpers.NeedsMoreBytes(context, out nextReader, out value))
+            {
+                return false;
+            }
+
+            return Helpers.TryReadChar(context, ']');
+        }
     }
 }
