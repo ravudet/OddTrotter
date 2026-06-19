3430fc2611bb083a9afd2367a3ace60a0186bcea
ravudet <170215607+ravudet@users.noreply.github.com>
2026-06-19 09:46:52 -0700

    asdf

---
+++ OddTrotter.Core/Fx/Json/FracCategory.cs
@@ -0,0 +1,46 @@
+﻿namespace Fx.Json
+{
+    using System;
+
+    public readonly ref struct FracCategory<TNextReader>
+    {
+        private enum Type
+        {
+            Absent = 1,
+            Present,
+        }
+
+        private Type type { get; init; }
+
+        public static FracCategory<TNextReader> Absent()
+        {
+            return new FracCategory<TNextReader>()
+            {
+                type = Type.Absent,
+            };
+        }
+
+        public static FracCategory<TNextReader> Present()
+        {
+            return new FracCategory<TNextReader>()
+            {
+                type = Type.Present,
+            };
+        }
+
+        public TResult Apply<TResult>(
+            Func<TNextReader?, TResult> absent,
+            Func<DigitReader<DigitsReader<TNextReader>>?, TResult> present)
+        {
+            switch (this.type)
+            {
+                case Type.Absent:
+                    return absent(default);
+                case Type.Present:
+                    return present(default);
+                default:
+                    throw new Exception("TODO bug");
+            }
+        }
+    }
+}
--- OddTrotter.Core/Fx/Json/FracReader.cs
+++ OddTrotter.Core/Fx/Json/FracReader.cs
@@ -1,6 +1,36 @@
 ﻿namespace Fx.Json
 {
-    public sealed class FracReader<TNextReader>
+    using System.Diagnostics.CodeAnalysis;
+
+    using Fx.Parsing.Reader;
+
+    public sealed class FracReader<TNextReader> : ICategoryReader<FracReader<TNextReader>, FracCategory<TNextReader>>
     {
+        public static bool TryMove(Context context, [MaybeNullWhen(false)] out FracCategory<TNextReader> category)
+        {
+            if (context.ValidBytes == 0)
+            {
+                //// TODO there are other places where ensurevalidbytes isn't called that need to follow this pattern
+                category = FracCategory<TNextReader>.Absent();
+                return true;
+            }
+
+            if (Helpers.NeedsMoreBytes(context, out category))
+            {
+                return false;
+            }
+
+            if (context.Buffer[context.CurrentByteIndex] == '.')
+            {
+                category = FracCategory<TNextReader>.Present();
+                ++context.CurrentByteIndex;
+            }
+            else
+            {
+                category = FracCategory<TNextReader>.Absent();
+            }
+
+            return true;
+        }
     }
 }
