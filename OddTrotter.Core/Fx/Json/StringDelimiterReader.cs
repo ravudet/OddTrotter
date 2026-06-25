82f9d8d3a776bc04b3878bdc20d570b32b1369df
ravudet <170215607+ravudet@users.noreply.github.com>
2026-06-25 09:26:08 -0700

    ASD

---
+++ OddTrotter.Core/Fx/Json/StringDelimiterReader.cs
@@ -0,0 +1,20 @@
+﻿namespace Fx.Json
+{
+    using System.Diagnostics.CodeAnalysis;
+
+    using Fx.Parsing.Reader;
+
+    public sealed class StringDelimiterReader<TNextReader> : IValueReader<StringDelimiterReader<TNextReader>, TNextReader, StringDelimiterToken>
+    {
+        public static bool TryMove(Context context, out TNextReader? nextReader, [MaybeNullWhen(false)] out StringDelimiterToken value)
+        {
+            Helpers.EnsureValidBytes(context);
+            if (Helpers.NeedsMoreBytes(context, out nextReader, out value))
+            {
+                return false;
+            }
+
+            return Helpers.TryReadChar(context, '"');
+        }
+    }
+}
--- OddTrotter.Core/Fx/Json/StringReader.cs
+++ OddTrotter.Core/Fx/Json/StringReader.cs
@@ -1,29 +1,8 @@
 ﻿namespace Fx.Json
 {
     using System;
-    using System.Diagnostics.CodeAnalysis;
-
-    using Fx.Parsing.Reader;
 
     public sealed class StringReader<TNextReader>
-    {
-    }
-
-    public sealed class StringDelimiterReader<TNextReader> : IValueReader<StringDelimiterReader<TNextReader>, TNextReader, StringDelimiterToken>
-    {
-        public static bool TryMove(Context context, out TNextReader? nextReader, [MaybeNullWhen(false)] out StringDelimiterToken value)
-        {
-            Helpers.EnsureValidBytes(context);
-            if (Helpers.NeedsMoreBytes(context, out nextReader, out value))
-            {
-                return false;
-            }
-
-            return Helpers.TryReadChar(context, '"');
-        }
-    }
-
-    public readonly ref struct StringDelimiterToken
     {
     }
 
