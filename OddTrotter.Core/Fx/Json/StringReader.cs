becb346da17d1b6a0ad9081f7f1205b81da94cfc
ravudet <170215607+ravudet@users.noreply.github.com>
2026-06-29 08:18:25 -0700

    asdf

--- OddTrotter.Core/Fx/Json/StringReader.cs
+++ OddTrotter.Core/Fx/Json/StringReader.cs
@@ -256,6 +256,52 @@
     {
     }
 
+    public readonly ref struct EscapableCharCategory<TNextReader>
+    {
+        private enum Type
+        {
+            Unicode = 1,
+            NonUnicode,
+        }
+
+        private Type type { get; init; }
+
+        public static EscapableCharCategory<TNextReader> Unicode()
+        {
+            return new EscapableCharCategory<TNextReader>()
+            {
+                type = Type.Unicode,
+            };
+        }
+
+        public static EscapableCharCategory<TNextReader> NonUnicode()
+        {
+            return new EscapableCharCategory<TNextReader>()
+            {
+                type = Type.NonUnicode,
+            };
+        }
+
+        public TResult Apply<TResult>(
+            Func<UnicodeReader<TNextReader>?, TResult> unicode,
+            Func<NonUnicodeReader<TNextReader>?, TResult> nonUnicode)
+        {
+            switch (this.type)
+            {
+                case Type.Unicode:
+                    return unicode(default);
+                case Type.NonUnicode:
+                    return nonUnicode(default);
+                default:
+                    throw new Exception("TODO bug");
+            }
+        }
+    }
+
+    public sealed class NonUnicodeReader<TNextReader>
+    {
+    }
+
     public sealed class UnicodeReader<TNextReader>
     {
     }
