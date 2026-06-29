7c5f74d5b0b559e3fc3b81224f854f0174fcac66
ravudet <170215607+ravudet@users.noreply.github.com>
2026-06-29 08:20:21 -0700

    asdf

--- OddTrotter.Core/Fx/Json/StringReader.cs
+++ OddTrotter.Core/Fx/Json/StringReader.cs
@@ -252,8 +252,27 @@
         }
     }
 
-    public sealed class EscapableCharReader<TNextReader> //// TODO you are here
+    public sealed class EscapableCharReader<TNextReader> : ICategoryReader<EscapableCharReader<TNextReader>, EscapableCharCategory<TNextReader>>
     {
+        public static bool TryMove(Context context, [MaybeNullWhen(false)] out EscapableCharCategory<TNextReader> category)
+        {
+            Helpers.EnsureValidBytes(context);
+            if (Helpers.NeedsMoreBytes(context, out category))
+            {
+                return false;
+            }
+
+            if (UToken.TryCreate(context.Buffer[context.CurrentByteIndex], out _))
+            {
+                category = EscapableCharCategory<TNextReader>.Unicode();
+            }
+            else
+            {
+                category = EscapableCharCategory<TNextReader>.NonUnicode();
+            }
+
+            return true;
+        }
     }
 
     public readonly ref struct EscapableCharCategory<TNextReader>
@@ -308,6 +327,13 @@
 
     public sealed class UReader<TNextReader>
     {
+    }
+
+    public readonly ref struct UToken
+    {
+        public static bool TryCreate(byte u, out UToken uToken)
+        {
+        }
     }
 
     public sealed class HexDigReader<TNextReader>
