8bf2cda0079036cea19441c666a6d0c505c95319
ravudet <170215607+ravudet@users.noreply.github.com>
2026-06-29 09:29:02 -0700

    asdf

--- OddTrotter.Core/Fx/Json/StringReader.cs
+++ OddTrotter.Core/Fx/Json/StringReader.cs
@@ -403,16 +403,48 @@
         }
     }
 
-    public sealed class HexDigReader<TNextReader>
+    public sealed class HexDigReader<TNextReader> : IValueReader<HexDigReader<TNextReader>, TNextReader, HexDigToken>
     {
+        public static bool TryMove(Context context, out TNextReader? nextReader, [MaybeNullWhen(false)] out HexDigToken value)
+        {
+            Helpers.EnsureValidBytes(context);
+            if (Helpers.NeedsMoreBytes(context, out nextReader, out value))
+            {
+                return false;
+            }
+
+            if (!HexDigToken.TryCreate(context.Buffer[context.CurrentByteIndex], out value))
+            {
+                throw new InvalidPayloadException("TODO");
+            }
+
+            return true;
+        }
     }
 
     public readonly ref struct HexDigToken
     {
         public static bool TryCreate(byte digit, out HexDigToken hexDigToken)
         {
-            if ((digit >= 0x30 && digit <= 0x39) || (digit >= 0x41 && digit <= 0x46)
+            if (
+                (digit >= 0x30 && digit <= 0x39) || 
+                (digit >= 0x41 && digit <= 0x46) ||
+                (digit >= 0x61 && digit <= 0x66))
+            {
+                hexDigToken = new HexDigToken(digit);
+                return true;
+            }
+
+            hexDigToken = default;
+            return false;
         }
+
+        private HexDigToken(byte digit)
+        {
+            Digit = digit;
+        }
+
+        public byte Digit { get; }
     }
 
     public sealed class SubsequentCharsReader<TNextReader>
