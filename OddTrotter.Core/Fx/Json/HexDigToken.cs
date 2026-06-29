9ccc629d6f74518472fa68f8d8fbc5c8649a6e38
ravudet <170215607+ravudet@users.noreply.github.com>
2026-06-29 09:43:31 -0700

    asdf

---
+++ OddTrotter.Core/Fx/Json/HexDigToken.cs
@@ -0,0 +1,27 @@
+﻿namespace Fx.Json
+{
+    public readonly ref struct HexDigToken
+    {
+        public static bool TryCreate(byte digit, out HexDigToken hexDigToken)
+        {
+            if (
+                (digit >= 0x30 && digit <= 0x39) || 
+                (digit >= 0x41 && digit <= 0x46) ||
+                (digit >= 0x61 && digit <= 0x66))
+            {
+                hexDigToken = new HexDigToken(digit);
+                return true;
+            }
+
+            hexDigToken = default;
+            return false;
+        }
+
+        private HexDigToken(byte digit)
+        {
+            Digit = digit;
+        }
+
+        public byte Digit { get; }
+    }
+}
