e573459376a1f31ec61f34b7553102eaa7705dd7
ravudet <170215607+ravudet@users.noreply.github.com>
2026-06-29 08:28:09 -0700

    asdef

--- OddTrotter.Core/Fx/Json/StringReader.cs
+++ OddTrotter.Core/Fx/Json/StringReader.cs
@@ -375,19 +375,44 @@
         }
     }
 
-    public sealed class UReader<TNextReader>
+    public sealed class UReader<TNextReader> : IValueReader<UReader<TNextReader>, TNextReader, UToken>
     {
+        public static bool TryMove(Context context, out TNextReader? nextReader, [MaybeNullWhen(false)] out UToken value)
+        {
+            Helpers.EnsureValidBytes(context);
+            if (Helpers.NeedsMoreBytes(context, out nextReader, out value))
+            {
+                return false;
+            }
+
+            if (!UToken.TryCreate(context.Buffer[context.CurrentByteIndex], out value))
+            {
+                throw new InvalidPayloadException("TODO");
+            }
+
+            return true;
+        }
     }
 
     public readonly ref struct UToken
     {
         public static bool TryCreate(byte u, out UToken uToken)
         {
+            uToken = default;
+            return u == 0x75;
         }
     }
 
     public sealed class HexDigReader<TNextReader>
     {
+    }
+
+    public readonly ref struct HexDigToken
+    {
+        public static bool TryCreate(byte digit, out HexDigToken hexDigToken)
+        {
+            if ((digit >= 0x30 && digit <= 0x39) || (digit >= 0x41 && digit <= 0x46)
+        }
     }
 
     public sealed class SubsequentCharsReader<TNextReader>
