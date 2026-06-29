74e03d6429372ef42a89727a9fcee7e9bc7f565a
ravudet <170215607+ravudet@users.noreply.github.com>
2026-06-29 08:24:39 -0700

    asdf

--- OddTrotter.Core/Fx/Json/StringReader.cs
+++ OddTrotter.Core/Fx/Json/StringReader.cs
@@ -366,8 +366,13 @@
         public byte Char { get; }
     }
 
-    public sealed class UnicodeReader<TNextReader>
+    public sealed class UnicodeReader<TNextReader> : IMoveReader<UnicodeReader<TNextReader>, UReader<HexDigReader<HexDigReader<HexDigReader<HexDigReader<TNextReader>>>>>>
     {
+        public static bool TryMove(Context context, out UReader<HexDigReader<HexDigReader<HexDigReader<HexDigReader<TNextReader>>>>>? nextReader)
+        {
+            nextReader = default;
+            return true;
+        }
     }
 
     public sealed class UReader<TNextReader>
