5d995ec8ac840347f0e89013b0d8571c6400f545
ravudet <170215607+ravudet@users.noreply.github.com>
2026-07-01 08:15:59 -0700

    asdf

--- TestProject3/Fx/Json/ReaderUnitTests.cs
+++ TestProject3/Fx/Json/ReaderUnitTests.cs
@@ -67,22 +67,62 @@
             ReadWhitespace(whitespace2, context, 6, out var members1);
             Assert.IsTrue(members1.TryMove(context, out var membersCategory1));
             Assert.IsTrue(membersCategory1.TrySome(out var member1));
+
+            // true
             ReadMemberToValue(member1, context, 4, out var value2);
             Assert.IsTrue(value2.TryMove(context, out var valueCategory2));
             Assert.IsTrue(valueCategory2.TryTrue(out var true1));
             Assert.IsTrue(true1.TryMove(context, out var subsequentMembers1, out _, out _));
-            Assert.IsTrue(subsequentMembers1.TryMove(context, out var subsequentMembersCategory1));
+
+            // false
             ReadSubsequentMemberToValue(subsequentMembers1, context, 5, 6, out var value3);
+            Assert.IsTrue(value3.TryMove(context, out var valueCategory3));
+            Assert.IsTrue(valueCategory3.TryFalse(out var false1));
+            Assert.IsTrue(false1.TryMove(context, out var subsequentMembers2, out _, out _));
+
+            // 1234
+            ReadSubsequentMemberToValue(subsequentMembers2, context, 6, 6, out var value4);
+            Assert.IsTrue(value4.TryMove(context, out var valueCategory4));
+            Assert.IsTrue(valueCategory4.TryNumber(out var number1));
+            ReadNumber(number1, context, out var subsequentMembers3);
+
+            // asdf
+            ReadSubsequentMemberToValue(subsequentMembers3, context, 6, 6, out var value5);
+            Assert.IsTrue(value5.TryMove(context, out var valueCategory5));
+            Assert.IsTrue(valueCategory5.TryString(out var string1));
+            ReadString(string1, context, 4, out var subsequentMember4);
 
             //// TODO you are here
         }
 
-        private static void ReadSubsequentMemberToValue<TNextReader>(SubsequentMembersReader<TNextReader>? subsequentMembersReader, Context context, int memberNameLength, int tabLength, out ValueReader<TNextReader>? valueReader)
+        private static void ReadNumber<TNextReader>(NumberReader<TNextReader>? numberReader, Context context, out TNextReader? nextReader)
+        {
+            Assert.IsTrue(numberReader.TryMove(context, out var sign));
+            Assert.IsTrue(sign.TryMove(context, out var @int, out _));
+            Assert.IsTrue(@int.TryMove(context, out var intCategory));
+            Assert.IsTrue(intCategory.TryNonZero(out var leadingDigit));
+            Assert.IsTrue(leadingDigit.TryMove(context, out var digits, out _));
+            for (int i = 0; i < 4; ++i)
+            {
+                Assert.IsTrue(digits.TryMove(context, out var category));
+                Assert.IsTrue(category.TrySome(out var digit));
+                Assert.IsTrue(digit.TryMove(context, out digits, out _));
+            }
+
+            Assert.IsTrue(digits.TryMove(context, out var digitsCategory));
+            Assert.IsTrue(digitsCategory.TryNone(out var frac));
+            Assert.IsTrue(frac.TryMove(context, out var fracCategory));
+            Assert.IsTrue(fracCategory.TryAbsent(out var exp));
+            Assert.IsTrue(exp.TryMove(context, out var expCategory));
+            Assert.IsTrue(expCategory.TryAbsent(out nextReader));
+        }
+
+        private static void ReadSubsequentMemberToValue<TNextReader>(SubsequentMembersReader<TNextReader>? subsequentMembersReader, Context context, int memberNameLength, int tabLength, out ValueReader<SubsequentMembersReader<TNextReader>>? valueReader)
         {
             Assert.IsTrue(subsequentMembersReader.TryMove(context, out var subsequentMembersCategory));
             Assert.IsTrue(subsequentMembersCategory.TrySome(out var comma));
-            Assert.IsTrue(comma.TryMove(context, out var whitespace, out _));
-            ReadWhitespace(whitespace, context, tabLength, out var member);
+            Assert.IsTrue(comma.TryMove(context, out var whitespace1, out _));
+            ReadWhitespace(whitespace1, context, tabLength, out var member);
             ReadMemberToValue(member, context, memberNameLength, out valueReader);
         }
 
